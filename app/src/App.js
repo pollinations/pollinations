5c27a6013551eb4867d7670b3a137d954b8cac8d
Thomas Haferlach <t.haferlach@gmail.com>
2021-04-18 13:21:44 +0200

    UI Improvements

--- app/src/App.js
+++ app/src/App.js
@@ -1,10 +1,10 @@
 import React, { useEffect, useMemo, useState } from 'react';
 import openStomp from './network/stompClient';
-import { Button,  TextField , Fab} from "@material-ui/core"
-import {PlayArrow, Stop} from '@material-ui/icons';
+import { Button,  TextField , Fab, Card, CardMedia, Container, CardContent,Typography, IconButton, CardHeader, Avatar, CardActions, Box, Paper, Divider,CardActionArea} from "@material-ui/core"
+import {PlayArrow, Stop, MoreVer, Favorite, Share} from '@material-ui/icons';
 
 function App() {
-    const [latestConsole, setLatestConsole] = useState({headers:{}});
+    const [latestConsole, setLatestConsole] = useState({headers: {text:"Connecting..."}, body:"Loading..."});
     const [latestMedia, setLatestMedia] = useState({headers:{type:"image/jpeg"}});
     const [isRunning, setRunning] = useState(false);
     const [text, setText] = useState("Petite and futuristic")
@@ -12,26 +12,57 @@ function App() {
     useMemo(() => {
       openStomp(setLatestConsole, setLatestMedia)
     },[]);
-    useEffect(() => setText(latestConsole.headers.text),[latestConsole])
+    useEffect(() => setText(latestConsole.headers.text),[latestConsole.headers.text])
     
     const colabURL = "https://colab.research.google.com/github/voodoohop/colabasaservice/blob/master/colabs/deep-daze.ipynb";
     return (
-      <div style={{margin:"auto", maxWidth:"800px"}}>
-      <center>
-        <h1>Text to Image (CLIP+SIREN)</h1>
-        <form>
-        <TextField style={{align:"center"}} id="standard-basic" label="Prompt" variant="filled"  multiline  value={text} disabled={isRunning}/>
-        <Fab onClick={() => setRunning(value => !value)}>{isRunning ? <Stop />:<PlayArrow />}</Fab>
-        </form>
-        <br />
-        { latestMedia.headers.type.startsWith("image") ?
-          <img style={{width:"80%", height:"auto", maxHeight:"500px", maxWidth:"500px"}} src={latestMedia.body} />
-          : <video style={{width:"80%", height:"auto", maxHeight:"500px", maxWidth:"500px"}} src={latestMedia.body} autoPlay loop />
-        } 
-        <h3>{latestConsole.body}</h3>
+      <Container maxWidth="sm">
 
-     </center>
-     </div>
+        <Card variant="outlined">
+        <CardActionArea>
+        <CardHeader 
+        action={
+          <Box marginTop="25px" marginRight="0px" paddingRight="0px" marginBottom="0px" paddingBottom="0px">
+          <TextField 
+            style={{align:"center"}} 
+            label="Prompt"  
+            multiline  
+            value={text} 
+            disabled={isRunning}
+            onChange={({target}) => setText(target.value)} /> 
+          <IconButton onClick={() => setRunning(value => !value)}>{isRunning ? <Stop />:<PlayArrow />}</IconButton>
+          </Box>
+        }
+        title="Text to Image "
+        subheader="(CLIP+SIREN)"
+      /> 
+      
+
+        <CardMedia
+          component={latestMedia.headers.type.startsWith("image") ? "img":"video"}
+          src={latestMedia.body}
+          title={text}
+          style={{minHeight:"500px"}}
+        />
+
+        <CardContent>
+        <Typography variant="body2" color="textPrimary" component="p" style={{fontWeight:"bold"}}>
+          {latestConsole.body.replace(/\].*/g,"")}
+        </Typography>
+        </CardContent>
+        </CardActionArea>
+        <CardActions>
+        
+        
+        <IconButton aria-label="add to favorites">
+          <Favorite />
+        </IconButton>
+        <IconButton aria-label="share">
+          <Share />
+        </IconButton>
+        </CardActions>
+        </Card>
+        </Container>
     );
 }
 

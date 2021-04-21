78f6e14fd409e70fb0b603cecf3d78216aab006e
Thomas Haferlach <t.haferlach@gmail.com>
2021-04-21 16:30:39 +0200

    Before making dynamic based on notebook

--- app/src/App.js
+++ app/src/App.js
@@ -1,82 +1,21 @@
+import { Model } from './Model';
 import React, { useEffect, useMemo, useState } from 'react';
-import openStomp from './network/stompClient';
-import {TextField , Fab, Card, CardMedia, Container, CardContent,Typography, IconButton, CardHeader, Avatar, CardActions, Box, Paper, Divider,CardActionArea} from "@material-ui/core"
-import {PlayArrow, Stop, MoreVer, Favorite, Share} from '@material-ui/icons';
-import colabLogoImage from "./help/colab_icon.png";
+
+import {Container} from "@material-ui/core"
+
 import "./network/connectToLocalColab";
+import notebooks from "./data/notebooks.json";
 
 function App() {
-    const [latestConsole, setLatestConsole] = useState({headers: {text:""}, body:"Loading..."});
-    const [latestMedia, setLatestMedia] = useState({headers:{type:"image/jpeg"}});
-    const [isRunning, setRunning] = useState(false);
-    const [text, setText] = useState("")
-    // console.log("latest",latestConsole);
-    const queueMessage = new useMemo(() => {
-      return openStomp(setLatestConsole, setLatestMedia)
-    },[]);
 
-    useEffect(() => setText(latestConsole.headers.text),[latestConsole.headers.text])
 
-    const colabURL = "https://colab.research.google.com/github/voodoohop/colabasaservice/blob/master/colabs/deep-daze.ipynb";
-    return (
+   return (
       <Container maxWidth="sm">
-
-        <Card variant="outlined">
-        <CardActionArea>
-        <CardHeader 
-          action={(
-
-            <Box marginTop="25px" marginRight="0px" paddingRight="0px" marginBottom="0px" paddingBottom="0px">
-            <a href={colabURL} target="_blank"><img src={colabLogoImage} width="70" height="auto" /> </a>
-            </Box>)
-          }
-          title="Text to Image "
-          subheader="(CLIP+SIREN)"
-        /> 
-      
-      <CardContent>
-
-      <TextField 
-          style={{width:"90%"}} 
-          label="Prompt"  
-          multiline
-          fullWidth  
-          value={text} 
-          disabled={isRunning}
-          onChange={({target}) => setText(target.value)} /> 
-        <IconButton onClick={() => {
-          if (!isRunning)
-            queueMessage(text);
-          setRunning(value => !value);
-          }}>{isRunning ? <Stop />:<PlayArrow />}</IconButton>
-
-        </CardContent>
-        <CardMedia
-          component={latestMedia.headers.type.startsWith("image") ? "img":"video"}
-          src={latestMedia.body}
-          title={text}
-          style={{minHeight:"500px"}}
-          controls
-        />
-
-        <CardContent>
-
-        <Typography variant="body2" color="textPrimary" component="p" style={{fontWeight:"bold"}}>
-          <pre>{latestConsole.body.replace(/\].*/g,"")}</pre>
-        </Typography>
-        </CardContent>
-        </CardActionArea>
-        <CardActions>
-        
-        
-        <IconButton aria-label="add to favorites">
-          <Favorite />
-        </IconButton>
-        <IconButton aria-label="share">
-          <Share />
-        </IconButton>
-        </CardActions>
-        </Card>
+        {
+          notebooks.map((notebook,i) => (
+          <Model key={i} notebook={notebook} />
+          ))
+        }
         </Container>
     );
 }

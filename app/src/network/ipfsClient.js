b1def8b4a5db3ac458b4ad9ba7ed19721b621f48
Thomas Haferlach <t.haferlach@gmail.com>
2021-04-26 01:04:54 +0200

    Add to ipfs folder instead of dispatching action

--- app/src/network/ipfsClient.js
+++ app/src/network/ipfsClient.js
@@ -1,21 +1,18 @@
 
 import {useState, useEffect, useMemo} from "react";
-
 import Client from 'ipfs-http-client';
-
-
-
+import toUrlSearchParams from "ipfs-http-client/src/lib/to-url-search-params";
 
 const connect =  (onConnect) => {
     const client = Client('http://18.157.173.110:5002')
 
     const colabChannel = new BroadcastChannel("colabconnection");
     let nodeID = null;
-    let contentID = null;
+    let rootContentID = null;
     colabChannel.onmessage = ({data}) => {
-        [nodeID, contentID] = data.split(",");
+        [nodeID, rootContentID] = data.split(",");
 
-        console.warn("nodeID from colab", nodeID, "contentId", contentID);
+        console.warn("nodeID from colab", nodeID, "contentId", rootContentID);
         
         if (onConnect)
             onConnect(nodeID);
@@ -25,23 +22,33 @@ const connect =  (onConnect) => {
     // sending any message will trigger a reply with the nodeID
     colabChannel.postMessage("get_nodeid");
     // client.pubsub.subscribe(nodeID, dispa)
-    const dispatch = async data => {
+    const add = async data => {
 
         // client.object.patch.addLink();
-        client.pubsub.publish(nodeID, JSON.stringify(data));
+        
+        const { path: contentID } = await client.add(JSON.stringify(data));
+        console.log("Added contentID", contentID);
+        const patchRes = await client.object.patch.addLink(rootContentID, {Hash: contentID,name:"metadata.json"});
+        console.log({patchRes});
+        for await (const getRes of client.ls(patchRes)) {
+            console.log("file:",getRes.name);
+            // for await (const content of getRes.content) 
+            //     console.log("content",content.toString());
+        }
+        client.pubsub.publish(nodeID, patchRes.toString());
     };
 
-    return dispatch;
+    return add;
 }
 
 // connect();
 const useColab= () => {
     const [nodeID, setNodeID] = useState(null);
 
-    const dispatch = useMemo(() => {
+    const add = useMemo(() => {
         return connect(setNodeID);
     }, []);
-    return {nodeID, dispatch};
+    return {nodeID, add};
 };
 
 export default useColab;

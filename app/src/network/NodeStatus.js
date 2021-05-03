6f5f147d4f7408516a61dda47f4778a4a49fe587
Thomas Haferlach <t.haferlach@gmail.com>
2021-05-03 19:42:55 +0200

    Before reducer

---
+++ app/src/network/NodeStatus.js
@@ -0,0 +1,32 @@
+import React from "react";
+import Table from '@material-ui/core/Table';
+import TableBody from '@material-ui/core/TableBody';
+import TableCell from '@material-ui/core/TableCell';
+import TableContainer from '@material-ui/core/TableContainer';
+import TableHead from '@material-ui/core/TableHead';
+import TableRow from '@material-ui/core/TableRow';
+import { displayContentID } from "./utils";
+
+export default ({ nodeID, contentID }) => (
+    <TableContainer>
+        <Table size="small" >
+            <TableBody>
+                <TableRow>
+                    <TableCell>
+                        NodeID
+            </TableCell>
+                    <TableCell>
+                        <b>{nodeID ? displayContentID(nodeID) : "Not connected..."}</b>
+                    </TableCell>
+                </TableRow>
+                <TableRow>
+                    <TableCell>
+                        ContentID </TableCell>
+                    <TableCell>
+                        <b>{contentID ? displayContentID(contentID) : "Not connected..."}</b>
+                    </TableCell>
+                </TableRow>
+            </TableBody>
+        </Table>
+    </TableContainer>
+)

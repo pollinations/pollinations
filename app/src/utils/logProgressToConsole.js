2f5c065f66417759c6222853e8d56cf623b8986f
Thomas Haferlach <t.haferlach@gmail.com>
2021-05-11 14:30:37 +0200

    Disable progress log

--- app/src/utils/logProgressToConsole.js
+++ app/src/utils/logProgressToConsole.js
@@ -1,38 +1,47 @@
-import {MultiBar, Presets, SingleBar} from 'cli-progress';
+// import {MultiBar, Presets, SingleBar} from 'cli-progress';
 
-const multibar = new MultiBar({
-    clearOnComplete: false,
-    hideCursor: false,
-    format: ' {bar} | "{cid}" | {value}/{total}',
-    // barCompleteChar: '\u2588',
-    // barIncompleteChar: '\u2591',
-    stopOnComplete: false,
-    fps:8
-});
+import { noop } from "../network/utils";
 
-const logProgress = (total, name="") => { 
+// const multibar = new MultiBar({
+//     clearOnComplete: false,
+//     hideCursor: false,
+//     format: ' {bar} | "{cid}" | {value}/{total}',
+//     // barCompleteChar: '\u2588',
+//     // barIncompleteChar: '\u2591',
+//     stopOnComplete: false,
+//     fps:8
+// });
+
+// const logProgress = (total, name="") => { 
     
-    const bar = multibar.create(total, 0, {name});
+//     const bar = multibar.create(total, 0, {name});
 
-    const update = increment => {
-        bar.increment(increment);
-    }
+//     const update = increment => {
+//         bar.increment(increment);
+//     }
 
-    const remove = () => {
-        // bar.stop();
-        multibar.remove(bar);
-    }
+//     const remove = () => {
+//         // bar.stop();
+//         multibar.remove(bar);
+//     }
 
-    return [update, remove];
-} 
+//     return [update, remove];
+// } 
 
-export default logProgress;
+// export default logProgress;
 
-export const logProgressAsync = async function* (iterator, total, name)  {
-    const [log, remove] = logProgress(total,name);
-    for await (const chunk of iterator) {
-        log(chunk.length,name);
-        yield(chunk);
-    }
-    remove();
-}
+// export const logProgressAsync = async function* (iterator, total, name)  {
+//     const [log, remove] = logProgress(total,name);
+//     for await (const chunk of iterator) {
+//         log(chunk.length,name);
+//         yield(chunk);
+//     }
+//     remove();
+// }
+
+
+export default logProgress = noop;
+
+export const logProgressAsync = it => it;
+
+

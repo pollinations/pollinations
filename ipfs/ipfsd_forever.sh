d11272cbbc1f441081d0559ec57fd4ff7667970d
Thomas Haferlach <t.haferlach@gmail.com>
2021-05-28 20:04:39 +0200

    Starter scripts

--- ipfs/ipfsd_forever.sh
+++ ipfs/ipfsd_forever.sh
@@ -3,7 +3,7 @@ set -m
 while :
 do
     ipfs daemon --enable-namesys-pubsub --enable-pubsub-experiment &
-    sleep 10
+    sleep 15
     ipfs swarm connect /ip4/18.157.205.205/tcp/4001/p2p/12D3KooWDwQ1R9ZmDRv8aWL4dJ4svS9AYvwfJicQ1F45W5aHAVmy
     fg
     sleep 2

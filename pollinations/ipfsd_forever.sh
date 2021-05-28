d59a2f189567b8d14951163a4ed5dbc5c324d80b
Thomas Haferlach <t.haferlach@gmail.com>
2021-05-28 18:24:29 +0200

    Update ipfsd_forever.sh

--- pollinations/ipfsd_forever.sh
+++ pollinations/ipfsd_forever.sh
@@ -1,6 +1,9 @@
 #!/bin/bash
 while :
 do
-    ipfs daemon --enable-namesys-pubsub --enable-pubsub-experiment
+    ipfs daemon --enable-namesys-pubsub --enable-pubsub-experiment &
+    sleep 10
+    ipfs swarm connect /ip4/18.157.205.205/tcp/4001/p2p/12D3KooWDwQ1R9ZmDRv8aWL4dJ4svS9AYvwfJicQ1F45W5aHAVmy
+    fg
     sleep 2
 done

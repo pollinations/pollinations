deb9de3d33fbf025672fe800bc5f80656e0c06ee
ale-rls <ale_@gmx.com>
2021-06-14 20:14:36 +0200

    Layout change

--- app/src/components/NodeStatus.js
+++ app/src/components/NodeStatus.js
@@ -3,41 +3,49 @@ import { getWebURL } from "../network/ipfsConnector";
 import Acordion from './Acordion'
 
 // Styles temporary
+let WrapperStyle = {
+    display: 'flex',
+    flexWrap: 'wrap'
+}
 let RowStyle = {
+    width: 'calc(50% - 5px)',
     display: 'flex', 
     flexDirection: 'column',
-    flexWrap: 'wrap',
+    justifyContent: 'flex-start',
     borderBottom: '1px solid rgba(255, 255, 255, 0.2)',
     alignItems: 'flex-start',
-    marginRight: 20,
-    paddingBottom: 5,
-    marginBottom: 5
+    padding: '10px 5px 10px 0 !important',
+    padding: 2.5
     };
 let LinkStyle = {
     wordBreak: 'break-all',
     color: 'whitesmoke',
-    margin: '0 0 20px',
+    padding: '10px 0'
+}
+let ParagraphStyle = {
+    margin: '5px 0',
+    fontWeight: 'bold',
+    opacity: 0.9
 }
 
 export default ({ nodeID, contentID }) => {
-    return <>
+    return <div style={WrapperStyle}>
 
     <div style={RowStyle}>
-        <p children='NodeID'/>
+        <p children='NodeID' style={ParagraphStyle}/>
         
         <Acordion 
             visibleContent={nodeID ? displayContentID(nodeID) : "Not connected..."}
-            hiddenContent={'Lorem ipsum dolor sit amet, consectetur adipiscing elit.'} />
+            hiddenContent={'Lorem ipsum dolor sit amet, consectetur adipiscing elit.123123123'} />
     </div>
     
 
     <div style={RowStyle}>
-        <p children='ContentID'/>
-
+        <p children='ContentID' style={ParagraphStyle}/>
         {contentID ? 
         <a style={LinkStyle}
             href={getWebURL(contentID)} children={displayContentID(contentID)}/>
         : <p children="Not connected..."/>}
     </div>
-    </>
+    </div>
 }

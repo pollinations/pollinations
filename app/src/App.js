bfe8307e7d4e7d64be6692df18b65bde0269d4ce
Thomas Haferlach <t.haferlach@gmail.com>
2021-06-15 20:39:17 +0200

    Small layout tweaks

--- app/src/App.js
+++ app/src/App.js
@@ -6,7 +6,7 @@ import { BrowserRouter as Router, Switch, Route } from 'react-router-dom'
 
 function App() {
     return <Router>
-        <Container>
+        <Container maxWidth="md">
 
             <Switch
                 children={

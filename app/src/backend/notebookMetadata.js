77e85a1e2d48985612bbb5c32cc51f70bf71e1a9
Thomas Haferlach <t.haferlach@gmail.com>
2021-06-15 19:44:43 +0200

    Fix parameter handling

--- app/src/backend/notebookMetadata.js
+++ app/src/backend/notebookMetadata.js
@@ -1,3 +1,8 @@
+import Debug from "debug";
+import { parse } from "json5";
+
+const debug = Debug("notebookMetadata");
+
 
 function readMetadata(notebookJSON) {
   if (!notebookJSON)
@@ -7,14 +12,16 @@ function readMetadata(notebookJSON) {
   const { name } = metadata["colab"];
   const descriptionCell = cells[0];
   const parameterCell = cells[1];
+  debug("parameter cell", parameterCell);
   const description = descriptionCell["cell_type"] === "markdown" ? descriptionCell["source"].join("\n") : null;
   const parameterTexts = parameterCell["cell_type"] === "code" ? parameterCell["source"] : null;
+  debug("parameter texts", parameterTexts)
   const allParameters = parameterTexts
         .map(extractParameters)
         .filter(param => param)
         .map(mapToJSONFormField);
   const properties = Object.fromEntries(allParameters);
-
+  debug("got parameters", properties);
   return {
       form: {
         // "title": name,
@@ -28,8 +35,8 @@ function readMetadata(notebookJSON) {
 
 };
 
-const extractParameters = text => text.match(/^([a-zA-Z0-9-_]+)\s=\s'(.*)'\s\s#@param\s{type:\s"(.*)"}/);
+const extractParameters = text => text.match(/^([a-zA-Z0-9-_]+)\s=\s(.*)\s\s#@param\s{type:\s"(.*)"}/);
 
-const mapToJSONFormField = ([_text, name, defaultVal, type]) => [name, {type, default:defaultVal, title: name}];
+const mapToJSONFormField = ([_text, name, defaultVal, type]) => [name, {type, default: parse(defaultVal), title: name}];
 
 export default readMetadata;

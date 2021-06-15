c60d94cb649efa989f413e87d56ce19cd9d2f97f
Thomas Haferlach <t.haferlach@gmail.com>
2021-06-15 20:31:44 +0200

    Video display

--- app/src/components/ImageViewer.js
+++ app/src/components/ImageViewer.js
@@ -1,24 +1,39 @@
-import { Typography, Card, CardContent, GridList, GridListTile, IconButton,Button } from "@material-ui/core"
-
+import { Typography, Card, CardContent, GridList, GridListTile, IconButton,Button, GridListTileBar } from "@material-ui/core"
+import Debug from "debug";
 
 // Icons
 import ListIcon from '@material-ui/icons/List';
 import AppsIcon from '@material-ui/icons/Apps';
 
+const debug = Debug("ImageViewer");
+
+const MediaDisplay = ({filename, ...props}) => 
+  filename.toLowerCase().endsWith(".mp4") ? <video {...props} controls /> : <img {...props} />;
 
 function ImageViewer({images}) {
-    let imgs = images?.reverse();
 
+    if (!images || images.length === 0)
+      return null;
+
+    const imgs = images.reverse();
+
+    debug("images", imgs);
+
+    const firstFilename = imgs[0][0];
+    const firstURL = imgs[0][1]
+    debug("first",firstFilename, firstURL)
     return (
         <div >
           <div style={{ width: '50%', margin: '20px auto' }}>
-          <img src={imgs[0]?.[1]} alt={imgs[0]?.[0]} style={{ width: '100%'}} />
+            <MediaDisplay src={firstURL} filename={firstFilename} alt={firstFilename} style={{ width: '100%'}} />
+            {firstFilename}
           </div>
             
           <GridList cellHeight={200} cols={4}
             children={imgs.map(([filename, url]) => (
               <GridListTile key={filename} cols={1}>
-                <img src={url} alt={filename} style={{ margin: 0 }} />
+                <MediaDisplay src={url} filename={filename} alt={filename} style={{ margin: 0 }} />
+                <GridListTileBar title={filename} />
               </GridListTile>
             ))}/>
 

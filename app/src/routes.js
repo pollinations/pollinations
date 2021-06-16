28946fe6666b712b9cbea64ed4fdbcb932b150d2
Thomas Haferlach <t.haferlach@gmail.com>
2021-06-16 14:04:12 +0200

    Route change

--- app/src/routes.js
+++ app/src/routes.js
@@ -1,4 +1,5 @@
 import Model from "./pages/Model";
+import { Redirect } from "react-router-dom";
 
 export let routes = [
     {
@@ -6,6 +7,11 @@ export let routes = [
         path: '/p/:hash',
         children: <Model />,
     },
+    { 
+        exact: true,
+        path: "/",
+        children: <Redirect to="/p/QmNxqEhzvm4g8EjhmzRPLd6t3jrZehHssF4cUR3JRytDrv"/>
+    }
     //
     // Private here with if clause...
     /////////////////////////////////

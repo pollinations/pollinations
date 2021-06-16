01e1130fd5dc6f68afe0d9e7e1da03bcb29d3f50
Thomas Haferlach <t.haferlach@gmail.com>
2021-06-16 16:01:22 +0200

    Update Helmet.js

--- app/src/components/Helmet.js
+++ app/src/components/Helmet.js
@@ -5,8 +5,6 @@ export const SEOImage = ({url}) =>
          <meta name="image" content={url} />
          <meta property="og:image" content={"https://pollinations.ai/ipfs/QmUGafH7Lm18Rv7737BwQq1iLBEFHjxPggaep6mtBfVJxz/output/vibrant-painting-of-a-ufo-in-the-style-of-dali_00005.png"} />
          <meta property="twitter:image" content={"https://pollinations.ai/ipfs/QmUGafH7Lm18Rv7737BwQq1iLBEFHjxPggaep6mtBfVJxz/output/vibrant-painting-of-a-ufo-in-the-style-of-dali_00005.png"} />
-         <meta property="og:image:width" content={512} />
-         <meta property="og:image:height" content={512} />
          
     </Helmet>);
 

87b6b087850c83506ecbd0cd2f0a2a3a4148daa4
Thomas Haferlach <t.haferlach@gmail.com>
2021-06-16 15:56:32 +0200

    Update Helmet.js

--- app/src/components/Helmet.js
+++ app/src/components/Helmet.js
@@ -3,8 +3,8 @@ import { Helmet } from "react-helmet";
 export const SEOImage = ({url}) => 
     (<Helmet >
          <meta name="image" content={url} />
-         <meta property="og:image" content={url} />
-         <meta property="twitter:image" content={url} />
+         <meta property="og:image" content={"https://pollinations.ai/ipfs/QmUGafH7Lm18Rv7737BwQq1iLBEFHjxPggaep6mtBfVJxz/output/vibrant-painting-of-a-ufo-in-the-style-of-dali_00005.png"} />
+         <meta property="twitter:image" content={"https://pollinations.ai/ipfs/QmUGafH7Lm18Rv7737BwQq1iLBEFHjxPggaep6mtBfVJxz/output/vibrant-painting-of-a-ufo-in-the-style-of-dali_00005.png"} />
          <meta property="og:image:width" content={512} />
          <meta property="og:image:height" content={512} />
          

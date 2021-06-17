8920021de16b7f07e2f6acb8401ecc43c1d9c89f
Thomas Haferlach <t.haferlach@gmail.com>
2021-06-17 18:35:24 +0200

    Update scripts

--- ipfs/ipfsd_install_remote.sh
+++ ipfs/ipfsd_install_remote.sh
@@ -10,6 +10,8 @@ cd -
 ipfs config Addresses.Swarm /ip4/0.0.0.0/tcp/4001
 ipfs config Addresses.API /ip4/0.0.0.0/tcp/5001
 ipfs config Addresses.Gateway /ip4/0.0.0.0/tcp/8080
+ipfs config Pubsub.Router floodsub
+ipfs config --json Pubsub.DisableSigning true
 ipfs config --json API.HTTPHeaders.Access-Control-Allow-Origin '["*"]'
 # sudo apt update
 

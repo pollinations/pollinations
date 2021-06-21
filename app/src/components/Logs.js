c12df7421e33e15f8d8674a6751c36f2d2548c55
Thomas Haferlach <t.haferlach@gmail.com>
2021-06-21 22:50:32 +0200

    Change state handling

--- app/src/components/Logs.js
+++ app/src/components/Logs.js
@@ -1,14 +1,10 @@
 import React from 'react'
 import { CardContent, Typography } from "@material-ui/core"
-import useColab from "../network/useColab"
 import ReactJson from 'react-json-view'
-import { displayContentID, noop } from "../network/utils";
+import { displayContentID } from "../network/utils";
 
-export const IpfsLog = () => {
-
-    const { state } = useColab(); // {state:{ipfs:{},contentID: null, nodeID:null}, dispatch: noop}
-    const { ipfs } = state;
-
+export const IpfsLog = ({state}) => {
+    const {ipfs, contentID} = state;
     return <div style={{maxWidth: '100%', overflow: 'hidden'}}>
 
         <CardContent>
@@ -16,16 +12,15 @@ export const IpfsLog = () => {
                 variant="body2"
                 color="textPrimary"
                 component="pre"
-                style={{ fontWeight: "bold" }}
                 children={
-                    ipfs.output && ipfs.output.log ? formatLog(ipfs) : "Loading..."
+                    ipfs.status && ipfs.status.log ? formatLog(ipfs.status.log) : "Loading..."
                 } />
         </CardContent>
 
         <CardContent>
             <ReactJson
-                src={state.ipfs}
-                name={displayContentID(state.contentID)}
+                src={ipfs}
+                name={displayContentID(contentID)}
                 enableClipboard={false}
                 displayDataTypes={false}
                 displayObjectSize={false}
@@ -35,11 +30,11 @@ export const IpfsLog = () => {
     </div>
 
 }
-function formatLog(ipfs) {
-    return ipfs.output.log
-            .replace(/\].*/g, "")
-            .split("\n")
-            .slice(-10)
-            .join("\n");
-}
+const formatLog = 
+        log =>  log
+                .replace(/\].*/g, "")
+                .split("\n")
+                .slice(-10)
+                .join("\n");
+
 

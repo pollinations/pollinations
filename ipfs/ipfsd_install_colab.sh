fb54630512c18b3467e7aa3e66a5dc3d8f088270
Thomas Haferlach <t.haferlach@gmail.com>
2021-06-24 08:06:47 +0200

    Update ipfsd_install_colab.sh

--- ipfs/ipfsd_install_colab.sh
+++ ipfs/ipfsd_install_colab.sh
@@ -2,7 +2,7 @@
 
 # IPFS
 
-wget https://ipfs.io/ipns/dist.ipfs.io/go-ipfs/v0.9.0/go-ipfs_v0.9.0_linux-arm64.tar.gz
+curl https://ipfs.io/ipns/dist.ipfs.io/go-ipfs/v0.9.0/go-ipfs_v0.9.0_linux-amd64.tar.gz --output ./go-ipfs_v0.9.0_linux-amd64.tar.gz
 tar -xvzf go-ipfs_v0.9.0_linux-amd64.tar.gz
 cd go-ipfs
 sudo bash install.sh

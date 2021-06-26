93e78ccb58ce9814f7fe5c62f9b3e52bd0066fb4
Thomas Haferlach <t.haferlach@gmail.com>
2021-06-26 23:40:24 +0200

    App bar

--- app/src/components/NodeStatus.js
+++ app/src/components/NodeStatus.js
@@ -1,60 +1,70 @@
 import { displayContentID } from "../network/utils";
 import { getWebURL } from "../network/ipfsConnector";
 import Acordion from './Acordion'
-import { Button, Link, Typography } from "@material-ui/core"
+import { Button, Link, Typography, AppBar, Toolbar, IconButton, List, ListItem as MuiListItem, Paper, TableContainer, Table, TableHead, TableRow, TableBody, TableCell as MuiTableCell, withStyles, styled} from "@material-ui/core"
+import ExpandMore from "@material-ui/icons/ExpandMore";
+import MenuIcon from "@material-ui/icons/Menu";
 
 const colabURL = "https://colab.research.google.com/github/voodoohop/pollinations/blob/dev/colabs/pollinator.ipynb";
 
-// Styles temporary
-let WrapperStyle = {
-    display: 'flex',
-    flexWrap: 'wrap'
-}
-let RowStyle = {
-    width: 'calc(50% - 5px)',
-    display: 'flex', 
-    flexDirection: 'column',
-    justifyContent: 'flex-start',
-    borderBottom: '1px solid rgba(255, 255, 255, 0.2)',
-    alignItems: 'flex-start',
-    padding: '10px 5px 10px 0 !important',
-    padding: 2.5
-    };
+const TableCell = withStyles({
+    root: {
+      borderBottom: "none"
+    }
+  })(MuiTableCell);
 
-let ParagraphStyle = {
-    margin: '5px 0',
-    fontWeight: 'bold',
-    opacity: 0.9
-}
+const ListItem = styled(MuiListItem)({
+      margin: "0px",
+      padding:"4px",
+      textAlign:"right"
+  });
+
+//     <Typography variant="h6" >
+//       News
+//     </Typography>
+//     <Button color="inherit">Login</Button>
+//   </Toolbar>
+// </AppBar>
 
 export default ({ nodeID, contentID, status, ipfs }) => {
     const colabState = ipfs?.output?.status;
-    return <>
-            <h3>Status <Typography component="span">[{status}, {colabState}]</Typography></h3>
-            <div style={WrapperStyle}>
-            <div style={RowStyle}>
-                <p children='NodeID' style={ParagraphStyle}/>
-                {nodeID ? <Link >{displayContentID(nodeID)}</Link> : <ColabConnectButton />}
-                   
+    return <AppBar position="static">
+        <Toolbar>
+         <IconButton edge="start" color="inherit" aria-label="menu">
+            <MenuIcon />
+         </IconButton>
+         {/* <List style={{marginLeft:"auto"}}>
+            <ListItem>
+                <Typography>Status</Typography>
+            </ListItem>
+            <ListItem>
+                <Typography component="span">[{status}, {colabState}]</Typography>
+            </ListItem>
+            </List> */}
+                <Table size="small" aria-label="a dense table" style={{width:"210px", marginLeft:"auto"}}>
+                    <TableBody>
+                        <TableRow>
+                            <TableCell style={{paddingRight: "0px"}}>Node</TableCell>
+                            <TableCell>{nodeID ? <Link >{displayContentID(nodeID)}</Link> : <ColabConnectButton />}</TableCell>
+                        </TableRow>
+                        <TableRow>
+                        <TableCell style={{paddingRight: "0px"}}>Content</TableCell>
+                            <TableCell>{contentID ?
+                                <Link
+                                    href={getWebURL(contentID)} children={displayContentID(contentID)}
+                                    target="_blank"
+                                />
+                                : <p children="Not connected..." />}
+                        </TableCell>
+                        </TableRow>
+                    </TableBody>
+                </Table>
+        </Toolbar>
 
-                
-            </div>
-
-            <div style={RowStyle}>
-                <p children='ContentID' style={ParagraphStyle}/>
-                {contentID ? 
-                <Link
-                    href={getWebURL(contentID)} children={displayContentID(contentID)}
-                    target="_blank"
-                    />
-                : <p children="Not connected..."/>}
-            </div>
-
-            </div>
-    </>
+    </AppBar>
 }
 
 
 function ColabConnectButton() {
-    return <Button color="secondary" href={colabURL} target="_blank">Launch Colab Node</Button>
+    return <Button color="secondary" href={colabURL} variant="outlined" target="_blank">Launch</Button>
 }

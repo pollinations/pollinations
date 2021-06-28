10c4fa297bec3b03b776df91cfb0cd5fa33def9d
Thomas Haferlach <t.haferlach@gmail.com>
2021-06-28 13:27:58 +0200

    Update NotebookProgress.js

--- app/src/components/NotebookProgress.js
+++ app/src/components/NotebookProgress.js
@@ -1,4 +1,4 @@
-import { LinearProgress } from "@material-ui/core";
+import { Box, LinearProgress, Typography } from "@material-ui/core";
 import { last } from "ramda";
 import React from "react";
 import Debug from "debug";
@@ -8,9 +8,19 @@ const debug = Debug("NotebookProgress");
 export const NotebookProgress = ({output, metadata}) => {
     if (!output?.log)
         return null;
-    const progress = getProgress(output.log, metadata.numCells);
+    const progress = getProgress(output.log, metadata.numCells)*100;
     debug(progress)
-    return <LinearProgress value={progress*100} variant="determinate" color="secondary" />
+    return  <Box display="flex" alignItems="center">
+    <Box width="100%" mr={1}>
+        <LinearProgress value={progress} variant="determinate" color="secondary" />
+    </Box>
+    <Box minWidth={35}>
+      <Typography variant="body2" color="textSecondary">{`${Math.floor(
+        progress
+      )}%`}</Typography>
+    </Box>
+  </Box>
+    
 }
 
 
@@ -24,7 +34,7 @@ export const getProgress = (log, numCells) => {
     if (!updateText)
         return 0;
     
-    const cellNo = parseInt(updateText.split("Ending Cell ")[1]);
+    const cellNo = parseInt(updateText.split("Ending Cell ")[1]) - 1;
     return cellNo / numCells;
 }
         

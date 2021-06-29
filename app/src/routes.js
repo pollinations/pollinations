e3a487e3e7865916473ef065df16943d23c636e0
Thomas Haferlach <t.haferlach@gmail.com>
2021-06-29 18:24:03 +0200

    Misc

--- app/src/routes.js
+++ app/src/routes.js
@@ -10,9 +10,9 @@ export let routes = [
     { 
         exact: true,
         path: "/",
-        children: <Redirect to="/p/QmdAKhLJiMYErqHpmQTgyezGw64cUHnYxMUjisCs1N8m5i" />,
+        children: <Redirect to="/p/QmQNGybDsMropwa2WuGU2HEpWabiiJke6tkDVr8FZ7kjVE" />,
     }
 ]
 
 // Deep Daze"/p/Qma18Qy5tHzNEK1D7Qwa7UkEzZBWjtStW2s7hDRqJyxJ3g"
-// Latent2Visions "/p/QmdAKhLJiMYErqHpmQTgyezGw64cUHnYxMUjisCs1N8m5i"
+// Latent2Visions "/p/QmQNGybDsMropwa2WuGU2HEpWabiiJke6tkDVr8FZ7kjVE"

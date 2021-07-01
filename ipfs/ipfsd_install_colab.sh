c00345be3c860ca8e000cbce8371bd25014731fa
Thomas Haferlach <t.haferlach@gmail.com>
2021-07-01 10:33:21 +0200

    Disable swarm connect

--- ipfs/ipfsd_install_colab.sh
+++ ipfs/ipfsd_install_colab.sh
@@ -14,8 +14,8 @@ ipfs config Addresses.API /ip4/0.0.0.0/tcp/5001
 ipfs config --json API.HTTPHeaders.Access-Control-Allow-Origin '["*"]'
 #ipfs config Pubsub.Router floodsub
 #ipfs config Pubsub.DisableSigning true
-ipfs config --json Peering.Peers '[{"ID":"12D3KooWDwQ1R9ZmDRv8aWL4dJ4svS9AYvwfJicQ1F45W5aHAVmy", "Addrs": ["/ip4/18.157.205.205/tcp/4001"]}]' 
-ipfs config --json Swarm.EnableAutoRelay true
+#ipfs config --json Peering.Peers '[{"ID":"12D3KooWDwQ1R9ZmDRv8aWL4dJ4svS9AYvwfJicQ1F45W5aHAVmy", "Addrs": ["/ip4/18.157.205.205/tcp/4001"]}]' 
+#ipfs config --json Swarm.EnableAutoRelay true
 
 # # Pollinations
 # git clone https://github.com/voodoohop/pollinations

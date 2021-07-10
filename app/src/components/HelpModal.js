d008487939785debebe4e839ad151807a1d21c5b
Thomas Haferlach <t.haferlach@gmail.com>
2021-07-10 12:27:42 +0200

    Improve usability a little and highlight problems

--- app/src/components/HelpModal.js
+++ app/src/components/HelpModal.js
@@ -5,21 +5,32 @@ import { Paper, Modal, Button, Box } from "@material-ui/core";
 
 export default function SimpleModal() {
 
-    const [open, setOpen] = useState(true);
+    const [open, setOpen] = useState(false);
     const [markdown, setMarkdown] = useState("*loading...*");
-    
+  
     useEffect(async () => {
         const response = await fetch("https://raw.githubusercontent.com/pollinations/pollinations/dev/docs/instructions.md");
         const md = await response.text();
         setMarkdown(md);
     },[]);
-    if (!open)
-      return null;
-    return (<Paper style={{maxWidth:"90%"}} variant="outlined"> <Box p={1}>
-                <Markdown style={{maxWidth: "600px"}}>{markdown}</Markdown>
-              </Box>
-              <Button onClick={() => setOpen(false)}>Close</Button>
-            </Paper>
 
+    return (
+        <>
+        <Button type="button" onClick={() => setOpen(true)}>
+          [ Help ]
+        </Button>
+        <Modal
+          open={open}
+          onClose={() => setOpen(false)}
+          aria-labelledby="simple-modal-title"
+          aria-describedby="simple-modal-description"
+          style={{display:'flex',alignItems:'center',justifyContent:'center'}}
+        >
+            <Paper style={{maxWidth:"90%"}} variant="outlined"> <Box p={1}>
+                <Markdown style={{maxWidth: "600px"}}>{markdown}</Markdown>
+                </Box>
+            </Paper>
+        </Modal>
+        </>
     );
   }

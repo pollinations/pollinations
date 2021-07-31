35f0fcb11c69028031db8dda8256245bf6ce9f0f
Thomas Haferlach <t.haferlach@gmail.com>
2021-07-31 13:36:09 +0200

    Add notebook selector

---
+++ app/src/components/NotebookSelector.js
@@ -0,0 +1,65 @@
+import React, { useState } from 'react';
+import Drawer from '@material-ui/core/Drawer';
+import Button from '@material-ui/core/Button';
+import List from '@material-ui/core/List';
+import Divider from '@material-ui/core/Divider';
+import ListItem from '@material-ui/core/ListItem';
+import ListItemIcon from '@material-ui/core/ListItemIcon';
+import ListItemText from '@material-ui/core/ListItemText';
+import WallpaperIcon from '@material-ui/icons/Wallpaper';
+import AppBar from '@material-ui/core/AppBar';
+import Toolbar from '@material-ui/core/Toolbar';
+import Typography from '@material-ui/core/Typography';
+import IconButton from '@material-ui/core/IconButton';
+import MenuIcon from '@material-ui/icons/Menu';
+
+import { noop } from '../network/utils';
+
+const notebookList = [{
+    name: "CLIP-guided VQGan",
+    path: "/p/QmXJnK2FUHEDjR5aFLYELChU6JqdfSxNsoZpy1yHiMXNoK",
+    category: "Text-to-Image",
+    Icon: WallpaperIcon
+},
+{
+    name: "CLIP-guided Diffusion",
+    path: "/p/Qma1zZwTYTX5rKoGpyBY4DWCK7ERXGpto4pNKfmHAEFoVM",
+    category: "Text-to-Image",
+    Icon: WallpaperIcon
+},];
+
+// guided diffusion /p/Qma1zZwTYTX5rKoGpyBY4DWCK7ERXGpto4pNKfmHAEFoVM
+
+export default function NotebookSelector() {
+    const [visible, setVisible] = useState(false);
+    
+    return <>
+         <AppBar
+        position="static"
+      >
+        <Toolbar>
+          <IconButton
+            color="inherit"
+            aria-label="open drawer"
+            onClick={() => setVisible(true)}
+            edge="start"
+          >
+            <MenuIcon />
+          </IconButton>
+          <Typography variant="h6" noWrap>
+            Pollinations
+          </Typography>
+        </Toolbar>
+      </AppBar>
+          <Drawer anchor={"top"} open={visible} onClose={() => setVisible(false)}>
+          <List>
+        {notebookList.map(({name, category, Icon, path}) => (
+          <ListItem button key={name} component="a" href={path}>
+            <ListItemIcon> <Icon /> </ListItemIcon>
+            <ListItemText primary={`${category} - ${name}`} />
+          </ListItem>
+        ))}
+      </List>
+          </Drawer>
+        </>
+}

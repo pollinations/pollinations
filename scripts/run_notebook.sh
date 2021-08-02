3ebf905222576c42a1c2b2f0dbba25d7e18c2d73
Thomas Haferlach <t.haferlach@gmail.com>
2021-08-02 20:29:12 +0200

    Update run_notebook.sh

--- scripts/run_notebook.sh
+++ scripts/run_notebook.sh
@@ -1,27 +1,32 @@
 #!/bin/bash
 IPFS_ROOT=${1:-"/content/ipfs"}
 
+NOTEBOOK_PATH=$IPFS_ROOT/input/notebook.ipynb
+NOTEBOOK_OUTPUT_PATH=/content/notebook_out.ipynb
 
-CMD=$1
+PARAMS="hello"
+for path in $IPFS_ROOT/input/*; do
 
-#  parameters["output_path"] = output_path
-#   _params = [["-p", quote(key), quote(str(value))] for key, value in parameters.items()]
-#   params = [str(val) for sublist in _params for val in sublist]
-#   notebook_out_path = f"/content/notebook_output.ipynb"
-#   cmd = ["papermill", notebook_path, notebook_out_path] + params + ["--log-output"]
-#   cmd = " ".join(cmd)
-  
-echo 📗: "Removing last run output (if there was any)."
+    key=$(basename $path)
+    value_raw=$(<$path)
+    value=$(printf '%q' "$value_raw")
+
+    PARAMS+=" -p ""${key}""=""${value}"
+done
+
+echo "📗 PARAMS:" "$PARAMS"
+
+echo "📗: Removing last run output if there was any."
 rm -rv $IPFS_ROOT/output/*
 
-echo 📗: "Setting colab status to 'running'"
+echo "📗: Setting colab status to 'running'"
 echo -n running > $IPFS_ROOT/output/status
 
-echo 📗: "Executing $CMD"
-bash $CMD
+echo "📗: Executing papermill" "$NOTEBOOK_PATH" "$NOTEBOOK_OUTPUT_PATH" $PARAMS --log-output 
+papermill "$NOTEBOOK_PATH" "$NOTEBOOK_OUTPUT_PATH" $PARAMS --log-output 
 
-echo 📗: "Setting colab status to waiting"
+echo "📗: Setting colab status to waiting"
 echo -n waiting > $IPFS_ROOT/output/status
-echo 📗: "Setting the state to signify the run has ended"
+echo "📗: Setting the state to signify the run has ended"
 echo -n true > $IPFS_ROOT/output/done
 rm -v $IPFS_ROOT/input/formAction

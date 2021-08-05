889ed6c54f733fca65e5bedc2e345a3801c94fbe
Thomas Haferlach <t.haferlach@gmail.com>
2021-08-05 23:15:45 +0200

    Make CLIP+VQGAN default notebook

--- app/src/data/notebooks.js
+++ app/src/data/notebooks.js
@@ -1,18 +1,18 @@
 import WallpaperIcon from '@material-ui/icons/Wallpaper';
 
 export const notebooks = [
-    {
+  {
+    name: "CLIP-guided VQGan",
+    path: "/p/QmdA2759ErfqJT8qtrJNsYgwUXsTrg69bVdGFW2i3ibXDP",
+    category: "Text-to-Image",
+    Icon: WallpaperIcon
+  },
+  {
       name: "CLIP-guided Diffusion",
       path: "/p/QmTBUAGsqWzJsF1Ccuzk9W5STkzGa8bK6QcPpj7JrT4a6J",
       category: "Text-to-Image",
       Icon: WallpaperIcon
-    },  
-    {
-      name: "CLIP-guided VQGan",
-      path: "/p/QmdA2759ErfqJT8qtrJNsYgwUXsTrg69bVdGFW2i3ibXDP",
-      category: "Text-to-Image",
-      Icon: WallpaperIcon
-  },
+  },  
   {
       name: "DALL-E Mini",
       path: "/p/QmQBzUpwF21ynVfSU3tW2WiWZfNjS94aq58verfghDLpWV",

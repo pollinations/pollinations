4dd8a570efc94052bcc2c7e5550a5da52fabb301
gokaykucuk <gokay@hey.com>
2021-08-11 01:09:55 +0200

    added easy mode for hiding all the params except primaryInput

--- app/src/components/Form.js
+++ app/src/components/Form.js
@@ -2,31 +2,32 @@ import React from "react";
 import Form from "@rjsf/material-ui";
 import Button from '@material-ui/core/Button'
 import Debug from "debug";
-import { Box, Typography } from "@material-ui/core";
+import {Box, Typography} from "@material-ui/core";
 import HelpModal from "./HelpModal";
+import {filter} from "ramda";
+
 
 const debug = Debug("Form");
 
-const FormView = ({ input, status, colabState, metadata, nodeID, onSubmit, onCancel}) => {
+const FormView = ({input, status, colabState, metadata, nodeID, onSubmit, onCancel}) => {
 
     const filledForm = getFormInputs(input, metadata);
 
     if (!filledForm)
         return null;
 
-    debug("colabState",colabState)
+    debug("colabState", colabState)
 
-    const showSubmit = status === "disconnected" || status === "ready" && colabState !== "running" ;
+    const showSubmit = status === "disconnected" || status === "ready" && colabState !== "running";
     const showCancel = false; //!showSubmit && input.formAction !== "cancel";
 
     const inProgress = false//!!(input && input.formAction);
     const formDisabled = status === "disconnected" || inProgress;
 
-    debug("nodeID",nodeID, formDisabled)
+    debug("nodeID", nodeID, formDisabled)
     const uiSchema = getUISchema(filledForm, showSubmit)
-    
-    debug("form uiSchema", uiSchema, filledForm, showSubmit)
 
+    debug("form uiSchema", uiSchema, filledForm, showSubmit)
 
 
     return <Form
@@ -36,18 +37,18 @@ const FormView = ({ input, status, colabState, metadata, nodeID, onSubmit, onCan
         disabled={formDisabled || colabState === "running"}
     >
         <Box m={1}>
-            { showSubmit ? <Button type="submit" disabled={formDisabled} >
-                        [ {inProgress ? "Submitting..." : "Submit" } ] 
-                    </Button>
+            {showSubmit ? <Button type="submit" disabled={formDisabled}>
+                    [ {inProgress ? "Submitting..." : "Submit"} ]
+                </Button>
                 : null
             }
-                    
-            { showCancel && <Button type="button" color="secondary" onClick={onCancel} disabled={formDisabled} >
-                        [ {inProgress ? "Stopping...": "Stop"} ]
-                    </Button>
+
+            {showCancel && <Button type="button" color="secondary" onClick={onCancel} disabled={formDisabled}>
+                [ {inProgress ? "Stopping..." : "Stop"} ]
+            </Button>
             }
             {!showSubmit || formDisabled && <HelpModal/>}
-            
+
         </Box>
     </Form>
 }
@@ -58,9 +59,18 @@ export default React.memo(FormView)
 function getFormInputs(ipfs, metadata) {
     if ((metadata === undefined) || (metadata === null)) return;
     ipfs = ipfs || {};
+    debug("properties", metadata);
+    const allParams = Object.fromEntries(Object.entries(metadata.form.properties).map(
+        ([formKey, prop]) => [formKey, formKey in ipfs ? {...prop, "default": ipfs[formKey]} : prop]));
 
-    return Object.fromEntries(Object.entries(metadata.form.properties).map(
-            ([formKey, prop]) => [formKey, formKey in ipfs ? { ...prop, "default": ipfs[formKey] } : prop]))
+    // TODO: Carry it to state so users can disable it.
+    const easyMode = true;
+    // debug("all params", fil);
+    if(easyMode){
+        return filter((param) => (param.title === metadata.primaryInput), allParams);
+    }else{
+        return allParams
+    }
 }
 
 
@@ -70,15 +80,15 @@ const getUISchema = (filledForm, enabled) =>
 
 const toSchema = (key, enabled) => {
     const mappings = [
-        ["text_","text"],
-        ["file_","file"],
-        ["num_","updown"],
-        ["save_","radio"],
-        ["super_resolution","radio"],
-        ["","text"]
+        ["text_", "text"],
+        ["file_", "file"],
+        ["num_", "updown"],
+        ["save_", "radio"],
+        ["super_resolution", "radio"],
+        ["", "text"]
     ];
 
-    return { 
+    return {
         "ui:widget": mappings.find(([keyPrefix, _]) => key.toLowerCase().startsWith(keyPrefix))[1],
         "ui_disabled": !enabled
     }

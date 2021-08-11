052148654eafff1c078dd7adc6dbb0602eb1d8d1
gokaykucuk <gokay@hey.com>
2021-08-11 09:54:07 +0200

    mature updated

--- app/src/backend/mature.test.js
+++ app/src/backend/mature.test.js
@@ -4,7 +4,7 @@ test('mature words are filtered if they have spaces around them', () => {
     expect(mature("ass")).toBe("***")
 });
 
-test('mature words are filtered if they have spaces around them', () => {
+test('mature words are filtered if they are at the end of sentence', () => {
     expect(mature("the ass.")).toBe("the ***.")
 });
 

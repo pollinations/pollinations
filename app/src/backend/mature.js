fca7c8eea7b9ad757d3067407f30cb45b950ea91
Thomas Haferlach <t.haferlach@gmail.com>
2021-08-11 08:55:01 +0200

    Update mature.js

--- app/src/backend/mature.js
+++ app/src/backend/mature.js
@@ -1,7 +1,13 @@
-import matureWords from "./matureWords.json"
+import matureWords from "./matureWords.json";
 
-const repeatChar = (c, n) => n === 0 ? c : c + repeatChar(c, n - 1)
-
+// replace all mature words by *
 const mature = raw_input => matureWords.reduce((text, word) => text.replace(word, repeatChar("*", word.length)), raw_input);
 
-export default mature;
+// create a string of * of length n
+const repeatChar = (c, n) => n === 0 ? c : c + repeatChar(c, n - 1)
+
+// replace non-alphanumeric characters by spaces
+const clean = raw_input => raw_input.replace(/[^a-zA-Z0-9]/g, " ");
+
+
+export default text => mature(clean(text));

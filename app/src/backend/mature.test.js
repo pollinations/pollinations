0384eea15bd96a59371e73aba7b1b0e55369a74d
Thomas Haferlach <t.haferlach@gmail.com>
2021-08-11 08:42:18 +0200

    Revert reading JSON to using import system

--- app/src/backend/mature.test.js
+++ app/src/backend/mature.test.js
@@ -1,8 +1,13 @@
 import mature from "./mature"
 
 test('mature words are filtered if they have spaces around them', () => {
-    expect(mature(" ass ")).toBe("******")
+    expect(mature("ass")).toBe("***")
 });
+
+test('mature words are filtered if they have spaces around them', () => {
+    expect(mature("the ass.")).toBe("the ***.")
+});
+
 
 test('mature words are not filtered if they are part of a word', () => {
     expect(mature("tasser")).toBe("tasser")

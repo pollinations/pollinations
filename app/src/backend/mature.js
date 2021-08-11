5852d1aadcd324eb8f3b3cdf272477144d245c90
gokaykucuk <gokay@hey.com>
2021-08-11 09:02:04 +0200

    updated words import

--- app/src/backend/mature.js
+++ app/src/backend/mature.js
@@ -1,7 +1,7 @@
-import matureWords from "./matureWords.json";
+import {words} from "./matureWords.json";
 
 // replace all mature words by *
-const mature = raw_input => matureWords.reduce((text, word) => text.replace(word, repeatChar("*", word.length)), raw_input);
+const mature = raw_input => words.reduce((text, word) => text.replace(word, repeatChar("*", word.length)), raw_input);
 
 // create a string of * of length n
 const repeatChar = (c, n) => n === 0 ? c : c + repeatChar(c, n - 1)

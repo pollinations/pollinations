2fb267263d5e99465ee8476862dfecedc267dc78
gokaykucuk <gokay@hey.com>
2021-08-16 20:07:52 +0200

    not done

--- app/src/data/notebooks.js
+++ app/src/data/notebooks.js
@@ -3,19 +3,19 @@ import WallpaperIcon from '@material-ui/icons/Wallpaper';
 export const notebooks = [
   {
     name: "CLIP-guided VQGan HQ",
-    path: "/p/QmebmPTxDAHW75tyjAyxLuYpPFUTSSQsTTEiucPJskzqLL",
+    path: "/p/#QmebmPTxDAHW75tyjAyxLuYpPFUTSSQsTTEiucPJskzqLL",
     category: "Text-to-Image",
     Icon: WallpaperIcon
   },
   {
       name: "CLIP-guided Diffusion",
-      path: "/p/QmTBUAGsqWzJsF1Ccuzk9W5STkzGa8bK6QcPpj7JrT4a6J",
+      path: "/p/#QmTBUAGsqWzJsF1Ccuzk9W5STkzGa8bK6QcPpj7JrT4a6J",
       category: "Text-to-Image",
       Icon: WallpaperIcon
   },  
   {
       name: "DALL-E Mini",
-      path: "/p/QmQBzUpwF21ynVfSU3tW2WiWZfNjS94aq58verfghDLpWV",
+      path: "/p/#QmQBzUpwF21ynVfSU3tW2WiWZfNjS94aq58verfghDLpWV",
       category: "Text-to-Image",
       Icon: WallpaperIcon
   },

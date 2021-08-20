d6d162627eb3c45a209e2d435237f33bc4dc7373
Thomas Haferlach <t.haferlach@gmail.com>
2021-08-20 12:05:20 +0200

    Warning update and quick form fix

--- app/src/data/notebooks.js
+++ app/src/data/notebooks.js
@@ -2,17 +2,17 @@ import WallpaperIcon from '@material-ui/icons/Wallpaper';
 
 export const notebooks = [
   {
+    name: "CLIP-guided Diffusion",
+    path: "/p/QmTBUAGsqWzJsF1Ccuzk9W5STkzGa8bK6QcPpj7JrT4a6J",
+    category: "Text-to-Image",
+    Icon: WallpaperIcon
+ },  
+  {
     name: "CLIP-guided VQGan HQ",
     path: "/p/QmebmPTxDAHW75tyjAyxLuYpPFUTSSQsTTEiucPJskzqLL",
     category: "Text-to-Image",
     Icon: WallpaperIcon
   },
-  {
-      name: "CLIP-guided Diffusion",
-      path: "/p/QmTBUAGsqWzJsF1Ccuzk9W5STkzGa8bK6QcPpj7JrT4a6J",
-      category: "Text-to-Image",
-      Icon: WallpaperIcon
-  },  
   {
       name: "DALL-E Mini",
       path: "/p/QmQBzUpwF21ynVfSU3tW2WiWZfNjS94aq58verfghDLpWV",

1576115a87d7ee21cc5cbb1a8c2e6ffb1e5b4ece
Thomas Haferlach <t.haferlach@gmail.com>
2021-08-20 12:12:28 +0200

    Update Model.js

--- app/src/pages/Model.js
+++ app/src/pages/Model.js
@@ -54,7 +54,7 @@ export default React.memo(function Model() {
        {/* Nav Bar */}
        <NotebookSelector {...state} />   
        <Alert severity="warning">
-          Google recently significantly reduced the memory available to free Cloud GPUs. Pollinations depends on these to do the computations. 
+          Google recently reduced the capacity of their free Cloud GPUs. Pollinations depends on these to do the computations. 
         <br/> <br/>
         Image generation may fail intermittently. We are working on a fix. Discussion on <Link href="https://github.com/pollinations/pollinations/issues/62">Github</Link>.
         <br /><br/>

4d8ee20fe828e474a8b26d545e7c58ffd2fa6040
Thomas Haferlach <t.haferlach@gmail.com>
2021-08-20 17:10:47 +0200

    Change notebook order

--- app/src/data/notebooks.js
+++ app/src/data/notebooks.js
@@ -2,17 +2,17 @@ import WallpaperIcon from '@material-ui/icons/Wallpaper';
 
 export const notebooks = [
   {
-    name: "CLIP-guided Diffusion",
-    path: "/p/QmTBUAGsqWzJsF1Ccuzk9W5STkzGa8bK6QcPpj7JrT4a6J",
-    category: "Text-to-Image",
-    Icon: WallpaperIcon
- },  
-  {
     name: "CLIP-guided VQGan HQ",
     path: "/p/QmXd1XRWWky6EJ4tQTsiUVy3g9AtiQskcYy6EEADAE2Cf7",
     category: "Text-to-Image",
     Icon: WallpaperIcon
   },
+  {
+    name: "CLIP-guided Diffusion",
+    path: "/p/QmTBUAGsqWzJsF1Ccuzk9W5STkzGa8bK6QcPpj7JrT4a6J",
+    category: "Text-to-Image",
+    Icon: WallpaperIcon
+ },  
   {
       name: "DALL-E Mini",
       path: "/p/QmQBzUpwF21ynVfSU3tW2WiWZfNjS94aq58verfghDLpWV",

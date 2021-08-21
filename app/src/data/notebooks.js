b4556ba2b8a695d25ca8d30bd6843ead4ae89252
Thomas Haferlach <t.haferlach@gmail.com>
2021-08-21 15:55:27 +0200

    Replace VQGan with new version that does 384x256 and then upscales 4x

--- app/src/data/notebooks.js
+++ app/src/data/notebooks.js
@@ -2,17 +2,17 @@ import WallpaperIcon from '@material-ui/icons/Wallpaper';
 
 export const notebooks = [
   {
-    name: "CLIP-guided VQGan HQ",
-    path: "/p/QmPDQbkn8QR5EmqGyQ2urn9r4eLs6qqa1iWwMB9hvQSxBq",
-    category: "Text-to-Image",
-    Icon: WallpaperIcon
-  },
-  {
     name: "CLIP-guided Diffusion",
     path: "/p/QmTBUAGsqWzJsF1Ccuzk9W5STkzGa8bK6QcPpj7JrT4a6J",
     category: "Text-to-Image",
     Icon: WallpaperIcon
- },  
+ }, 
+  {
+    name: "CLIP-guided VQGan HQ",
+    path: "/p/QmQZE8bPGSLcJp2hvEcSj6KEdBRjK3H8Yb2UZm7AcMv866",
+    category: "Text-to-Image",
+    Icon: WallpaperIcon
+  },
   {
       name: "DALL-E Mini",
       path: "/p/QmQBzUpwF21ynVfSU3tW2WiWZfNjS94aq58verfghDLpWV",

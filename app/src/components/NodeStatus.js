ad11e04f10a6646e0201b0c4f78ce0e41b685ede
voodoohop <t.haferlach@gmail.com>
2021-09-03 09:30:10 +0200

    Add code to parse and display smilie (#86)
    * Add code to parse and display smilie
    
    * Update NodeStatus.js

--- app/src/components/NodeStatus.js
+++ app/src/components/NodeStatus.js
@@ -1,32 +1,33 @@
 import { displayContentID } from "../network/utils";
 import { getIPNSURL, getWebURL } from "../network/ipfsConnector";
 import { Button, Link, ListItem as MuiListItem, Table, TableRow, TableBody, TableCell as MuiTableCell, withStyles, styled, List, Typography, Box} from "@material-ui/core"
+import Debug from "debug";
+
+const debug = Debug("NodeStatus");
+
 const colabURL = "https://colab.research.google.com/github/pollinations/pollinations/blob/dev/colabs/pollinator.ipynb";
 
-const TableCell = withStyles({
-    root: {
-      borderBottom: "none",
-      padding: "2px"
-    }
-  })(MuiTableCell);
+// Display the connection status to colab and currect IPFS hash
+export default ({ nodeID, contentID, ipfs }) => {
+    
+    const gpu = parseGPU(ipfs);
+    debug("parsed GPU", gpu)
 
-
-
-
-export default ({ nodeID, contentID, status, ipfs }) => {
-    const colabState = ipfs?.output?.status;
+    const gpuInfo = gpu && `${gpu} ${gpuSmilie[gpu]}`;
+    
     return <Box style={{width:"220px", marginLeft:"auto"}}>
         <Table size="small" aria-label="a dense table" >
                     <TableBody>
                         <TableRow>
-                            <TableCell><b>NodeID</b></TableCell>
-                            <TableCell>{nodeID ? displayContentID(nodeID) : <ColabConnectButton />}</TableCell>
+                            <TableCell><b>Node</b></TableCell>
+                            <TableCell>{ nodeID ? gpuInfo || displayContentID(nodeID) : <ColabConnectButton />}</TableCell>
                         </TableRow>
                         <TableRow>
                         <TableCell ><b>ContentID</b></TableCell>
                             <TableCell>{contentID ?
                                 <Link
-                                    href={getWebURL(contentID)} children={displayContentID(contentID)}
+                                    href={getWebURL(contentID)} 
+                                    children={displayContentID(contentID)}
                                     target="_blank"
                                 />
                                 : <p children="Not connected..." />}
@@ -44,6 +45,26 @@ export default ({ nodeID, contentID, status, ipfs }) => {
 }
 
 
-function ColabConnectButton() {
-    return <Button color="secondary" href={colabURL} target="_blank">[ Launch ]</Button>
+const gpuSmilie = {
+    "Tesla T4" : "😴",
+    "Tesla K80" : "😐",
+    "Tesla P100" : "😀",
+    "Tesla V100" : "😍",
 }
+
+// extract GPU name from ipfs
+// the GPU was written to ipfs by running `nvidia-smi` on colab
+const parseGPU = (ipfs)  => 
+ ipfs?.output?.gpu?.replace(/\(.*\)/g, "")?.replace("GPU 0:", "")?.trim();
+
+
+const ColabConnectButton = () => <Button color="secondary" href={colabURL} target="_blank">[ Launch ]</Button>;
+
+
+const TableCell = withStyles({
+    root: {
+      borderBottom: "none",
+      padding: "2px"
+    }
+  })(MuiTableCell);
+

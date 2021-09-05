427106b3b1d96e51af936d97ed29e3754c8f7a81
Thomas Haferlach <t.haferlach@gmail.com>
2021-09-05 17:12:57 +0200

    Basic front page without images or description

--- app/src/data/notebooks.js
+++ app/src/data/notebooks.js
@@ -1,9 +1,7 @@
 import WallpaperIcon from '@material-ui/icons/Wallpaper';
-
-import Debug from 'debug';
+import { useEffect, useState } from 'react';
 import { ipfsLs, ipfsResolve } from '../network/ipfsConnector';
-
-import { getIPFSState } from '../network/ipfsState';
+import Debug from 'debug';
 
 const debug = Debug('notebooks');
 
@@ -31,30 +29,18 @@ export const getNotebooks = async (ipfsPath="/ipns/k51qzi5uqu5dhpj5q7ya9le4ru112
 }
 
 export const getDefaultNotebook = async () => (await getNotebooks())[0];
- 
-const notebooks = [
-  {
-    name: "CLIP-guided VQGan HQ",
-    path: "/p/QmPvWkYmLgHXGEhcrTy7BLj42PEMuZSVdf5K5JKdi8Ch3f",
-    category: "Text-to-Image",
-    Icon: WallpaperIcon
-  },
-  {
-    name: "PixelDraw",
-    path: "/p/QmZy8RzwsnfTvRsPZ9ifGchF2L317mienHsS4WDCP576rs",
-    category: "Text-to-Image",
-    Icon: WallpaperIcon
-  },
-  {
-    name: "CLIP-guided Diffusion",
-    path: "/p/QmTBUAGsqWzJsF1Ccuzk9W5STkzGa8bK6QcPpj7JrT4a6J",
-    category: "Text-to-Image",
-    Icon: WallpaperIcon
- }, 
-  {
-      name: "DALL-E Mini",
-      path: "/p/QmQBzUpwF21ynVfSU3tW2WiWZfNjS94aq58verfghDLpWV",
-      category: "Text-to-Image",
-      Icon: WallpaperIcon
-  }
-];
+
+export const useNotebooks = () => {
+  const [notebooks, setNotebooks] = useState([]);
+
+
+  useEffect(async () => {
+    const loadNotebooks = async () => {
+      const notebooks = await getNotebooks();
+      setNotebooks(notebooks);
+    };
+    loadNotebooks();
+  }, []);
+
+  return notebooks;
+}

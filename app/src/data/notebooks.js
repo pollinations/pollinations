5aa797d4778ed4ea360ea04402eceb005594e0b5
Thomas Haferlach <t.haferlach@gmail.com>
2021-09-05 18:28:10 +0200

    Format front page

--- app/src/data/notebooks.js
+++ app/src/data/notebooks.js
@@ -1,11 +1,15 @@
 import WallpaperIcon from '@material-ui/icons/Wallpaper';
 import { useEffect, useState } from 'react';
-import { ipfsLs, ipfsResolve } from '../network/ipfsConnector';
+import { ipfsGet, ipfsLs, ipfsResolve } from '../network/ipfsConnector';
 import Debug from 'debug';
+import { fetchAndMakeURL } from '../network/ipfsClient';
+import readMetadata from '../utils/notebookMetadata';
 
 const debug = Debug('notebooks');
 
 // get list of notebooks from IPNS path
+// this should be refactored once we cleaned the IFPS state code
+// no need to do raw ipfs operations or data mangling here
 export const getNotebooks = async (ipfsPath="/ipns/k51qzi5uqu5dhpj5q7ya9le4ru112fzlx9x1jk2k68069wmuy6gps5i4nc8888") => {
   const cid = await ipfsResolve(ipfsPath);
 
@@ -14,11 +18,20 @@ export const getNotebooks = async (ipfsPath="/ipns/k51qzi5uqu5dhpj5q7ya9le4ru112
   const notebooksPromise = Promise.all(notebookCategories.map(async ({name:category, cid:categoryCid}) => {
     const notebooks = await ipfsLs(categoryCid);
     debug('getNotebooks', category, notebooks);
-    return notebooks.map(({name,cid}) => ({
-      category, 
-      name, 
-      path:`/p/${cid}`, 
-      Icon: WallpaperIcon
+  
+    return await Promise.all(notebooks.map(async ({name,cid}) => {
+      debug("cid",cid);
+      const notebookJSON = await fetchAndMakeURL({ name:"notebook.ipynb", cid:`${cid}/input/notebook.ipynb`});
+      const {description} = readMetadata(notebookJSON)
+      // debug("notebookMetadata", description);
+      
+      return {
+        category, 
+        name, 
+        path:`/p/${cid}`, 
+        description,
+        Icon: WallpaperIcon
+      };
     }));
   }));
   const notebooks = (await notebooksPromise).flat();
@@ -43,4 +56,16 @@ export const useNotebooks = () => {
   }, []);
 
   return notebooks;
+}
+
+const getNotebookData = async ({name,cid}) => { 
+
+  const notebookJSON = ipfsGet(cid);
+  debug("notebookJSON", notebookJSON);
+  return {
+    category, 
+    name, 
+    path:`/p/${cid}`, 
+    Icon: WallpaperIcon
+  };
 }

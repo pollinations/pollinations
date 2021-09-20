6cd7ba2f6936d5f9ac883f61ffa6df6e9c3981d8
Thomas Haferlach <t.haferlach@gmail.com>
2021-09-20 20:48:02 +0200

    Delete autohashtag.sh

--- autohashtag.sh
+++
@@ -1,2 +0,0 @@
-#!/bin/bash
-curl -H "Authorization: Bearer 1SQJNTJ-KSZ41EF-N2HXXZN-BEDYDYC" -d '{"post": "the next time i found this walrus", "max": 3, "position": "auto"}' -X POST https://app.ayrshare.com/api/auto-hashtag

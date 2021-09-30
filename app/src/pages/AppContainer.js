95211361471b6074fc31b9810ef29730753ac5a0
Thomas Haferlach <t.haferlach@gmail.com>
2021-09-30 20:57:48 +0200

    Refactored page layout to higher order component

---
+++ app/src/pages/AppContainer.js
@@ -0,0 +1,27 @@
+import { Box, Link } from "@material-ui/core";
+import React from "react";
+import NotebookSelector from "../components/NotebookSelector";
+import useColab from "../network/useColab";
+import Debug from "debug";
+
+const debug = Debug("AppContainer");
+
+export const AppContainer = ({ Page }) => {
+
+    console.error("need to pass in whether to update hash somehow to useColab")
+    const { state, dispatch} = useColab(false);
+
+
+    return <>
+        {/* Nav Bar */}
+        <NotebookSelector {...state} />
+
+        {/* Children that get IPFS state */}
+        { <Page {...state} dispatchInput={dispatch} /> }
+
+        {/* Footer */}
+        <Box align="right" fontStyle="italic">
+            Discuss, get help and contribute on <Link href="https://github.com/pollinations/pollinations/discussions">[ Github ]</Link> or <Link href="https://discord.gg/jgH9y2p7" target="_blank">[ Discord ]</Link>.
+        </Box>
+    </>;
+}

ea2231600dfc1a112600d7542a42fb734a3d0631
voodoohop <t.haferlach@gmail.com>
2021-09-30 19:28:53 +0200

    Feature/new form input types (#111)
    * Add react dropzone
    
    * Update Form.js
    
    * Remove unused scripts
    
    * Remove unused notebooks
    
    * Add handling for enumerable form inputs
    
    * Refactor widget handling a little
    
    * Update Model.js

--- app/src/components/Form.js
+++ app/src/components/Form.js
@@ -1,10 +1,12 @@
-import React from "react";
+import React, { useCallback } from "react";
 import Form from "@rjsf/material-ui";
 import Button from '@material-ui/core/Button'
 import Debug from "debug";
 import { Box, Typography } from "@material-ui/core";
 import Alert from '@material-ui/lab/Alert';
 import HelpModal from "./HelpModal";
+import { useDropzone } from 'react-dropzone'
+import { CID } from "ipfs-http-client";
 
 const debug = Debug("Form");
 
@@ -31,7 +33,7 @@ const FormView = ({ input, status, colabState, metadata, nodeID, onSubmit, onCan
 
 
     debug("nodeID", nodeID, formDisabled)
-    const uiSchema = getUISchema(filledForm, metadata?.form?.properties, showSubmit)
+    const uiSchema = getUISchema(filledForm, showSubmit)
 
     debug("form uiSchema", uiSchema, filledForm, showSubmit)
 
@@ -41,6 +43,7 @@ const FormView = ({ input, status, colabState, metadata, nodeID, onSubmit, onCan
         onSubmit={({ formData }) => onSubmit(formData)}
         disabled={formDisabled || colabState === "running"}
     >
+        {/* <FileUpload /> */}
         <Box m={1}>
             {showSubmit ? <Button type="submit" disabled={formDisabled} >
                 [ {inProgress ? "Submitting..." : "Submit"} ]
@@ -75,45 +78,81 @@ const addSocialCheckbox = (filledFormNoSocial) =>
 
 
 // Get the form inputs from the ipfs object. Use the metadata to find default values
-function getFormInputs(ipfs, metadata) {
+function getFormInputs(ipfsInput, metadata) {
     if ((metadata === undefined) || (metadata === null)) return;
-    ipfs = ipfs || {};
+    ipfsInput = ipfsInput || {};
 
     const propertiesWithSocial = addSocialCheckbox(metadata.form.properties);
-
+    
     return Object.fromEntries(Object.entries(propertiesWithSocial).map(
-        ([formKey, prop]) => [formKey, formKey in ipfs ? { ...prop, "default": ipfs[formKey] } : prop]))
+        ([formKey, prop]) => [formKey, formKey in ipfsInput ? { ...prop,"enum": prop.enum, "default": ipfsInput[formKey] } : prop]))
 }
 
 
 // Get the ui schema for the form
 const getUISchema = (filledForm, enabled) => {
     debug("getUISchema", filledForm, enabled);
-    return Object.fromEntries(Object.keys(filledForm).map(key => [key, toSchema(key, filledForm[key].type, enabled)]))
+    return Object.fromEntries(Object.keys(filledForm).map(key => [key, toSchema(key, filledForm[key], enabled)]))
 };
 
 
 // Convert the form input type to the ui schema type
-const toSchema = (key, type, enabled) => {
-    const typeMappings = {
-        "boolean": "radio",
-        "string": "textarea",
-        "number": "updown",
-    };
-
-    const prefixMappings = {
-        "file_": "file",
-        "num_": "updown"
-    };
-
+const toSchema = (key, props, enabled) => {
     // TODO: enable prefixMappings
-
-    debug("Got type", type, "Looking for", key);
-
-    const widget = typeMappings[type] || "text";
+    const typeMappings = {
+        "boolean": () => "radio",
+        "string": mapStringType,
+        "number": () => "updown",
+    };
+    
     return {
-        "ui:widget": widget,
+        "ui:widget":  typeMappings[props.type](props),
         "ui_disabled": !enabled
     }
 
 }
+
+// Map the string type to the ui schema type
+// - Handles enumerable options differently
+// - If the default text has multiple lines it becomes a textarea. Otherwise text.
+const mapStringType = ({ default: defaultVal, enum: enumOptions }) => enumOptions ? "select" : textOrTextarea(defaultVal);
+
+
+const prefixMappings = {
+    "file_": "file",
+    "num_": "updown"
+};
+
+
+// If the text has multiple lines return textarea, otherwise text.
+function textOrTextarea(defaultVal) {
+    return (defaultVal.split("\n").length > 1 ? "textarea" : "text");
+}
+
+// File upload widget using dropzone
+function FileUpload() {
+    const onDrop = useCallback(async acceptedFiles => {
+      // Do something with the files
+      const _client = await client;
+      debug("dropped files", acceptedFiles);
+      const file = acceptedFiles[0];
+      const {cid} = await _client.add({content: file.stream(), path: file.path});
+
+      
+      
+    }, []);
+
+    const {getRootProps, getInputProps, isDragActive} = useDropzone({onDrop})
+  
+    return (
+      <div {...getRootProps()}>
+        <input {...getInputProps()} />
+        {
+          isDragActive ?
+            <p>Drop the files here ...</p> :
+            <p>Drag 'n' drop some files here, or click to select files</p>
+        }
+      </div>
+    )
+  }
+
--- pollinations/pollinations.py
+++
@@ -1,7 +0,0 @@
-import stomp
-
-conn = stomp.Connection([('b-4e89df1f-8226-4cc4-a518-4e1ac0023c97-1.mq.eu-central-1.amazonaws.com', 61614)], use_ssl=True)
-conn.connect('guest', 'iamcolabguest', wait=True)
-conn.send(body='hello', destination='/queue/test')
-conn.disconnect()
-

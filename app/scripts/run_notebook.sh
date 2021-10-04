3999c77488d40b7f90831dfc62a4cd227f9c09ed
Thomas Haferlach <t.haferlach@gmail.com>
2021-10-05 00:08:28 +0200

    Update run_notebook.sh

--- app/scripts/run_notebook.sh
+++ app/scripts/run_notebook.sh
@@ -21,7 +21,7 @@ for path in $IPFS_ROOT/input/*; do
     key=$(basename $path)
 
     # skip if file has extension
-    if [[ $key in *.* ]]; then
+    if [[ $key == *.* ]]; then
         continue
     fi
 

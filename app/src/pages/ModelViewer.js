b662c263826490ff56b1e2a0a7d5f6cc1306a2b4
ale <ale@ales-MacBook-Pro.local>
2021-11-05 11:56:38 +0100

    Pollen viewer

---
+++ app/src/pages/ModelViewer.js
@@ -0,0 +1,112 @@
+import { memo, useEffect, useState } from "react"
+
+import Box from '@material-ui/core/Box'
+import Typography from "@material-ui/core/Typography"
+
+// Components
+import { SEO } from "../components/Helmet"
+import useColab from "../network/useColab"
+import { getMedia } from "../data/media"
+import readMetadata from "../utils/notebookMetadata"
+
+export default memo(function ModelViewer() {
+  
+  const { state } = useColab()
+  let { ipfs, contentID } = state
+
+  const metadata = getNotebookMetadata(ipfs)
+
+  const [ images, setImages ] = useState([])
+  const [ first, setFirst ] = useState({ isVideo: false, filename: '', url: ''})
+
+  
+  useEffect(() => {
+    const images = getMedia(ipfs?.output)
+    if (!images || images.length === 0) return 
+    
+    // remove first image for large display
+    const firstImage = images.shift();
+
+    setImages( every_nth(images) )
+    setFirst({
+        isVideo: firstImage[0].toLowerCase().endsWith(".mp4"),
+        filename: firstImage[0],
+        url: firstImage[1]
+    })
+  },[ipfs.output])
+  
+  return <Box my={2}>
+      
+        <SEO metadata={metadata} ipfs={ipfs} cid={contentID}/>
+
+        <div style={styles.big}>
+
+            {   
+            // Big Preview
+            first.isVideo ?
+            <video {...video_props} alt={first.filename} src={first.url}/>
+            : <img alt={first.filename} src={first.url}/>
+            }
+
+            <div style={styles.query} children={ 
+            // Input Query
+            ipfs?.input?.text_input && true &&
+            <Typography 
+                variant="h5" 
+                component="h5" 
+                gutterBottom
+                children={`${ipfs?.input?.text_input}`}
+            /> }/>
+
+        </div>
+
+        <div style={styles.steps} children={   
+            // Steps Preview
+            images.map( ([filename, url]) => <img src={url} alt={filename} style={styles.img} /> )
+        }/>
+      </Box>
+})
+
+const video_props = {
+    muted: true,
+    autoPlay: true,
+    controls: true,
+    loop: true,
+    style: { width: 'calc(100vh - 90px)' }
+}
+
+// temporary
+const styles = {
+    big: {
+        width: '100%', 
+        display: 'flex', 
+        justifyContent: 'space-between',
+        alignItems: 'flex-end',
+        flexWrap: 'wrap',
+        marginBottom: '1em'
+    },
+    steps: {
+        width: '100%',
+        display: 'grid',
+        gap: '1em',
+        gridTemplateColumns: 'repeat(auto-fit, minmax(240px, 1fr))',
+        margin: '1em 0'
+    },
+    query: {
+        maxWidth: '20%',
+        minWidth: 200,
+    },
+    img: {
+        width: '100%'
+    }
+}
+
+// for backward compatibility we check if the notebook.ipynb is at / or at /input
+// the new "correct" way is to save the notebook.ipynb to /input
+
+const getNotebookMetadata = ipfs => readMetadata((ipfs?.input && ipfs.input["notebook.ipynb"]) || ipfs && ipfs["notebook.ipynb"]);
+
+function every_nth(array){
+    const nth = Math.max(1, Math.floor(array.length / 20))
+    return array.filter((e, i) => i % nth === nth - 1)
+}

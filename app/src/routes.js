751f31d6b5dd0dc185e2a8db2929544e164c255c
ale <ale@ales-MacBook-Pro.local>
2021-11-05 11:56:38 +0100

    Pollen viewer

--- app/src/routes.js
+++ app/src/routes.js
@@ -1,12 +1,18 @@
 import React from "react";
 import Model from "./pages/Model";
 import Home from "./pages/Home";
+import ModelViewer from "./pages/ModelViewer";
 
 export let getRoutes =  () => { 
     return [
         {
             exact: false,
             path: '/p/:hash*',
+            Page: ModelViewer,
+        },
+        {
+            exact: false,
+            path: '/c/:hash*',
             Page: Model,
         },
         { 

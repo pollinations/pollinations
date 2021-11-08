3bd178b515de012eac8c22587555b951094afda8
ale <ale@ales-MacBook-Pro.local>
2021-11-08 11:30:54 +0100

    cleanup NotebookSelector

--- app/src/components/NotebookSelector.js
+++ app/src/components/NotebookSelector.js
@@ -1,8 +1,6 @@
 import React, { useEffect, useState } from 'react';
 import Drawer from '@material-ui/core/Drawer';
-import Button from '@material-ui/core/Button';
 import List from '@material-ui/core/List';
-import Divider from '@material-ui/core/Divider';
 import ListItem from '@material-ui/core/ListItem';
 import ListItemIcon from '@material-ui/core/ListItemIcon';
 import ListItemText from '@material-ui/core/ListItemText';
@@ -13,50 +11,56 @@ import IconButton from '@material-ui/core/IconButton';
 import MenuIcon from '@material-ui/icons/Menu';
 
 import { getNotebooks } from "../data/notebooks.js";
-import NodeStatus from './NodeStatus';
 import { SocialLinks } from './Social';
 import { Box } from '@material-ui/core';
 import { Link } from 'react-router-dom'
 
 
 export default function NotebookSelector({children}) {
-    const [visible, setVisible] = useState(false);
-    
-    const [notebooks,setNotebooks] = useState([]);
-    useEffect(async () => setNotebooks(await getNotebooks()),[]);
 
-    return <>
-         <AppBar
-        position="static"
-      >
-        <Toolbar>
-          <IconButton
-            color="inherit"
-            aria-label="open drawer"
-            onClick={() => setVisible(true)}
-            edge="start"
-          >
-            <MenuIcon />
-          </IconButton>
-          <Typography variant="h6" noWrap>
-            <Link to="/">Pollinations</Link>
-          </Typography>
-          <Box marginLeft="15"> 
-            <SocialLinks />
-          </Box>
-         {children}
-        </Toolbar>
-      </AppBar>
-          <Drawer anchor={"top"} open={visible} onClose={() => setVisible(false)}>
-          <List>
-        {notebooks.map(({name, category, Icon, path}) => ( <Link key={path} onClick={() => setVisible(false)} to={path}>
-          <ListItem button key={name} component="a" >
+  const [visible, setVisible] = useState(false)
+  const [notebooks,setNotebooks] = useState([])
+  useEffect(async () => setNotebooks(await getNotebooks()),[])
+
+  return <>
+
+    <AppBar position="static">
+      <Toolbar>
+
+        <IconButton
+          color="inherit"
+          aria-label="open drawer"
+          onClick={() => setVisible(true)}
+          edge="start">
+
+          <MenuIcon fontSize="small" />
+
+        </IconButton>
+
+        <Typography variant="h6" noWrap>
+          <Link to="/">Pollinations</Link>
+        </Typography>
+
+        <Box marginLeft="15"> 
+          <SocialLinks />
+        </Box>
+
+        {children}
+
+      </Toolbar>
+    </AppBar>
+
+    <Drawer anchor={"top"} open={visible} onClose={() => setVisible(false)}>
+      <List>
+        {notebooks.map(({name, category, Icon, path}) => ( 
+          <Link key={path} onClick={() => setVisible(false)} to={path}>
+            <ListItem button key={name} component="a" >
             <ListItemIcon> <Icon /> </ListItemIcon>
             <ListItemText primary={`${category} - ${name}`} />
-          </ListItem>
+            </ListItem>
           </Link>
         ))}
       </List>
-          </Drawer>
-        </>
+    </Drawer>
+  </>
 }

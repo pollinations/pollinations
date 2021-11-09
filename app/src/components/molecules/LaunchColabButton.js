053153fe672590a3d5498b32f7ea4423e4ff445d
ale <ale@ales-MBP.fritz.box>
2021-11-10 00:51:33 +0100

    Toolbar as drawer

--- app/src/components/molecules/LaunchColabButton.js
+++ app/src/components/molecules/LaunchColabButton.js
@@ -18,7 +18,7 @@ const LaunchColabButton = ({ connected }) => {
         href={colabURL} 
         target="colab">
         {loading ? 'Launching...' : '[ Launch Colab ]'}
-    </Button> : <></>
+    </Button> : <Button disabled children='Connected to Colab'/>
 }
 
 export default LaunchColabButton

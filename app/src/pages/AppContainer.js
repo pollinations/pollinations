75623d513a41770db622e9d4593df84afeff26f3
Thomas Haferlach <t.haferlach@gmail.com>
2021-11-11 10:37:42 +0100

    Simplify showing node

--- app/src/pages/AppContainer.js
+++ app/src/pages/AppContainer.js
@@ -6,7 +6,6 @@ import Creator from "./Create";
 import Home from "./Home";
 import { BrowserRouter } from "react-router-dom";
 import React, { cloneElement } from "react";
-import NodeViewer from "./NodeViewer";
 import Footer from "../components/footer";
 import AppBar from "../components/AppBar";
 import Debug from "debug";
@@ -26,7 +25,7 @@ export const AppContainer = () => {
         {/* Children that get IPFS state */}
         <Container maxWidth="md" >
             <Routes>
-                <Route path='n/:nodeID' element={<NodeViewer {...node} />} />
+                <Route path='n/:nodeID' element={<NodeWithData { ...node } />} />
                 <Route path='p/:contentID/*' element={<ModelRoutes node={node} />} />
                 <Route path='/*' element={<HomeWithData />} />
             </Routes>
@@ -41,21 +40,30 @@ const HomeWithData =() => {
     const params = useParams();
     const path = params["*"];
     const ipfs = useIPFS("/ipns/k51qzi5uqu5dhpj5q7ya9le4ru112fzlx9x1jk2k68069wmuy6gps5i4nc8888" + path);
+    
     debug("home ipfs",ipfs);
+    
     return <Home ipfs={ipfs} />;
 };
+
+const NodeWithData = ({ contentID }) => {
+    // TODO: we shouldn't be calling useIPFS twice (here and in ResultViewer.js)
+    const ipfs = useIPFS(contentID);
+    
+    useNavigateToResultsWhenDone(ipfs);
+
+    return <ResultViewer ipfs={ipfs} />;
+}
 
 const ModelRoutes = ({node}) => {
     const { contentID } = useParams();
     const ipfs = useIPFS(contentID);
-    const metadata ={};// getMetadata(ipfs);
     return (
         <Routes>
             <Route index element={<Navigate replace to="view" />} />
-            <Route path='view' element={<ResultViewer ipfs={ipfs} metadata={metadata} />} />
-            <Route path='create' element={<Creator ipfs={ipfs} metadata={metadata} node={node} />} />
+            <Route path='view' element={<ResultViewer ipfs={ipfs} />} />
+            <Route path='create' element={<Creator ipfs={ipfs} node={node} />} />
         </Routes>
-       
     );
 }
 
@@ -64,3 +72,19 @@ const More = () => <div style={{margin: '1em auto 4em auto'}}>
   <Link href="https://github.com/pollinations/pollinations/discussions" target="_blank"> [ Github ] </Link>  
   or <Link href="https://discord.gg/XXd99CrkCr" target="_blank">[ Discord ]</Link>.
 </div>
+
+
+
+// Navigate to viewing page with hash when done
+function useNavigateToResultsWhenDone(ipfs) {
+    const navigate = useNavigate();
+
+    useEffect(() => {
+
+        if (!ipfs?.output?.done)
+            return;
+
+        navigate(`/p/${ipfs[".cid"]}`);
+
+    }, [ipfs?.output?.done]);
+}

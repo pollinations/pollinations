a1d1f290652e81c39c496a1455832e38f5821ca2
Thomas Haferlach <t.haferlach@gmail.com>
2021-11-15 21:09:37 +0100

    Allow publishing social post and pinning from cli

--- app/src/backend/pinning-cli.js
+++ app/src/backend/pinning-cli.js
@@ -1,14 +1,25 @@
 import { writer } from "../network/ipfsConnector";
+import { publisher } from "../network/ipfsPubSub";
 import { receive } from "./ipfs/receiver";
 
+if (process.argv[2]) {
+    const { publish, close } = publisher("done_pollent","");
+    async function run() {
+        await publish(process.argv[2]);
+        close();
+    }
+    run();
+} else {
 
-(async () => {
-    const { pin } = await writer();
-    receive({
-        ipns: true,
-        nodeid: "done_pollen",
-    }, async cid => {
-        console.log("pinning result", await pin(cid));
-    }, "");
-    console.log("listening to publish of inseminated topic and pinning")
-})();
+    (async () => {
+        const { pin } = await writer();
+        receive({
+            ipns: true,
+            nodeid: "done_pollen",
+        }, async cid => {
+            console.log("pinning result", await pin(cid));
+        }, "");
+        console.log("listening to publish of inseminated topic and pinning")
+    })();
+
+}

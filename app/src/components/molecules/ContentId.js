99896e2a656ab029709bea09a47773a9417824b3
ale <ale@ales-MBP.fritz.box>
2021-11-16 23:28:51 +0100

    ContentId Link

--- app/src/components/molecules/ContentId.js
+++ app/src/components/molecules/ContentId.js
@@ -5,7 +5,7 @@ import { displayContentID } from "../../network/utils"
 const ContentIdInfo = ({ contentID, connected }) => {
     if (!connected) return <></>
     if (!contentID) return <Typography> ContentId: N/A </Typography>
-    return <Typography > ContentId: <Link to="/n" children={displayContentID(contentID)}/> </Typography>
+    return <Typography > ContentId: <Link to={`/p/${contentID}`} children={displayContentID(contentID)}/> </Typography>
 }
 
 export default ContentIdInfo

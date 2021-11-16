5559700ca1eda0d988ea8f8579ca0c7dd717fc80
Thomas Haferlach <t.haferlach@gmail.com>
2021-11-16 17:47:17 +0100

    Fix imports

--- app/src/App.js
+++ app/src/App.js
@@ -1,7 +1,5 @@
 import React, {useEffect, useState} from 'react'
 
-
-import { BrowserRouter as Router, Switch, Route } from 'react-router-dom'
 import { AppContainer } from './pages/AppContainer';
 
 

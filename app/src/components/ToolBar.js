22ae602caa8732d012c9940e0cdfa5e2d6294901
Thomas Haferlach <t.haferlach@gmail.com>
2021-11-17 14:06:24 +0100

    Some hacker status info

--- app/src/components/ToolBar.js
+++ app/src/components/ToolBar.js
@@ -2,11 +2,13 @@ import GpuInfo from "./molecules/GpuInfo"
 import ContentIdInfo from "./molecules/ContentId"
 import ToolBarContainer from "./organisms/Toolbar"
 import Instructions from "./molecules/Instructions"
+import NodeIdInfo from "./molecules/NodeId"
 
 const ToolBar = ({ ...node }) => <ToolBarContainer node={node}>
     {
         node?.connected ?  <>
             <GpuInfo {...node} />
+            <NodeIdInfo {...node} />
             <ContentIdInfo {...node} />
         </> 
         : <Instructions />

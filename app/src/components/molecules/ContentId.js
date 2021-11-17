26d556fd7cfcea8c17705e6ab87c29fdf3310245
Thomas Haferlach <t.haferlach@gmail.com>
2021-11-17 14:33:07 +0100

    Change link of contentid to ipfs

--- app/src/components/molecules/ContentId.js
+++ app/src/components/molecules/ContentId.js
@@ -1,11 +1,15 @@
-import { Typography } from "@material-ui/core"
-import { Link } from "react-router-dom"
+import { Link, Typography } from "@material-ui/core"
 import { displayContentID } from "../../network/utils"
 
 const ContentIdInfo = ({ contentID, connected }) => {
     if (!connected) return <></>
     if (!contentID) return <Typography> ContentId: N/A </Typography>
-    return <Typography > ContentId: <Link to={`/p/${contentID}`} children={displayContentID(contentID)}/> </Typography>
+    return <Typography >
+                ContentId: 
+                <Link href={`https://polliantions.ai/ipfs/${contentID}`}>
+                    { displayContentID(contentID) }
+                </Link>
+            </Typography>
 }
 
 export default ContentIdInfo

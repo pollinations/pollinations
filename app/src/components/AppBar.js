ea16cc5caa4e3a8252ba34f45db4438784b8a5e2
Thomas Haferlach <t.haferlach@gmail.com>
2021-11-21 17:29:31 +0100

    Add notification and link to old pollinations

--- app/src/components/AppBar.js
+++ app/src/components/AppBar.js
@@ -3,6 +3,9 @@ import AppBarMaterial from '@material-ui/core/AppBar';
 import Toolbar from '@material-ui/core/Toolbar';
 import Typography from '@material-ui/core/Typography';
 import { SocialLinks } from './Social';
+
+import Alert from '@material-ui/lab/Alert';
+import { Link as MaterialLink } from '@material-ui/core';
 import { Link } from 'react-router-dom'
 
 
@@ -20,5 +23,6 @@ export default function AppBar({ children }) {
           <SocialLinks />
       </Toolbar>
     </AppBarMaterial>
+    <Alert severity="info">This is the <b>new experimental</b> frontend to Pollinations.AI. To visit the old one go to <MaterialLink href="https://old.pollinations.ai">https://old.pollinations.ai</MaterialLink> </Alert>
   </>
 }

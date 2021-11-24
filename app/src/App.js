57b3f3e38ca460e8f5e79d8598a5a4ae4e85bffd
voodoohop <t.haferlach@gmail.com>
2021-11-25 00:59:22 +0100

    Feed (#186)
    * override node if from params again
    
    * publish for feed
    
    * Pollinations Feed
    
    * Disable node from url again again
    
    * Change typography

--- app/src/App.js
+++ app/src/App.js
@@ -1,21 +1,21 @@
-import { Routes, Route, useParams, Navigate, useNavigate } from "react-router"
-import { BrowserRouter } from "react-router-dom"
-import Debug from "debug"
-
-import useColabNode from "./hooks/useColabNode"
-import useIPFS from "./hooks/useIPFS"
-
 import Container from "@material-ui/core/Container"
 import Link from '@material-ui/core/Link'
-
-import ToolBar from "./components/ToolBar"
+import Debug from "debug"
+import { useCallback, useEffect } from "react"
+import { Navigate, Route, Routes, useNavigate, useParams } from "react-router"
+import { BrowserRouter } from "react-router-dom"
 import AppBar from "./components/AppBar"
-
-import ResultViewer from "./pages/ResultViewer"
+import ToolBar from "./components/ToolBar"
+import useColabNode from "./hooks/useColabNode"
+import useIPFS from "./hooks/useIPFS"
 import Creator from "./pages/Create"
+import Feed from "./pages/Feed"
 import Home from "./pages/Home"
-import { useEffect, useState } from "react"
-import { useCallback } from "react"
+import ResultViewer from "./pages/ResultViewer"
+
+
+
+
 
 
 const debug = Debug("AppContainer");
@@ -27,9 +27,9 @@ const App = () => (
 )
 
 const Pollinations = () => {
-    const {node, overrideContentID, overrideNodeID} = useColabNode();
+    const { node, overrideContentID, overrideNodeID } = useColabNode();
     debug("got colab node info", node);
-    
+
     const navigate = useNavigate()
 
     const navigateToNode = useCallback((contentID) => {
@@ -39,45 +39,47 @@ const Pollinations = () => {
             navigate(`/n/${node.nodeID}`)
         else {
             history.go(0)
-            console.error("For some reason NodeID is not set...",node)
+            console.error("For some reason NodeID is not set...", node)
         }
     }, [node?.nodeID])
 
-    return (<>   
+    return (<>
         {/* Nav Bar     */}
-        <AppBar/>
+        <AppBar />
         {/* Children that get IPFS state */}
         <Container maxWidth="md" >
             <Routes>
                 <Route path='n/:nodeID' element={<NodeWithData node={node} overrideNodeID={overrideNodeID} />} />
                 <Route path='p/:contentID/*' element={<ModelRoutes node={node} navigateToNode={navigateToNode} />} />
                 <Route path='c/:selected' element={<HomeWithData />} />
+                <Route path='feed' element={<Feed />} />
                 <Route index element={<Navigate replace to="c/Anything" />} />
             </Routes>
-            <More/>
+            <More />
         </Container>
 
         <ToolBar node={node} showNode={navigateToNode} />
     </>)
 }
 
-const HomeWithData =() => {
-    const ipfs = useIPFS("/ipns/k51qzi5uqu5dhpj5q7ya9le4ru112fzlx9x1jk2k68069wmuy6gps5i4nc8888" );
+const HomeWithData = () => {
+    const ipfs = useIPFS("/ipns/k51qzi5uqu5dhpj5q7ya9le4ru112fzlx9x1jk2k68069wmuy6gps5i4nc8888");
 
-    debug("home ipfs",ipfs);
-    
+    debug("home ipfs", ipfs);
+
     return <Home ipfs={ipfs} />
 }
 
 const NodeWithData = ({ node, overrideNodeID }) => {
     const ipfs = useIPFS(node.contentID);
-    // const { nodeID } = useParams();
+    const { nodeID } = useParams();
     // useEffect(() => {
-    //     overrideNodeID(nodeID);
+    //     if (nodeID)
+    //         overrideNodeID(nodeID)
     // }, [nodeID])
 
-    if (ipfs?.output?.done) return <Navigate to={`/p/${ipfs[".cid"]}`}/>
-    
+    if (ipfs?.output?.done) return <Navigate to={`/p/${ipfs[".cid"]}`} />
+
     return <ResultViewer ipfs={ipfs} />
 }
 
@@ -95,10 +97,10 @@ const ModelRoutes = ({ node, navigateToNode }) => {
     )
 }
 
-const More = () => <div style={{margin: '1em auto 4em auto'}}>
-  Discuss, get help and contribute on 
-  <Link href="https://github.com/pollinations/pollinations/discussions" target="_blank"> [ Github ] </Link>  
-  or <Link href="https://discord.gg/XXd99CrkCr" target="_blank">[ Discord ]</Link>.
+const More = () => <div style={{ margin: '1em auto 4em auto' }}>
+    Discuss, get help and contribute on
+    <Link href="https://github.com/pollinations/pollinations/discussions" target="_blank"> [ Github ] </Link>
+    or <Link href="https://discord.gg/XXd99CrkCr" target="_blank">[ Discord ]</Link>.
 </div>
 
 export default App;

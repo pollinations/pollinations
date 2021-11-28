bd1f0d47118dd63eff4efd5302b3f404097d6049
voodoohop <t.haferlach@gmail.com>
2021-11-28 19:46:09 +0100

    Link style

--- app/src/components/molecules/NodeId.js
+++ app/src/components/molecules/NodeId.js
@@ -1,11 +1,15 @@
-import { Typography } from "@material-ui/core"
+import { Link as MaterialLink, Typography } from "@material-ui/core"
 import { Link } from "react-router-dom"
 import { displayContentID } from "../../network/utils"
 
 const NodeIdInfo = ({ nodeID, connected }) => {
     if (!connected) return <></>
     if (!nodeID) return <Typography> NodeID: N/A </Typography>
-    return <Typography > NodeID: <Link to={`/n/${nodeID}`} children={displayContentID(nodeID)}/> </Typography>
+    return <Typography > NodeID:
+        <MaterialLink compponent={Link} to={`/n/${nodeID}`}>
+            {displayContentID(nodeID)}
+        </MaterialLink>
+    </Typography>
 }
 
 export default NodeIdInfo;

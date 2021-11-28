9a95d894905cfa6292a46bb0a5e15a23ae400cbe
voodoohop <t.haferlach@gmail.com>
2021-11-28 19:29:19 +0100

    Change pollinations link component

--- app/src/components/AppBar.js
+++ app/src/components/AppBar.js
@@ -1,12 +1,12 @@
-import React, { useEffect, useState } from 'react';
-import AppBarMaterial from '@material-ui/core/AppBar';
-import Toolbar from '@material-ui/core/Toolbar';
-import Typography from '@material-ui/core/Typography';
-import { SocialLinks } from './Social';
-
-import Alert from '@material-ui/lab/Alert';
-import { Link as MaterialLink } from '@material-ui/core';
+import { Link as MaterialLink } from '@material-ui/core'
+import AppBarMaterial from '@material-ui/core/AppBar'
+import Toolbar from '@material-ui/core/Toolbar'
+import Typography from '@material-ui/core/Typography'
+import Alert from '@material-ui/lab/Alert'
+import React from 'react'
 import { Link } from 'react-router-dom'
+import { SocialLinks } from './Social'
+
 
 
 export default function AppBar({ children }) {
@@ -16,11 +16,11 @@ export default function AppBar({ children }) {
     <AppBarMaterial position="static">
       <Toolbar variant="dense">
 
-          <Typography variant="h6" noWrap>
-            <Link to="/">Pollinations</Link>
-          </Typography>
+        <Typography variant="h6" noWrap>
+          <MaterialLink component={Link} to="/">Pollinations</MaterialLink>
+        </Typography>
 
-          <SocialLinks />
+        <SocialLinks />
       </Toolbar>
     </AppBarMaterial>
     <Alert severity="info">This is the <b>new experimental</b> frontend to Pollinations.AI. To visit the old one go to <MaterialLink href="https://old.pollinations.ai">https://old.pollinations.ai</MaterialLink> </Alert>

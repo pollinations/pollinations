f7b1884ab15f9bd2832edf99bc36a3f6d41fa007
Thomas Haferlach <t.haferlach@gmail.com>
2021-11-28 23:08:33 +0100

    Create RouterLink molecule

--- app/src/components/AppBar.js
+++ app/src/components/AppBar.js
@@ -1,28 +1,29 @@
-import { Link as MaterialLink } from '@material-ui/core'
+import { Link } from '@material-ui/core'
 import AppBarMaterial from '@material-ui/core/AppBar'
 import Toolbar from '@material-ui/core/Toolbar'
 import Typography from '@material-ui/core/Typography'
 import Alert from '@material-ui/lab/Alert'
 import React from 'react'
-import { Link } from 'react-router-dom'
+import RouterLink from './molecules/RouterLink'
 import { SocialLinks } from './Social'
-
-
 
 export default function AppBar({ children }) {
 
   return <>
-
     <AppBarMaterial position="static">
       <Toolbar variant="dense">
 
         <Typography variant="h6" noWrap>
-          <MaterialLink component={Link} to="/">Pollinations</MaterialLink>
+          <RouterLink to={"/"}>
+            Pollinations
+          </RouterLink>
         </Typography>
 
         <SocialLinks />
       </Toolbar>
     </AppBarMaterial>
-    <Alert severity="info">This is the <b>new experimental</b> frontend to Pollinations.AI. To visit the old one go to <MaterialLink href="https://old.pollinations.ai">https://old.pollinations.ai</MaterialLink> </Alert>
+    <Alert severity="info">This is the <b>new experimental</b> frontend to Pollinations.AI. To visit the old one go to <Link href="https://old.pollinations.ai">https://old.pollinations.ai</Link> </Alert>
   </>
 }
+
+

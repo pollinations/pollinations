9f80c4aa2c942ca34dc4748057b9b04df78c21cf
Thomas Haferlach <t.haferlach@gmail.com>
2021-11-29 02:33:53 +0100

    Remove unnecessary async unctions

--- app/src/hooks/useIPFSWrite.js
+++ app/src/hooks/useIPFSWrite.js
@@ -2,37 +2,45 @@
  
 import {useCallback, useEffect, useMemo, useReducer, useState} from "react";
 import { updateInput, getWriter } from "../network/ipfsWebClient";
+import {usePrevious} from 'react-use';
 
 import Debug from "debug";
+import { noop } from "../network/utils";
 
 const debug = Debug("useIPFSWrite");
 
 export default (ipfs, node) => {
 
     const { publish } = node;
-    
-    const cid = ipfs && ipfs[".cid"];
+  
+    const [writer, setWriter] = useState(noop);
 
-    const writer = useMemo(() => {
-        if (!cid)
-            return;
-        return getWriter(ipfs);
-    }, [cid]);
 
-    debug("inputCID", cid, writer);
+
 
     const dispatch = useCallback(async inputState => {
+          
+        const cid = ipfs && ipfs[".cid"];
+
+        debug("inputCID", cid);
         
-        debug("dispatching", ipfs, writer)
-        const newContentID = await updateInput(await writer, inputState);
+        if (!cid)
+            return;
+
+        const writer = getWriter(ipfs);
+        debug("dispatching", ipfs)
+        const newContentID = await updateInput(writer, inputState);
 
         debug("added input", inputState, "got cid", newContentID, "to state");
 
         publish(newContentID);
+        
+        await writer.close();
 
         return newContentID;
         
     }, [publish, writer, ipfs]);
+
 
     return dispatch;
 }

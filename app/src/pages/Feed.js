b3dc0650394ebe6144543643c93a1c6e8e65a502
voodoohop <t.haferlach@gmail.com>
2021-12-09 22:26:46 +0100

    UI Update #2 + Feed (#197)
    * Fix social post typo
    
    * Reduce social networks
    
    * new design
    
    * Fix ipns hash
    
    * new design changes
    
    * design changes
    
    * design changes
    
    * bit of cleanup
    
    * Parallel file adding
    
    * Remove alert
    
    * Delete output earlier
    
    * Remove gradient
    
    * Change message to processing_pollen
    
    * better retry logging
    
    * Fix initializedFolder called multiple times
    
    * dont do parallel stuff
    
    * Try to ix syncing
    
    * close sender more conservatively
    
    * parallelize again to avoid ipfs sync being slower than files added
    
    * Directory writing handler
    
    * Some logging and http client upgrade
    
    * Better subscription behaviour
    
    * Refactor layout slightly
    
    * Bot to discord
    
    * typo
    
    * Finally fixed initial state problem
    
    * full width image card
    
    * feed page
    
    * bugfix
    
    * Merge branch 'seedling' of https://github.com/pollinations/pollinations into seedling
    
    * remove big image from mediaviewer.js
    
    Co-authored-by: ale-rls <85608868+ale-rls@users.noreply.github.com>

--- app/src/pages/Feed.js
+++ app/src/pages/Feed.js
@@ -1,8 +1,16 @@
-import { useEffect, useState } from "react"
+import { useEffect, useMemo, useState } from "react"
 import useIPFS from "../hooks/useIPFS"
 import { subscribeCID } from "../network/ipfsPubSub"
 import ResultViewer from "./ResultViewer"
 import Typography from "@material-ui/core/Typography"
+import { getNotebookMetadata } from "../utils/notebookMetadata"
+import { mediaToDisplay } from "../data/media"
+import { Box, Button, Link } from "@material-ui/core"
+import { SEO } from "../components/Helmet"
+import NotebookTitle from "../components/NotebookTitle"
+import { NotebookProgress } from "../components/NotebookProgress"
+import { IpfsLog } from "../components/Logs"
+import MediaViewer from "../components/MediaViewer"
 
 const Feed = () => {
     const [cid, setCid] = useState(null)
@@ -11,12 +19,56 @@ const Feed = () => {
 
     useEffect(() => subscribeCID("processing_pollen", "", setCid),[])
     
+    const contentID = ipfs[".cid"];
+  const metadata = getNotebookMetadata(ipfs);
+  
+  const primaryInputField = metadata?.primaryInput;
+  const primaryInput = ipfs?.input?.[primaryInputField];
+
+  const {images, first} = useMemo(() => {
+    return mediaToDisplay(ipfs.output);
+  }, [ipfs.output]);
+
     return <>
-        <Typography variant="h3" component="h3" gutterBottom>
-          🌸 Pollinations Feed
-        </Typography>
-        <ResultViewer ipfs={ipfs} />
+    <Box my={2} marginBottom='5em'>
+      
+      
+
+      <Box marginTop='2em' minWidth='100%' display='flex' 
+          justifyContent={!contentID ? 'center' : 'space-around'} alignItems='center' flexWrap='wrap'>
+
+          {   // Waiting Screen goes here
+              !contentID ?
+              <Box minHeight='70vh' alignItems='center' display='flex'>
+                <Typography>
+                    Connecting to Feed...
+                </Typography> 
+              </Box>  : <>
+
+                <BigPreview {...first}/>
+
+                <Box minWidth='200px' maxWidth='20%'>
+                    <Typography variant="h5" gutterBottom> 
+                        {primaryInput}
+                    </Typography>
+                </Box>
+
+              </>
+          }
+
+          
+      </Box>
+      
+    </Box>
+    
     </>
 }
 
 export default Feed
+
+const BigPreview = ({ isVideo, filename, url }) => isVideo ?
+    <video muted autoPlay controls loop alt={filename} src={url}
+    style={{ width: 'calc(100vh - 90px)' }}/>
+    : 
+    <img alt={filename} src={url} style={{height: '80vh'}} />
+

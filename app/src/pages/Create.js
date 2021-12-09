3369236630f088fbbad9145b686e60b584f91369
voodoohop <t.haferlach@gmail.com>
2021-12-09 19:37:02 +0100

    Finally fixed initial state problem

--- app/src/pages/Create.js
+++ app/src/pages/Create.js
@@ -1,43 +1,30 @@
-import React, { useCallback, useMemo } from "react"
-import Markdown from 'markdown-to-jsx'
+import Box from "@material-ui/core/Box";
+import Typography from "@material-ui/core/Typography";
+import Alert from '@material-ui/lab/Alert';
 import Debug from "debug";
-
-import { getNotebookMetadata } from "../utils/notebookMetadata"
-import useIPFSWrite from "../hooks/useIPFSWrite"
-
-import Typography from "@material-ui/core/Typography"
-import Box from "@material-ui/core/Box"
-import Alert from '@material-ui/lab/Alert'
-
-import FormView from '../components/Form'
-import MediaViewer from '../components/MediaViewer'
-import { SEO } from "../components/Helmet";
-import { SocialPostStatus } from "../components/Social";
+import Markdown from 'markdown-to-jsx';
+import React, { useCallback, useMemo } from "react";
 import Acordion from "../components/Acordion";
+import FormView from '../components/Form';
+import { SEO } from "../components/Helmet";
+import MediaViewer from '../components/MediaViewer';
 import NotebookTitle from "../components/NotebookTitle";
+import { getNotebookMetadata } from "../utils/notebookMetadata";
 
-const debug = Debug("Model");
 
-export default React.memo(function Create({ ipfs, node, onSubmit }) {
+
+
+const debug = Debug("Create");
+
+export default React.memo(function Create({ ipfs, node, dispatch }) {
 
   const contentID = ipfs[".cid"]
-  
-  const { connected } = node
 
-  const dispatchInput = useIPFSWrite(ipfs, node)
+  const { connected } = node
 
   const metadata = useMemo(() => getNotebookMetadata(ipfs), [ipfs?.input])
 
-  const dispatchForm = useCallback(async inputs => {
-    debug("dispatchForm", inputs);
-    const contentID = await dispatchInput({
-      ...(ipfs?.input || {}),
-      ...inputs
-    });
-    onSubmit(contentID);
-    debug("dispatched Form");
-
-  }, [ipfs?.input]);
+  debug("Create", { ipfs, node, metadata })
 
   const cancelForm = useCallback(() => dispatchInput({ ...ipfs.input, formAction: "cancel" }), [ipfs?.input]);
 
@@ -45,36 +32,35 @@ export default React.memo(function Create({ ipfs, node, onSubmit }) {
 
 
   return <>
-      <Box my={2}>
+    <Box my={2}>
 
-        <SEO metadata={metadata} ipfs={ipfs} cid={contentID}/>
-        {/* control panel */}
-        <NotebookTitle metadata={metadata} />
-        {/* just in case */}
-        <NotebookDescription metadata={metadata}/>
-        
+      <SEO metadata={metadata} ipfs={ipfs} cid={contentID} />
+      {/* control panel */}
+      <NotebookTitle metadata={metadata} />
+      {/* just in case */}
+      <NotebookDescription metadata={metadata} />
 
-        {/* inputs */}
-        <div style={{ width: '100%' }}>
-          {
-             !connected && <Alert severity="info">The inputs are <b>disabled</b> because <b>no Colab node is running</b>! Click on <b>LAUNCH</b> (bottom right) or refer to INSTRUCTIONS for further instructions.</Alert>
-          }
-          <FormView
-            input={ipfs?.input}
-            connected={connected}
-            metadata={metadata}
-            onSubmit={dispatchForm}
-            onCancel={cancelForm}
-          />
-        </div>
 
-        {/* previews */}
-        {ipfs.output && <div >
-          <MediaViewer output={ipfs.output} contentID={contentID} />
-        </div>
+      {/* inputs */}
+      <div style={{ width: '100%' }}>
+        {
+          !connected && <Alert severity="info">The inputs are <b>disabled</b> because <b>no Colab node is running</b>! Click on <b>LAUNCH</b> (bottom right) or refer to INSTRUCTIONS for further instructions.</Alert>
         }
+        <FormView
+          input={ipfs?.input}
+          connected={connected}
+          metadata={metadata}
+          onSubmit={dispatch}
+        />
+      </div>
 
-      </Box>
+      {/* previews */}
+      {ipfs.output && <div >
+        <MediaViewer output={ipfs.output} contentID={contentID} />
+      </div>
+      }
+
+    </Box>
   </>
 });
 
@@ -82,15 +68,15 @@ export default React.memo(function Create({ ipfs, node, onSubmit }) {
 
 // Notebook Description
 
-const NotebookDescription = ( { metadata } ) => {
+const NotebookDescription = ({ metadata }) => {
   if (metadata === null) return null
   return (
-  <Acordion visibleContent='Details'
-    hiddenContent={
-      <Typography color="textSecondary">
-        <Markdown children={metadata.description}/>
-      </Typography>}
-  />);
+    <Acordion visibleContent='Details'
+      hiddenContent={
+        <Typography color="textSecondary">
+          <Markdown children={metadata.description} />
+        </Typography>}
+    />);
 }
 
 

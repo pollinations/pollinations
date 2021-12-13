48a99c077b4ec39c6bf3e979c616844d2f088103
voodoohop <t.haferlach@gmail.com>
2021-12-13 15:38:04 +0100

    UI changes and double file submit bug resolution (#203)
    * fixed toolbar overflow
    
    * try deleting file before adding it (fixes bug on second file submission)
    
    * useClickOutside
    
    * useClickOutside
    
    * catch CID is falsy
    
    * error feedback
    
    * !notebooks && alert
    
    Co-authored-by: ale-rls <85608868+ale-rls@users.noreply.github.com>

---
+++ app/src/hooks/UI/useClickOutside.js
@@ -0,0 +1,26 @@
+import { useEffect, useRef } from "react"
+
+const useClickOutside = (ref, callback) => {
+    // reference callback otherwise it rerenders all the time as dependency in useEffect
+    // avoids using useCallback in the callback function
+    const callbackRef = useRef()
+    callbackRef.current = callback
+
+    useEffect(()=> {
+        // avoids passing the function as dependency
+        const handleClickOutside = e => {
+            if (!ref?.current?.contains(e.target) && callbackRef.current) { 
+             callbackRef.current(e)
+            }
+        }
+
+        document.addEventListener('click', handleClickOutside, true)
+
+        // cleanup
+        return () => {
+            document.removeEventListener('click', handleClickOutside, true)
+        }
+    },[ callbackRef, ref ])
+}
+
+export default useClickOutside

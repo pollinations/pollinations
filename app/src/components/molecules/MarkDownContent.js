c2a4a4b4353d64e786191edafbf3f8fca518ff6c
voodoohop <t.haferlach@gmail.com>
2021-12-18 03:15:04 +0100

    LocalPollen handle no output, functionalize markdown component

--- app/src/components/molecules/MarkDownContent.js
+++ app/src/components/molecules/MarkDownContent.js
@@ -1,32 +1,28 @@
 import Typography from "@material-ui/core/Typography"
 import Markdown from "markdown-to-jsx"
+import { range, zipObj } from "ramda"
 import useContent from "../../hooks/useContent"
 
 const MarkDownContent = ({ id }) => {
 
     let content = useContent(id)
 
-    return <Markdown options={{
-        overrides: {
-            h1: { component: H1 },
-            h2: { component: H2 },
-            h3: { component: H3 },
-            h4: { component: H4 },
-            h5: { component: H5 },
-            h6: { component: H6 },
-        }
-    }}>
+    const headersToInclude = range(1, 7)
+
+    // header tags
+    const tags = headersToInclude.map(i => `h${i}`)
+
+    // elements to override the header tags with
+    const overrideElements = headersToInclude.map(i =>
+        ({ children }) => <Typography variant={`h${i}`} children={children} />
+    )
+
+    const overrides = zipObj(tags, overrideElements)
+
+    return <Markdown options={{ overrides }}>
         {content}
     </Markdown>
 }
-
-// surprise
-const H1 = ({ children }) => <Typography variant='h1' children={children} />
-const H2 = ({ children }) => <Typography variant='h2' children={children} />
-const H3 = ({ children }) => <Typography variant='h3' children={children} />
-const H4 = ({ children }) => <Typography variant='h4' children={children} />
-const H5 = ({ children }) => <Typography variant='h5' children={children} />
-const H6 = ({ children }) => <Typography variant='h6' children={children} />
 
 
 

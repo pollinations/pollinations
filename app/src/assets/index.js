bf7b3c28a0c382986e933b0269982b6f53a54d5e
voodoohop <t.haferlach@gmail.com>
2021-12-18 14:25:03 +0100

    add an index to content and move a little logic out of useContent

---
+++ app/src/assets/index.js
@@ -0,0 +1,9 @@
+import about from './contents/about.md'
+import blank from './contents/blank.md'
+import help from './contents/help.md'
+import instructions from './contents/instructions.md'
+
+export const textContent = {
+    help, about, instructions, blank
+}
+

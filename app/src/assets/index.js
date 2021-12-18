59a09f1e155ab49c5f67cf17612e5b9d63244b06
voodoohop <t.haferlach@gmail.com>
2021-12-18 14:45:59 +0100

    Update index.js

--- app/src/assets/index.js
+++ app/src/assets/index.js
@@ -3,6 +3,7 @@ import blank from './text/blank.md'
 import failure from './text/failure.md'
 import help from './text/help.md'
 import instructions from './text/instructions.md'
+
 export const textContent = {
     help, about, instructions, blank, failure
 }

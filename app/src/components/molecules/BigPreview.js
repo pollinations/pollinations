0936de73d96dc086b62a933c876d9c82fd047724
Thomas Haferlach <t.haferlach@gmail.com>
2021-12-23 10:11:37 +0100

    Temporary isVideo check

--- app/src/components/molecules/BigPreview.js
+++ app/src/components/molecules/BigPreview.js
@@ -1,6 +1,7 @@
 
-const BigPreview = ({ isVideo, filename, url }) => {
+const BigPreview = ({ filename, url }) => {
 
+    const isVideo = filename.endsWith(".mp4") || filename.endsWith(".webm") || filename.endsWith(".ogv") || filename.endsWith(".mov") || filename.endswith(".avi")
     if (!url) return <div style={{minHeight:'80vh'}}/>
   
     if (!isVideo) return <img alt={filename} src={url} style={{height: '80vh'}} />

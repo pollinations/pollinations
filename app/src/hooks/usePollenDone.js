4d1565ee3a81873f5686ff6eac57575bbfb4ad9e
voodoohop <t.haferlach@gmail.com>
2022-01-02 12:11:32 +0100

    Fix small issues with localPollens + feedback that run is starting (#213)
    * Change handling of done state
    
    * Fix writing done false at start of run
    
    * Rename
    
    * Update App.js
    
    * Warming up message

---
+++ app/src/hooks/usePollenDone.js
@@ -0,0 +1,16 @@
+import { usePrevious } from "react-use"
+import { noop } from "../network/utils"
+
+
+export default (ipfs, callback=noop) => {
+
+    const done = ipfs?.output?.done
+    const previousDone = usePrevious(done)
+
+    if (previousDone === false && done === true) {
+        callback(ipfs)
+        return true
+    }
+
+    return false
+}

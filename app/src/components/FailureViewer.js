15b688656febb809e8c8cb1d11e1eba34aca85a5
voodoohop <t.haferlach@gmail.com>
2022-03-17 16:53:51 +0100

    Failure explanations (#256)
    * Add failure message for when no GPU is detected

--- app/src/components/FailureViewer.js
+++ app/src/components/FailureViewer.js
@@ -14,6 +14,7 @@ export const FailureViewer = ({ contentID, ipfs }) => {
 
 const failureHelpers = {
     outOfGPUMemory: log => log.includes("RuntimeError: CUDA out of memory.") ? outOfGPUMemoryText : null,
+    noGPU: log => log.includes("RuntimeError: No CUDA GPUs are available") ? noGPUText : null,
     generalException: log => {
         const lastLine =  last(log.split("\n").filter(line => line.trim() !== ""))
         if (lastLine.startsWith("Exception: "))
@@ -26,4 +27,9 @@ const failureHelpers = {
 const outOfGPUMemoryText = `##### GPU memory error
 
 The GPU ran out of memory. If you are generating images try reducing the resolution or the size of the input. Subscribing to *Google Colab Pro* could alleviate this problem.`
+
+
+const noGPUText = `##### No GPU found
+
+Pollinations.AI requires a GPU on Google Colab to run. It seems we cannot find the GPU. Try clicking *Runtime -> Change Runtime Type* and selecting GPU.`
 

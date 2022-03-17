1beb4f08ffc0d75dc06e49a4522cfba580af35be
Thomas Haferlach <t.haferlach@gmail.com>
2022-03-17 14:02:17 +0100

    Show general exception

--- app/src/components/FailureViewer.js
+++ app/src/components/FailureViewer.js
@@ -1,24 +1,30 @@
-import { values } from 'ramda'
+import { last, values } from 'ramda'
 import React from 'react'
 import MarkDownContent from './molecules/MarkDownContent'
 
 export const FailureViewer = ({ contentID, ipfs }) => {
 
     const log = ipfs?.output?.log
-
-    console.log("loooog",log)
     
-    const failureMessage = values(failureHelpers).reduce((failureString, {check, message}) => check(log) ? message : failureString, null)
+    const failureMessage = values(failureHelpers).reduce((failureString, getMessage) => getMessage(log) ? failureString+"\n"+getMessage(log) : failureString, "")
 
     return <><MarkDownContent id={"failure"} contentID={contentID} failureMessage={failureMessage}/></>
 }
 
 
 const failureHelpers = {
-    outOfGPUMemory: {
-        check:  (log) => log.includes("RuntimeError: CUDA out of memory."),
-        message: `##### GPU memory error
-
-The GPU ran out of memory. If you are generating images try reducing the resolution or the size of the input. Subscribing to *Google Colab Pro* could alleviate this problem.`
+    outOfGPUMemory: log => log.includes("RuntimeError: CUDA out of memory.") ? outOfGPUMemoryText : null,
+    generalException: log => {
+        const lastLine =  last(log.split("\n").filter(line => line.trim() !== ""))
+        console.log("lastLine", lastLine)
+        if (lastLine.startsWith("Exception: "))
+            return lastLine.replace("Exception: ", "#####")+"\n&nbsp;\n"
+        return null
     }
 }
+
+
+const outOfGPUMemoryText = `##### GPU memory error
+
+The GPU ran out of memory. If you are generating images try reducing the resolution or the size of the input. Subscribing to *Google Colab Pro* could alleviate this problem.`
+

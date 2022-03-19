4e02993de2255cd62d4774bce39d6d72dcb256aa
Thomas Haferlach <t.haferlach@gmail.com>
2022-03-19 13:06:45 +0100

    Add missing imports

--- app/src/components/organisms/LoggedUser.js
+++ app/src/components/organisms/LoggedUser.js
@@ -1,4 +1,5 @@
-import * as React from 'react'
+import { Avatar, Menu, MenuItem } from '@material-ui/core';
+import * as React from 'react';
 import { useNavigate } from "react-router-dom";
 import { useAuth } from "../../hooks/useAuth";
 

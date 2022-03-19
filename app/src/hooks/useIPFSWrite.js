c60717936678606b0e2fc5e2ba1cc7db63e1b5fc
Thomas Haferlach <t.haferlach@gmail.com>
2022-03-19 18:11:49 +0100

    Media viewer abstraction and cleaner writing to IPFS and localpollens use IPFS

--- app/src/hooks/useIPFSWrite.js
+++ app/src/hooks/useIPFSWrite.js
@@ -1,44 +1,17 @@
+import Debug from "debug"
+import { useMemo } from "react"
+import { getWriter } from "../network/ipfsWebClient"
 
 
-import Debug from "debug";
-import { useCallback } from "react";
-import { getWriter, updateInput } from "../network/ipfsWebClient";
+const debug = Debug("useIPFSWrite")
 
+export default (ipfs) => {
 
-const debug = Debug("useIPFSWrite");
+   
+    const writer = useMemo(() => {
+        const writer = getWriter(ipfs)
+        return writer
+    }, [ipfs])
 
-export default (ipfs, node) => {
-
-    const { publish } = node;
-
-    debug("publish", publish)
-
-
-
-    const dispatch = useCallback(async inputState => {
-
-        const cid = ipfs && ipfs[".cid"]
-
-        debug("inputCID", cid)
-
-        if (!cid)
-            return
-
-        const writer = getWriter(ipfs);
-        debug("dispatching", ipfs)
-        const newContentID = await updateInput(writer, { ...ipfs.input, ...inputState })
-
-        debug("added input", inputState, "got cid", newContentID, "to state")
-
-        debug("publishing with publish function", publish)
-        publish(newContentID)
-
-        await writer.close()
-
-        return newContentID;
-
-    }, [publish, ipfs])
-
-
-    return dispatch
+    return writer
 }

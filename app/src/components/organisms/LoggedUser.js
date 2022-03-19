364405bbb789e0bb164be830c7ba70264a5fbc6c
ale-rls <85608868+ale-rls@users.noreply.github.com>
2022-03-19 12:06:57 +0100

    cleanup

---
+++ app/src/components/organisms/LoggedUser.js
@@ -0,0 +1,38 @@
+import * as React from 'react'
+import { useNavigate } from "react-router-dom";
+import { useAuth } from "../../hooks/useAuth";
+
+
+
+
+const LoggedUser = ({ user }) => {
+    const { handleSignOut } = useAuth()
+    const navigate = useNavigate()
+    
+    const [anchorEl, setAnchorEl] = React.useState(null);
+    const open = Boolean(anchorEl);
+ 
+    return <>
+        <Avatar onClick={e => setAnchorEl(e.currentTarget)} src={user?.user_metadata?.avatar_url}/>
+        <Menu  anchorEl={anchorEl} open={open} onClose={() => setAnchorEl(null)} style={{ marginTop: '2em' }}>
+
+            <MenuItem onClick={() => {
+                setAnchorEl(null)
+                navigate("profile")
+            }} > Profile </MenuItem>
+
+            <MenuItem onClick={() => {
+                setAnchorEl(null)
+                navigate("localpollens")
+            }}> My Pollens </MenuItem>
+
+            <MenuItem onClick={() => {
+                setAnchorEl(null)
+                handleSignOut()
+            }}> Logout </MenuItem>
+
+      </Menu>
+    </>
+}
+
+export default LoggedUser

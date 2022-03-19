6cacb266688c46d2bc56fb6c1dac38b494c767c3
Thomas Haferlach <t.haferlach@gmail.com>
2022-03-19 20:16:24 +0100

    Use MediaViewer in BigPreview

--- app/src/components/molecules/BigPreview.js
+++ app/src/components/molecules/BigPreview.js
@@ -1,13 +1,11 @@
+import { MediaViewer } from "../MediaViewer"
 
-const BigPreview = ({ filename, url }) => {
+const BigPreview = ({ filename, url, type }) => {
 
-    const isVideo = filename && (filename.endsWith(".mp4") || filename.endsWith(".webm") || filename.endsWith(".ogv") || filename.endsWith(".mov") || filename.endsWith(".avi"))
-    if (!url) return <div style={{minHeight:'80vh'}}/>
   
-    if (!isVideo) return <img alt={filename} src={url} style={{height: '80vh'}} />
-    
-    return <video muted autoPlay controls loop alt={filename} src={url}
-    style={{ width: 'calc(100vh - 90px)', maxWidth: '100%' }}/>
+    if (!url) return <div style={{minHeight:'80vh'}}>Processing...</div>
+
+    return <MediaViewer filename={filename} content={url} type={type} style={{ width: 'calc(100vh - 90px)', maxWidth: '100%' }} />
 }
 
 export default BigPreview

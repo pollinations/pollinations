8ded7c7ba276d954852b325d4d3a20369b9e4e87
Thomas Haferlach <t.haferlach@gmail.com>
2022-03-25 00:41:35 +0100

    Revert "UI Updates + Fix text Viewer + IPFS rollback (#268)"
    This reverts commit d792052ffee1c80a962c4f4735d5c12be605e98e.

--- app/src/components/molecules/NodeId.js
+++ app/src/components/molecules/NodeId.js
@@ -1,12 +1,12 @@
 import { Typography } from "@material-ui/core"
+
 import { displayContentID } from "../../network/utils"
 import RouterLink from "./RouterLink"
-
 
 const NodeIdInfo = ({ nodeID, connected }) => {
     if (!connected) return <></>
     if (!nodeID) return <Typography> NodeID: N/A </Typography>
-    return <Typography > NodeID:&nbsp;
+    return <Typography > NodeID:
         <RouterLink to={`/n/${nodeID}`}>
             {displayContentID(nodeID)}
         </RouterLink>

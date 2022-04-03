345af3ed64f6bd372764e807c4e6cf99164e4f9e
Thomas Haferlach <t.haferlach@gmail.com>
2022-04-03 18:59:50 +0200

    Show fine grained progress if it exists for the current step

--- app/src/components/NotebookProgress.js
+++ app/src/components/NotebookProgress.js
@@ -1,7 +1,7 @@
 import { Box, LinearProgress, Typography } from "@material-ui/core";
+import Debug from "debug";
 import { last } from "ramda";
 import React from "react";
-import Debug from "debug";
 
 const debug = Debug("NotebookProgress");
 
@@ -9,15 +9,16 @@ export const NotebookProgress = ({output, metadata}) => {
     if (!output?.log?.split || !metadata)
         return null;
     const progress = getProgress(output.log, metadata.numCells)*100;
-
+    const fineProgress = getFinegrainedProgress(output.log)*100;
     
     const inProgress =  progress >= 0 && !output?.done && !(progress >= 100);
-    debug("progress", progress, inProgress);
+    debug("progress", progress, inProgress, fineProgress);
     if (!inProgress)
       return null;
 
     return  <><Box display="flex" alignItems="center">
       <Box width="100%" mr={1}>
+         Overall:
           <LinearProgress value={progress} variant="determinate" color="secondary" />
       </Box>
       <Box minWidth={35}>
@@ -26,6 +27,20 @@ export const NotebookProgress = ({output, metadata}) => {
         )}%`}</Typography>
       </Box>
       </Box>
+      
+      { fineProgress && <Box display="flex" alignItems="center">
+        <Box width="100%" mr={1}>
+          Current Step:
+          <LinearProgress value={fineProgress} variant="determinate" color="primary" /> 
+        </Box> 
+        <Box minWidth={35}>
+        <Typography variant="body2" color="textSecondary">{`${Math.floor(
+          fineProgress
+        )}%`}</Typography>    
+        </Box>   
+        </Box> 
+        }
+ 
        <Typography variant="body2" color="textSecondary" align="center">Please wait... Results should start appearing within a minute or two.</Typography>
     </>
 }
@@ -44,4 +59,22 @@ export const getProgress = (log, numCells) => {
     const cellNo = parseInt(updateText.split("Ending Cell ")[1]) - 1;
     return cellNo / numCells;
 }
-        
+
+export const getFinegrainedProgress = (log) => {
+    const lastCell = last(
+        log.split("Executing Cell")
+    );
+
+    if (!lastCell)
+        return null;
+
+    if (lastCell.includes("Ending Cell")) 
+        return null;
+    
+    const percentageMatched = last(lastCell.split("\n").map(s => s.trim().match(/(\d*)%.*/)).filter(n => n?.length === 2))
+    
+    if (!percentageMatched)
+        return null;
+    
+    return parseInt(percentageMatched[1]) / 100
+}

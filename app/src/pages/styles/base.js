99e780e21e0989a94453e3e74e4c3f15ce8033f5
ale-rls <85608868+ale-rls@users.noreply.github.com>
2022-04-25 18:52:09 +0200

    fix aspect ratio of preview frames

--- app/src/pages/styles/base.js
+++ app/src/pages/styles/base.js
@@ -1,6 +1,12 @@
-
+import styled from '@emotion/styled'
 
 export const SmallContainer = ({ children }) => 
     <div style={{ minWidth: '20%', maxWidth: 600, margin: 'auto', marginBottom: '7em' }}>
         {children}
     </div>
+
+export const GridStyle = styled.div`
+display: grid;
+grid-template-columns: repeat(auto-fill, minmax(300px, 1fr));
+grid-gap: 1em;
+`

bcf7384a8960075ea9b7132eb6a51a94c7316bd5
Thomas Haferlach <t.haferlach@gmail.com>
2022-04-29 12:54:29 +0200

    Update dependencies and cleanup

--- app/src/index.js
+++ app/src/index.js
@@ -5,8 +5,6 @@ import ReactDOM from 'react-dom'
 import App from './App'
 import { AuthProvider } from './hooks/useAuth'
 import './index.css'
-import * as serviceWorkerRegistration from './serviceWorkerRegistration'
-
 
 const darkTheme = createMuiTheme({
   typography: {
@@ -33,12 +31,3 @@ ReactDOM.render(
   document.getElementById('root')
 );
 
-// If you want your app to work offline and load faster, you can change
-// unregister() to register() below. Note this comes with some pitfalls.
-// Learn more about service workers: https://cra.link/PWA
-serviceWorkerRegistration.unregister();
-
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-// reportWebVitals(console.log);

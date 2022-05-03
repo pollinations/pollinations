ef56e5ce3b99f204acca7922ff694642992dac04
Thomas Haferlach <t.haferlach@gmail.com>
2022-05-03 22:48:23 +0200

    Update run_notebook.sh

--- app/scripts/run_notebook.sh
+++ app/scripts/run_notebook.sh
@@ -34,7 +34,7 @@ for path in $IPFS_ROOT/input/*; do
 
     # if it is a directory then save list of absolute file paths to $value in one line
     if [[ -d $path ]]; then
-        value=$(ls -1 -d $path/*)
+        value=$(ls -1 -d $path/* | awk '{print "\x27" $1 "\x27" }') | tr '\n' ' '
     else
         value=$(<$path)
     fi

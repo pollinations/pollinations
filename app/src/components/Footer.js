bb6f468398db64a09ec706eb1a2872a9a9a5fe8e
ale-rls <85608868+ale-rls@users.noreply.github.com>
2022-05-17 14:25:36 +0200

    blank impressum (#319)
    * blank impressum
    
    * filled impressum
    
    * Add impressum to footer
    
    * moved impressum link to about page
    
    * Move impressum to markdown
    
    Co-authored-by: Thomas Haferlach <t.haferlach@gmail.com>

---
+++ app/src/components/Footer.js
@@ -0,0 +1,10 @@
+import Link from '@material-ui/core/Link'
+
+const Footer = () => <div style={{ margin: '1em auto 1em auto' }}>
+    Discuss, get help and contribute on
+    <Link href="https://github.com/pollinations/pollinations" target="_blank"> [ Github ] </Link>
+    or <Link href="https://discord.gg/XXd99CrkCr" target="_blank">[ Discord ]</Link>.
+    <br/>
+</div>
+
+export default Footer

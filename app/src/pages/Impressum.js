2a5a851791c6d8198b79fb5a19a4ecfab851a5d5
ale-rls <85608868+ale-rls@users.noreply.github.com>
2022-05-17 14:25:36 +0200

    blank impressum (#319)
    * blank impressum
    
    * filled impressum
    
    * Add impressum to footer
    
    * moved impressum link to about page
    
    * Move impressum to markdown
    
    Co-authored-by: Thomas Haferlach <t.haferlach@gmail.com>

---
+++ app/src/pages/Impressum.js
@@ -0,0 +1,11 @@
+import MarkDownContent from "../components/molecules/MarkDownContent"
+import { SmallContainer } from "./styles/base"
+
+const Impressum = () => {
+
+    return <SmallContainer>
+        <MarkDownContent id='impressum'/>
+    </SmallContainer>
+}
+
+export default Impressum

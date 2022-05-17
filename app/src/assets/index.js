af6bf283c11d7eba8daf7ab65b7b5d0ebd272ab9
Alexander Reiling <alexanderreiling@googlemail.com>
2022-05-17 14:50:21 +0200

    Create lookup for expo markdown urls with webpack

--- app/src/assets/index.js
+++ app/src/assets/index.js
@@ -1,13 +1,25 @@
-import about from './text/about.md'
-import blank from './text/blank.md'
-import failure from './text/failure.md'
-import help from './text/help.md'
-import instructions from './text/instructions.md'
-import landingLeft from './text/landingLeft.md'
-import landingRight from './text/landingRight.md'
-import impressum from './text/impressum.md'
+import about from "./text/about.md"
+import blank from "./text/blank.md"
+import failure from "./text/failure.md"
+import help from "./text/help.md"
+import instructions from "./text/instructions.md"
+import landingLeft from "./text/landingLeft.md"
+import landingRight from "./text/landingRight.md"
+import impressum from "./text/impressum.md"
 
 export const textContent = {
-    help, about, instructions, blank, failure, landingLeft, landingRight, impressum
+  help,
+  about,
+  instructions,
+  blank,
+  failure,
+  landingLeft,
+  landingRight,
+  impressum,
 }
 
+const expoCtx = require.context("./expo", false, /\.md$/)
+export const EXPOS = expoCtx.keys().reduce((prev, key, index) => {
+  prev[index] = expoCtx(key).default
+  return prev
+}, {})

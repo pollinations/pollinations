295ad2ad6134ce85c1101011595200863d26278b
Alexander Reiling <alexanderreiling@googlemail.com>
2022-05-17 14:49:29 +0200

    Refactor SocialLinks to use new custom Icons

---
+++ app/src/_globalConfig/socialLinks.js
@@ -0,0 +1,15 @@
+import { ICONS } from '../assets/icons'
+export const SOCIAL_LINKS = {
+  twitter: { icon: ICONS.twitter, url: 'https://twitter.com/pollinations_ai' },
+  facebook: { icon: ICONS.facebook, url: 'https://facebook.com/pollinations' },
+  // "linkedin": { icon: <LinkedInIcon fontSize='small'/>, url: "https://linkedin.com/company/pollinations-ai"},
+  instagram: { icon: ICONS.instagram, url: 'https://instagram.com/pollinations_ai' },
+  youtube: { icon: ICONS.youtube, url: 'https://www.youtube.com/channel/UCk4yKnLnYfyUmCCbDzOZOug' },
+  telegram: {
+    icon: ICONS.telegram,
+    url: 'https://t.me/joinchat/Ft4jOGXbIyViM2My',
+  },
+  // "pinterest":  { icon: <PinterestIcon fontSize='small'/>, url: "https://pinterest.com/pollinations_ai/"},
+  // "gmb":  { icon: <GoogleIcon />, url: "https://facebook.com/pollinations"},
+  // "pinterest": { icon: <GoogleIcon fontSize='small'/>, url: "https://facebook.com/pollinations"},
+}

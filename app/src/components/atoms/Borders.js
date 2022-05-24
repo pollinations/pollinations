28e7813b986bffe83e4e5d952ec5448f358a453f
ale-rls <85608868+ale-rls@users.noreply.github.com>
2022-05-24 12:50:24 +0200

    border fix

--- app/src/components/atoms/Borders.js
+++ app/src/components/atoms/Borders.js
@@ -3,6 +3,6 @@ import styled from '@emotion/styled'
 export const HorizontalBorder = styled.div`
 
 height: ${props => props.Height ? props.Height : '1px'};
-margin-top: ${props => props.margin ? props.margin : '1em 0 0 0'};
+margin: ${props => props.margin ? props.margin : '1em 0 0 0'};
 background-color: white;
 `

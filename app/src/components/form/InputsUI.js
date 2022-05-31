2c241b0c2b8165fc6dc6740396bb785f03e27a33
voodoohop <t.haferlach@gmail.com>
2022-05-31 15:50:52 +0200

    Fix open in colab button (#332)
    * FormikForm unfinished
    
    * Formik with Radio, Select, TextField inputs
    
    * disable checkbox
    
    * Add more flexible parsing of type annotations
    
    * Add parsing of whether the type is advanced
    
    * Dropzone, fix imgBase64
    
    * file name fix spaces
    
    * functional formik form
    
    * handle audio filename in dropzone
    
    * ...
    
    * advanced inputs
    
    * dropzone content onMount
    
    * dropzone fix empty form thumb
    
    * Disable revokeObjectURL
    
    * Don't show advanced fields if none are defined
    
    * exclude upload button
    
    * disable dropzone fix
    
    * disable dropzone fix
    
    * fix advancedInputs box shadow
    
    * dropzone description
    
    * remove log
    
    * dropzone description
    
    * Change ipfs base url
    
    * Allow arbitrary file types for certain form fields
    
    * Remove RJSF dependency
    
    * cleanup
    
    * import mime
    
    * Remove npm version definition from package.json
    
    * Remove npm version from package lock
    
    * Change node version
    
    * Update dependencies and cleanup
    
    * Remove npm dependency
    
    * Tweak dependencies
    
    * Dependency mess
    
    * Don't build backend
    
    * Update .nvmrc
    
    * Add emotion/react dependency
    
    * Add abort-controller dependency
    
    * Initial switch to IPFS
    
    * Format URL correctly
    
    * Update file.js
    
    * IPFS handling update
    
    * Update file.js
    
    * make paths in input relative to ipfs root
    
    * Update
    
    * Update run_notebook.sh
    
    * Reflect wording change on Google Colab
    
    * Create a folder per file upload place
    
    * Update run_notebook.sh
    
    * Update run_notebook.sh
    
    * Update run_notebook.sh
    
    * Update run_notebook.sh
    
    * Fix error when adding .cid to non-objects
    
    * Update run_notebook.sh
    
    * Update run_notebook.sh
    
    * Update run_notebook.sh
    
    * Update run_notebook.sh
    
    * Update run_notebook.sh
    
    * Update run_notebook.sh
    
    * Empty ipfs folder before fetching new IPFS state
    
    * Import rmSync
    
    * remove input path only for now
    
    * Update receiver.js
    
    * disable cache
    
    * Execute tweaks
    
    * upd
    
    * restructure
    
    * ipfs receiving sending logic
    
    * ipfs receiving sending logic
    
    * ipfs receiving sending logic
    
    * ipfs receiving sending logic
    
    * ipfs receiving sending logic
    
    * ipfs receiving sending logic
    
    * ipfs receiving sending logic
    
    * ipfs receiving sending logic
    
    * ipfs receiving sending logic
    
    * ipfs receiving sending logic
    
    * ipfs receiving sending logic
    
    * ipfs receiving sending logic
    
    * ipfs receiving sending logic
    
    * ipfs receiving sending logic
    
    * ipfs receiving sending logic
    
    * ipfs receiving sending logic
    
    * ipfs receiving sending logic
    
    * ipfs receiving sending logic
    
    * ipfs receiving sending logic
    
    * ipfs receiving sending logic
    
    * ipfs receiving sending logic
    
    * Backend
    
    * handle "integer" type
    
    * Fix ipfs sender
    
    * Disable google drive saving for now
    
    * Fix change google drive copying while getting correct status code
    
    * Fix ipfsreceiver. Pin only to nft storage
    
    * Update package-lock.json
    
    * cta changes, colablink
    
    Co-authored-by: ale-rls <85608868+ale-rls@users.noreply.github.com>

--- app/src/components/form/InputsUI.js
+++ app/src/components/form/InputsUI.js
@@ -1,76 +1,73 @@
-import {
-  Button, FormControlLabel, Link, MenuItem,
-  Radio,
-  RadioGroup,
-  TextField
-} from "@material-ui/core";
-import Debug from "debug";
+import { 
+  FormControlLabel, 
+  MenuItem, 
+  Radio, 
+  RadioGroup, 
+  TextField,
+  Button, Box, Link } from "@material-ui/core"
 import HelpModal from "../HelpModal";
 import DropZone from "./customInputs/file";
 
-const debug = Debug("InputsUI")
-
 export const InputField = (props) => {
 
-    if (!props)
-      return null
-    debug("props", props)
-    // Dropzone file input
-    if (`${props.id}`.includes('file'))
-        return <DropZone {...props} />
+  if (!props)
+    return null
 
-    // Radio buttons Yes/No
-    if (props.type === 'boolean') 
-      return <div style={props.style}>
-                <label> {props.title} </label>
-                <RadioGroup  value={props.value} onChange={e => props.setFieldValue(props.id, e.target.value === 'true' ? true : false)}>
-                  <FormControlLabel
-                    label='Yes'
-                    value={true}
-                    control={<Radio  />}
-                    disabled={props.disabled}/>
-                  <FormControlLabel
-                    label='No'
-                    value={false}
-                    control={<Radio  />}
-                    disabled={props.disabled}/>      
-                </RadioGroup> 
-              </div>
+  // Dropzone file input
+  if (`${props.id}`.includes('file'))
+      return <DropZone {...props} />
+
+  // Radio buttons Yes/No
+  if (props.type === 'boolean') 
+    return <div style={props.style}>
+    <label> {props.title} </label>
+    <RadioGroup  value={props.value} onChange={e => props.setFieldValue(props.id, e.target.value === 'true' ? true : false)}>
+      <FormControlLabel
+        label='Yes'
+        value={true}
+        control={<Radio  />}
+        disabled={props.disabled}/>
+      <FormControlLabel
+        label='No'
+        value={false}
+        control={<Radio  />}
+        disabled={props.disabled}/>      
+    </RadioGroup> 
+  </div>
+  
+  // Select Component
+  if (props.enum?.length > 0) 
+  return <TextField select {...props}
+  onChange={e => props.setFieldValue(props.id, e.target.value)}>
+  {
+      props.enum.map(option => <MenuItem key={option} value={option}>
+          {option}
+      </MenuItem>)
+  }
+  </TextField>    
+
+  // Text/Rest...
+  return <TextField fullWidth {...props}/>
+}
+
+
+
+export const FormActions = ({ isDisabled, formik, colabLink }) => {
+
+  return <>
+  <Box 
+    display="flex" 
+    justifyContent="center" >
+
+    <Button type="submit" disabled={isDisabled || formik.isSubmitting} variant='outlined'>
+        <p style={{fontSize: '1.1em', margin: 0}}>
+          {formik.isSubmitting ? "Creating..." : "Create"}
+        </p>
+    </Button>
+
+
     
-    // Select Component
-    if (props.enum?.length > 0) 
-      return <TextField select {...props}
-              onChange={e => props.setFieldValue(props.id, e.target.value)}>
-                {
-                    props.enum.map(option => (
-                      <MenuItem key={option} value={option}>
-                        {option}
-                      </MenuItem>))
-                }
-            </TextField>    
-  
-    // Text/Rest...
-
-    if (props.type === 'integer')
-      return <TextField {...props}  type='number'  />
+  </Box>
       
-    return <TextField fullWidth {...props}/>
-  }
-
-
-
-  export const FormActions = ({ isDisabled, formik, colabLink }) => {
-
-    return <>
-      <Button 
-        disabled={isDisabled || formik.isSubmitting} 
-        type="submit">
-        [ {formik.isSubmitting ? "Submitting" : "Submit"} ]
-      </Button>
-  
-      <HelpModal />
-  
-      {colabLink && <Link href={colabLink} target="_blank">[ OPEN IN COLAB ]</Link> }
-  
-    </>
-  }
+  </>
+}

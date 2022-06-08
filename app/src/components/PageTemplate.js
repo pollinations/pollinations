22fc0f267097414d6f4aaeff63265555941d4faa
ale-rls <85608868+ale-rls@users.noreply.github.com>
2022-06-08 14:46:36 +0200

    home, page template seo headers

--- app/src/components/PageTemplate.js
+++ app/src/components/PageTemplate.js
@@ -1,13 +1,17 @@
 import { textContent } from "../assets"
 import MarkDownContent from "../components/molecules/MarkDownContent"
 import { SmallContainer } from "../pages/styles/base"
+import { SEOMetadata } from "./Helmet"
 
 const PageTemplate = ({ label }) => {
+
+
 
   if (!label) return <></>
 
   return (
     <SmallContainer>
+      <SEOMetadata title={`${label[0].toUpperCase()}${label.slice(1)}`} />
       <MarkDownContent url={textContent[label]} />
     </SmallContainer>
   )

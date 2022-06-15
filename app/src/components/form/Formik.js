ed89cda57cb99389602d18ab932ebe9a6b8ed6d1
Thomas Haferlach <t.haferlach@gmail.com>
2022-06-15 18:35:11 +0200

    Update Formik.js

--- app/src/components/form/Formik.js
+++ app/src/components/form/Formik.js
@@ -1,9 +1,6 @@
-import { Accordion, AccordionSummary } from '@material-ui/core';
-import ExpandMoreIcon from '@material-ui/icons/ExpandMore';
 import Debug from "debug";
-import { useFormik } from 'formik';
+import CustomFormikForm from './CustomFormik';
 import { getForm } from './helpers';
-import { FormActions, InputField } from './InputsUI';
 
 const debug = Debug("Formik")
 
@@ -17,76 +14,9 @@ const FormikForm = ({ input, connected, metadata, onSubmit }) => {
   const colabLink = metadata?.colabLink;
   const isDisabled = !connected;
 
-  const hasAdvancedFields = Object.values(inputs).some(({ advanced }) => advanced);
-  
-  debug("inputs",inputs)
-  if (!inputs || !initialValues)
-    return null
-
-  // Formik hook holds the form state and methods 
-  const formik = useFormik({
-    initialValues: initialValues,
-    // validationSchema: validationSchema,
-    onSubmit
-  });
-  debug("formik", formik)
-
-  return <form onSubmit={formik.handleSubmit}>
-    { // Basic Inputs
-      Object.keys(formik.values).map(key => 
-      !inputs[key].advanced &&
-      <>
-        <InputField
-          {...inputs[key]}
-          {...formik}
-          fullWidth
-          disabled={isDisabled}
-          id={key}
-          label={inputs[key].title}
-          type={inputs[key].type}
-          helperText={inputs[key].description}
-          value={formik.values[key]}
-          inputCID={input[".cid"]}
-          onChange={formik.handleChange}
-          style={{ margin: '1rem 0' }}
-        />
-      </>
-      )
-    }   
-    { hasAdvancedFields && <Accordion elevation={0}>
-        <AccordionSummary
-          expandIcon={<ExpandMoreIcon />}
-          aria-controls={`advanced_inputs`}
-          id={`advanced_inputs`}>
-            Advanced Inputs
-        </AccordionSummary>
-        {
-          Object.keys(formik.values).map(key => 
-            inputs[key].advanced &&
-            <>
-              <InputField
-                {...inputs[key]}
-                {...formik}
-                fullWidth
-                disabled={isDisabled}
-                id={key}
-                label={inputs[key].title}
-                type={inputs[key].type}
-                helperText={inputs[key].description}
-                value={formik.values[key]}
-                onChange={formik.handleChange}
-                style={{ margin: '1.5rem 0' }}
-              />
-            </>
-            )
-        }
-      </Accordion> }
-       
-    <FormActions 
-      isDisabled={isDisabled} 
-      formik={formik} 
-      colabLink={colabLink} />
-
-  </form>
+  // const hasAdvancedFields = Object.values(inputs).some(({ advanced }) => advanced);
+  return <>
+          <CustomFormikForm inputs={inputs} onSubmit={onSubmit} isDisabled={isDisabled} />
+        </>
 };
 export default FormikForm;

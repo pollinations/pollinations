2efd0eef0348a909923fe49d4b00a3504fe3a597
ale-rls <85608868+ale-rls@users.noreply.github.com>
2022-06-17 09:48:15 +0200

    same page results

--- app/src/pages/Envisioning.js
+++ app/src/pages/Envisioning.js
@@ -4,17 +4,21 @@ import Typography from "@material-ui/core/Typography";
 import Debug from "debug";
 import React from "react";
 import FormikForm from '../components/form/Formik';
+import MediaViewer from '../components/MediaViewer';
 import NotebookTitle from "../components/NotebookTitle";
+import useColabNode from '../hooks/useColabNode';
+import useIPFS from '../hooks/useIPFS';
 import useIPFSWrite from '../hooks/useIPFSWrite';
 import { submitToAWS } from '../network/aws';
 
 
 const debug = Debug("Envisioning");
 
-export default React.memo(function Create({navigateToNode}) {
+export default React.memo(function Create() {
 
   const ipfsWriter = useIPFSWrite()
-
+  const { overrideNodeID, node } = useColabNode()
+  
   const inputs = {
     "Prompt": {
       type: "string",
@@ -49,26 +53,38 @@ export default React.memo(function Create({navigateToNode}) {
               values = {...values, customEndpoint: "/envisioning"}
 
               const nodeID = await submitToAWS(values, ipfsWriter);
-              navigateToNode(nodeID);
+              // navigateToNode(nodeID);
+              overrideNodeID(nodeID);
             }}/>
           </div> 
+          <Previewer contentID={node.contentID}/>
         </CenterContent>
 
           
     </Box>
 });
 
+const Previewer = ({ contentID }) => {
 
-// Functions
+  const ipfs = useIPFS(contentID)
 
+  if (!ipfs.output) return null;
 
+  return <PreviewerStyle>
+    <MediaViewer output={ipfs.output} contentID={ipfs[".cid"]} />
+    </PreviewerStyle>
+}
 
 // STYLES
 const CenterContent = styled.div`
 display: flex;
 justify-content: center;
-align-items: center;
+align-items: flex-start;
 width: 100%;
 margin-top: 1em;
 `;
+
+const PreviewerStyle = styled.div`
+padding: 0 5em;
+`
 

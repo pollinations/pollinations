ac8ca814fc43967373ab351051e596693c2030d0
ale-rls <85608868+ale-rls@users.noreply.github.com>
2022-06-17 10:22:08 +0200

    split into controls and previewer

--- app/src/pages/Envisioning.js
+++ app/src/pages/Envisioning.js
@@ -4,11 +4,11 @@ import Typography from "@material-ui/core/Typography";
 import Debug from "debug";
 import React from "react";
 import FormikForm from '../components/form/Formik';
-import MediaViewer from '../components/MediaViewer';
+import {MediaViewer} from '../components/MediaViewer';
 import NotebookTitle from "../components/NotebookTitle";
+import { getMedia } from '../data/media';
 import useColabNode from '../hooks/useColabNode';
 import useIPFS from '../hooks/useIPFS';
-import useIPFSWrite from '../hooks/useIPFSWrite';
 import { submitToAWS } from '../network/aws';
 
 
@@ -16,7 +16,6 @@ const debug = Debug("Envisioning");
 
 export default React.memo(function Create() {
 
-  const ipfsWriter = useIPFSWrite()
   const { overrideNodeID, node } = useColabNode()
   
   const inputs = {
@@ -39,24 +38,7 @@ export default React.memo(function Create() {
 
       
         <CenterContent>
-          <div>
-
-            <NotebookTitle name='Envisioning "API"' />
-            <Typography variant="h5" gutterBottom>
-              Inputs
-            </Typography>
-
-            <FormikForm inputs={inputs} onSubmit={async (values) => {
-              
-              // adding customEndpoint is just a way to be able to redirect back to this page from the results viewer
-              // can be removed if we replace results viewer with something custom
-              values = {...values, customEndpoint: "/envisioning"}
-
-              const nodeID = await submitToAWS(values, ipfsWriter);
-              // navigateToNode(nodeID);
-              overrideNodeID(nodeID);
-            }}/>
-          </div> 
+          <Controls inputs={inputs} overrideNodeID={overrideNodeID} />
           <Previewer contentID={node.contentID}/>
         </CenterContent>
 
@@ -64,27 +46,68 @@ export default React.memo(function Create() {
     </Box>
 });
 
+
+const Controls = ({ inputs, overrideNodeID }) => {
+
+
+  return <div style={{ maxWidth: 300, gridColumnStart: 1, gridColumnEnd: 2}}>
+
+    <NotebookTitle name='Envisioning "API"' />
+    <Typography variant="h5" gutterBottom>
+      Inputs
+    </Typography>
+
+    <FormikForm inputs={inputs} onSubmit={async (values) => {
+      
+      // adding customEndpoint is just a way to be able to redirect back to this page from the results viewer
+      // can be removed if we replace results viewer with something custom
+      values = {...values, customEndpoint: "/envisioning"}
+
+      const nodeID = await submitToAWS(values, ipfsWriter);
+      // navigateToNode(nodeID);
+      overrideNodeID(nodeID);
+    }}/>
+  </div> 
+}
+
 const Previewer = ({ contentID }) => {
 
   const ipfs = useIPFS(contentID)
 
   if (!ipfs.output) return null;
-
+  let images = getMedia(ipfs.output);
   return <PreviewerStyle>
-    <MediaViewer output={ipfs.output} contentID={ipfs[".cid"]} />
+    {
+          images?.map(([filename, url, type]) => (
+            <MediaViewer 
+              content={url} 
+              filename={filename} 
+              type={type}
+            />
+          ))
+      }
     </PreviewerStyle>
 }
 
 // STYLES
 const CenterContent = styled.div`
-display: flex;
-justify-content: center;
-align-items: flex-start;
 width: 100%;
+
+display: grid;
+grid-template-columns: repeat(auto-fill, minmax(300px, 1fr));
+
+
 margin-top: 1em;
 `;
 
 const PreviewerStyle = styled.div`
-padding: 0 5em;
+
+grid-column-start: 2;
+grid-column-end: 4;
+
+display: grid;
+grid-template-columns: repeat(auto-fit, minmax(300px, 1fr));
+grid-gap: 0.5em;
+
 `
 

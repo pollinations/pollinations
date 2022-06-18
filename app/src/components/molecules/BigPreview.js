25a763f818a2bf7cd8c5064427f86b4a47823b9f
voodoohop <t.haferlach@gmail.com>
2022-06-18 20:11:18 +0200

    Update BigPreview.js (#358)

--- app/src/components/molecules/BigPreview.js
+++ app/src/components/molecules/BigPreview.js
@@ -5,7 +5,7 @@ const BigPreview = ({ filename, url, type }) => {
   
     if (!url) return <div style={{minHeight:'80vh'}}>Processing...</div>
 
-    return <MediaViewer filename={filename} content={url} type={type} style={{ width: 'calc(100vh - 90px)', maxWidth: '100%', maxHeight: '75vh' }} />
+    return <MediaViewer filename={filename} content={url} type={type} style={{ width: 'calc(100vh - 90px)', maxWidth: '100%'}} />
 }
 
 export default BigPreview

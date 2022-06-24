23e88118ece8c4ec709de024012ce00ec9326c0a
ale-rls <85608868+ale-rls@users.noreply.github.com>
2022-06-24 11:16:13 +0200

    Features/fancy home (#360)
    * slider component
    
    * rows per category
    
    * cleanup
    
    * scrollbar
    
    * bg, drawer
    
    * drawer, cleanup
    
    * cleanup
    
    * models page
    
    * toolbar, cleanup
    
    * cleanup
    
    * design iteration
    
    * fontsieze
    
    * font size
    
    * font color
    
    * font size
    
    * MarkDownStyle
    
    * faxina
    
    * faxina
    
    * slider cleanup
    
    * test
    
    * models page back to filter
    
    * background fixed
    
    * cleanup
    
    * cleanup
    
    * test
    
    * test
    
    * -lock
    
    * revert test, json lock
    
    * kkkkk
    
    * Your engine
    
    * wrap filterui
    
    * cleanup
    
    Co-authored-by: Thomas Haferlach <t.haferlach@gmail.com>

---
+++ app/src/components/temp/NotebookCard.js
@@ -0,0 +1,51 @@
+import { Box, Card, CardContent, CardHeader, Typography } from "@material-ui/core";
+import { Link } from "react-router-dom";
+import RouterLink from "../molecules/RouterLink";
+import NotebookImage from "../organisms/markdownParsers/NotebookImage";
+import NotebookInfo from "../organisms/markdownParsers/NotebookInfo";
+
+const NotebookCard = ({ notebook }) => {
+    let { category, name, path, description } = notebook
+  
+    if (!description) return null;
+  
+    // remove credits etc (they are separated by a horizontal rule)
+    description = description.split("---")[0]
+  
+    // parse category
+    const parsedCategory = category?.slice(2)
+      .replace('-', ' ')
+      .replace('-', ' ')
+      .toLowerCase();
+  
+    return (
+      <Box>
+        <div style={{ borderRadius: 20, backgroundColor: 'rgba(0,0,0,0.3)'}}>
+            <Link to={path} style={{textDecoration: 'none'}}>
+            <CardHeader
+                subheader={<CardTitle children={name?.slice(2)} to={path} variant="h4" />}
+                title={<CardTitle children={parsedCategory} to={path} variant="h6" />}
+              />
+              
+  
+              <NotebookImage metadata={notebook} style={{ width: "100%" }} />
+  
+              <CardContent>
+                <NotebookInfo description={description} noImg />
+              </CardContent>
+            </Link>
+          </div>
+      </Box>
+    )
+  }
+  
+  
+  const CardTitle = ({ to, children, variant }) => (
+    <>
+      <Typography className="Lato noMargin" variant={variant} gutterBottom>
+        <RouterLink to={to}>{children}</RouterLink>
+      </Typography>
+    </>
+  )
+
+  export default NotebookCard

3a896f0e8136c17d1654cedd68766aa91bb4a3b2
ale-rls <85608868+ale-rls@users.noreply.github.com>
2022-07-12 16:06:54 +0200

    back to static

--- app/src/pages/Home/Hero.js
+++ app/src/pages/Home/Hero.js
@@ -1,6 +1,6 @@
 import styled from '@emotion/styled'
 import { BackGroundImage } from '../../styles/global';
-import heroBG from '../../assets/imgs/bg_animated.gif'
+import heroBG from '../../assets/imgs/bghero.jpg'
 import heroBGOverlay from '../../assets/imgs/bgherooverlay.jpeg'
 // Hero Section
 

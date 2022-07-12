93c45fb65f220d7ec87c27a4bc75f4d79f9481db
ale-rls <85608868+ale-rls@users.noreply.github.com>
2022-07-12 17:32:50 +0200

    fix (#379)

--- app/src/pages/Integrate.js
+++ app/src/pages/Integrate.js
@@ -20,7 +20,7 @@ const Integrate = props => {
           Imagine a gaming platform in which players can <b>create 3D objects</b> inside the game just by typing a few words into a box, or an NFT marketplace in which users can <b>create and mint NFTs</b> on the spot. Not bad, huh?
           <br/><br/>
         
-          By integrating with Pollinations’ API users can create all of this and much more <b> without switching platforms</b> We offer <b>presets and looks</b> so that all media created can have the visual identity of your brand.
+          By integrating with Pollinations’ API users can create all of this and much more <b> without switching platforms.</b> We offer <b>presets and looks</b> so that all media created can have the visual identity of your brand.
           <br/><br/>
 
           Music platforms, NFTs, articles about emerging technology, luxury hotels, 3D object stores and many more are getting a touch of magic with Pollinations’ AI-power. Let us hear about your goals at hello@pollinations.ai.

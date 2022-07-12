ffb380ff4127caaf8b29a34fa45527e3c278485d
ale-rls <85608868+ale-rls@users.noreply.github.com>
2022-07-12 16:20:36 +0200

    integrate page

--- app/src/routes/publicRoutes.js
+++ app/src/routes/publicRoutes.js
@@ -1,5 +1,6 @@
 export const ROUTES = {
     about: { label: "About", to: "/about", id: 'about' },
+    integrate: { label: "Integrate", to: "/integrate", id: 'integrate' },
     feed: { label: "Feed", to: "/feed", id: 'feed' },
     help: { label: "Help", to: "/help", id: 'help' },
     impressum: { label: "impressum", to: "/impressum", id: 'impressum' },
@@ -11,11 +12,13 @@ export const ROUTES = {
 export const MAIN_NAV_ROUTES = [
     ROUTES.models,
     ROUTES.about, 
+    ROUTES.integrate,
     ROUTES.feed,
     ROUTES.help
 ]
 export const MARKDOWN_ROUTES = [
     ROUTES.about,
+    ROUTES.integrate,
     ROUTES.help,
     ROUTES.impressum
 ]

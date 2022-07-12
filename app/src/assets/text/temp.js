f4a214576014c8ac4de3a79228808408057b8c66
ale-rls <85608868+ale-rls@users.noreply.github.com>
2022-07-12 16:51:22 +0200

    Features/designchanges 8722 (#376)
    * home
    
    * footer, whywedoit
    
    * endlesspossibilitiessection
    
    * media placeholders
    
    * create section on home
    
    * mobile try out
    
    * initial state
    
    * Switch model for LAION frontpage preset
    
    * media, backgrounds, copywrite
    
    * use preset-frontpage
    
    * dreams and img fix
    
    * Readd slicing of images
    
    * Use prod
    
    * containing
    
    * use pollinations useAWSNode hook and change initial state handling a little
    
    * simplify useAWS usage
    
    * Update pollinations ipfs
    
    * Defensive coding
    
    * fix when connection fails
    
    * recommit
    
    * use dev for now
    
    * 3d assets
    
    * 3d stuff
    
    * animated bg
    
    * sizes
    
    * back with animated bg
    
    * Smalle animated background and stops at end
    
    * Choose between dev and prod randomly
    
    * cleanup
    
    * meme asset
    
    * get loading state from useAWS hook
    
    * back to static
    
    * compress 3d objects
    
    * compression
    
    * integrate page
    
    * use new simplified useAWSNode
    
    * integrate alternative
    
    Co-authored-by: Thomas Haferlach <t.haferlach@gmail.com>

---
+++ app/src/assets/text/temp.js
@@ -0,0 +1,12 @@
+
+
+const texts = {
+    about: {
+        title: 'About',
+        body: <>
+        Pollinations is a platform to generate media with the help of AI. Here you can create customized, royalty-free pieces of audio, images, 3D objects and soon fully immersive 3D environments on the fly.
+        <br/><br/>
+        We offer cutting-edge AI models that are constantly being updated. Every creation is unique and free to use.
+        </>
+    }
+}

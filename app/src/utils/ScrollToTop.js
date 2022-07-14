43b0fd33d997fb67dba8bd805a648d125ac2e578
ale-rls <85608868+ale-rls@users.noreply.github.com>
2022-07-14 10:12:28 +0200

    explicit

--- app/src/utils/ScrollToTop.js
+++ app/src/utils/ScrollToTop.js
@@ -1,13 +1,13 @@
 import { useEffect } from "react";
 import { useLocation } from "react-router";
 
-const ScrollToTop = (props) => {
+const ScrollToTop = ({ children }) => {
   const location = useLocation();
   useEffect(() => {
     window.scrollTo(0, 0);
   }, [location]);
 
-  return <>{props.children}</>
+  return <>{ children }</>
 };
 
 export default ScrollToTop;

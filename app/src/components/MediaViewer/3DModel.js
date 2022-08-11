6b03eb2c34fa163ccef7c9975c3a29b419c57cb3
voodoohop <t.haferlach@gmail.com>
2022-08-11 17:51:52 +0200

    Feature/failed feedback / new 3D Viewer (#403)
    * 3D VIewer for glb
    
    * Logs and feedback
    
    * Show Failure Viewer when success===false

--- app/src/components/MediaViewer/3DModel.js
+++ app/src/components/MediaViewer/3DModel.js
@@ -1,10 +1,20 @@
 import {OBJModel} from 'react-3d-viewer';
+import Debug from "debug";
 
+const debug = Debug("3DModelViewer");
 export default function ObjViewer({ content, style, filename }) {
+  debug("ObjViewer", content)
+
+  if (filename.toLowerCase().endsWith(".glb")) 
+    return <model-viewer 
+      src={content} 
+      style={{...style, width: "70vh", height: "70vh"}} 
+      camera-controls />
+  else
     return <OBJModel 
       src={content} 
       style={{...style, height: null, }} 
       background="rgba(0,0,0,0)" 
       scale= {{x:0.2, y:0.2, z: 0.2}} />
-  }
+}
 

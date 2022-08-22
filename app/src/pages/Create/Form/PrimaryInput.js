3d4080842c16c4d9cb94852fe41fc614233898a6
voodoohop <t.haferlach@gmail.com>
2022-08-22 21:50:26 +0200

    Add stable diffusion (#427)
    * Add stable diffusion
    
    * textarea input
    
    * correct link
    
    * fiix stable diffusion url
    
    * key change
    
    Co-authored-by: ale-rls <85608868+ale-rls@users.noreply.github.com>

--- app/src/pages/Create/Form/PrimaryInput.js
+++ app/src/pages/Create/Form/PrimaryInput.js
@@ -16,10 +16,27 @@ const PrimaryInput = props => {
 const { isDisabled, formik, models, selectedModel } = props;
 
 const { primary_input } = getInputs(models, selectedModel);
-
+console.log(selectedModel)
 const type = primary_input.type;
 
 const isFile = (type === 'string') && (primary_input.format === 'uri');
+
+
+if (selectedModel.key === '614871946825.dkr.ecr.us-east-1.amazonaws.com/pollinations/stable-diffusion-private') return <>
+<CreateTextArea
+    maxlength={10}
+    rows={4}
+    value={formik.values[primary_input?.key]}
+    onChange={formik.handleChange} 
+    name={primary_input.key} 
+    title={primary_input.title} 
+    disabled={isDisabled}
+    placeholder='Type your prompt'
+/>
+<CreateButton type='submit' disabled={isDisabled} marginLeft style={{alignSelf: 'flex-end'}}>
+    { formik.isSubmitting ? 'Creating...' : 'Create' }
+</CreateButton>
+</>
 
 if (isFile) return <>
 <DropZone 
@@ -66,4 +83,25 @@ justify-content: center;
 const CreateInputFullWidth = styled(CreateInput)`
 width: 100%;
 padding-right: calc(153px - 0.5em);
+`
+
+const CreateTextArea = styled.textarea`
+resize: none;
+width: 100%;
+background: linear-gradient(90deg, rgba(255, 255, 255, 0.15) 0%, rgba(255, 255, 255, 0.1) 100%);
+// border-radius: 60px;
+border: none;
+
+font-family: 'DM Sans';
+font-style: normal;
+font-weight: 400;
+font-size: 18px;
+line-height: 23px;
+display: flex;
+align-items: center;
+
+color: #FFFFFF;
+padding: 0.5rem;
+
+
 `

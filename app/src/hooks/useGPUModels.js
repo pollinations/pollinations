48eea66654d1a6c2a28efeb59d8fb33c1ba14593
voodoohop <t.haferlach@gmail.com>
2022-08-23 17:44:08 +0200

    Refactor Model Metadata handling (#428)
    * remove context for gpumodels
    
    * Load models from github
    
    * Refactor model metadata loading logic
    
    * Refactor model metadata loading
    
    * Debug buggy rerenders
    
    * Update index.js
    
    * Fix models indexed by path
    
    * debugging
    
    * Add stable diffusion
    
    * Fix initial input values
    
    * Bump @pollinations/ipfs version to get correct loading state
    
    Co-authored-by: eni-lorac <39300569+eni-lorac@users.noreply.github.com>

--- app/src/hooks/useGPUModels.js
+++ app/src/hooks/useGPUModels.js
@@ -1,86 +1,60 @@
-import * as React from 'react'
-import { keys, mapObjIndexed, zipObj } from 'ramda';
+import { useMemo }  from 'react'
+import { mapObjIndexed, zipObj } from 'ramda';
+import useModelsMetadata from './useFetchModelsMetadata';
+import Debug from "debug";
 
-const GPUModelsContext = React.createContext()
-
-function GPUModelsProvider({ children }) {
-
-    const [ models, setModels ] = React.useState({});
-    const [ areModelsLoading, setLoading ] = React.useState(false);
-    const [ error, setError ] = React.useState({});
-
-    const wantedModels = MODELS;
-
-    React.useEffect(()=>{
-        
-        
-        async function fetchInitialModels(){
-            
-            setLoading(true)
-            try {
-                const response = await fetch('https://raw.githubusercontent.com/pollinations/model-index/main/metadata.json');
-                const dataMetadaFormat = await response.json();
-                
-                // transform the new metadata format to the old one (extract each entrie's "openapi" prop)
-                const data = mapObjIndexed(({openapi}) => openapi, dataMetadaFormat);
-
-                const filtered_data = zipObj(
-                    Object.values(wantedModels), 
-                    Object.values(wantedModels)
-                    // return only the models we want
-                    .map(model => data[model] )
-                    // 
-                );
-
-                setModels(filtered_data);
-                setLoading(false);
-            } catch (error) {
-                setError(error);
-                setLoading(false);
-            }
-        }
-
-        fetchInitialModels()
-
-    },[])
-
-
-    
-
-    return <GPUModelsContext.Provider value={{ 
-            models, 
-            error, 
-            areModelsLoading, 
-        }}>
-        {children}
-    </GPUModelsContext.Provider>
-}
+const debug = Debug("useGPUModels")
 
 function useGPUModels() {
-  const context = React.useContext(GPUModelsContext)
-  if (context === undefined) {
-    throw new Error('useGPUModels must be used within a GPUModelsProvider')
-  }
-  return context
+    const modelsMetadata = useModelsMetadata();
+    debug("modelsMetadata", modelsMetadata);
+
+    const result = useMemo(() => {
+
+        const models = filterModels(modelsMetadata);
+
+        return {
+            models, 
+            error: {}, 
+            areModelsLoading: modelsMetadata === null, 
+        }
+    }, [modelsMetadata]);
+
+    return result;
 }
 
-export { GPUModelsProvider, useGPUModels }
+const filterModels = modelsMetadata => {
+    if (!modelsMetadata)
+        return {};
 
-const MODELS = {
-    "latent-diffusion": "614871946825.dkr.ecr.us-east-1.amazonaws.com/pollinations/latent-diffusion-400m",
-    "envisioning": "614871946825.dkr.ecr.us-east-1.amazonaws.com/pollinations/preset-envisioning",
-    // "pollinations/preset-envisioning": "614871946825.dkr.ecr.us-east-1.amazonaws.com/pollinations/preset-envisioning",
-    // "piano-transcription": "r8.im/bytedance/piano-transcription",
-    "voodoohop/dalle-playground": "614871946825.dkr.ecr.us-east-1.amazonaws.com/voodoohop/dalle-playground",
-    "pollinations/latent-diffusion-400m": "614871946825.dkr.ecr.us-east-1.amazonaws.com/pollinations/latent-diffusion-400m",
-    "pollinations/min-dalle": "614871946825.dkr.ecr.us-east-1.amazonaws.com/pollinations/min-dalle",
-    "kuprel/min-dalle": "r8.im/kuprel/min-dalle",
-    "pollinations/avatarclip": "614871946825.dkr.ecr.us-east-1.amazonaws.com/pollinations/avatarclip",
-    "pollinations/preset-frontpage": "614871946825.dkr.ecr.us-east-1.amazonaws.com/pollinations/preset-frontpage",
-    "pollinations/majesty-diffusion-cog": "614871946825.dkr.ecr.us-east-1.amazonaws.com/pollinations/majesty-diffusion-cog",
-    "jingyunliang/swinir": "r8.im/jingyunliang/swinir@sha256:9d91795e944f3a585fa83f749617fc75821bea8b323348f39cf84f8fd0cbc2f7",
-    "pollinations/disco-diffusion": "614871946825.dkr.ecr.us-east-1.amazonaws.com/pollinations/disco-diffusion",
-    "pollinations/adampi" : "614871946825.dkr.ecr.us-east-1.amazonaws.com/pollinations/adampi",
-    "pollinations/dreamfields-torch": "614871946825.dkr.ecr.us-east-1.amazonaws.com/pollinations/dreamfields-torch",
-    "pollinations/stable-diffusion": "614871946825.dkr.ecr.us-east-1.amazonaws.com/pollinations/stable-diffusion-private"
+    // transform the new metadata format to the old one (extract each entrie's "openapi" prop)
+
+    const filtered_data = zipObj(
+        MODELS, 
+        MODELS
+        // return only the models we want
+        .map(model => modelsMetadata[model] )
+    );
+
+    debug("filtered_data" , filtered_data);
+    return filtered_data;
 }
+
+
+export default useGPUModels;
+
+const MODELS = [
+    "614871946825.dkr.ecr.us-east-1.amazonaws.com/pollinations/latent-diffusion-400m",
+    "614871946825.dkr.ecr.us-east-1.amazonaws.com/pollinations/preset-envisioning",
+    "614871946825.dkr.ecr.us-east-1.amazonaws.com/voodoohop/dalle-playground",
+    "614871946825.dkr.ecr.us-east-1.amazonaws.com/pollinations/latent-diffusion-400m",
+    "614871946825.dkr.ecr.us-east-1.amazonaws.com/pollinations/min-dalle",
+    "614871946825.dkr.ecr.us-east-1.amazonaws.com/pollinations/avatarclip",
+    "614871946825.dkr.ecr.us-east-1.amazonaws.com/pollinations/preset-frontpage",
+    "614871946825.dkr.ecr.us-east-1.amazonaws.com/pollinations/majesty-diffusion-cog",
+    "614871946825.dkr.ecr.us-east-1.amazonaws.com/pollinations/swinir",
+    "614871946825.dkr.ecr.us-east-1.amazonaws.com/pollinations/disco-diffusion",
+    "614871946825.dkr.ecr.us-east-1.amazonaws.com/pollinations/adampi",
+    "614871946825.dkr.ecr.us-east-1.amazonaws.com/pollinations/dreamfields-torch",
+    "614871946825.dkr.ecr.us-east-1.amazonaws.com/pollinations/stable-diffusion-private",
+]

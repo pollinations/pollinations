2a805f616b664bb28dbbc5d8303158943f9e71e9
voodoohop <t.haferlach@gmail.com>
2022-09-01 14:20:31 +0200

    List own gpu models first (#438)
    * reenable sd
    
    * List own models first
    
    * delist dreamfields

--- app/src/hooks/useGPUModels.js
+++ app/src/hooks/useGPUModels.js
@@ -1,61 +1,95 @@
 import Debug from "debug";
-import { zipObj } from 'ramda';
-import { useMemo } from 'react';
+import {
+    zipObj
+} from 'ramda';
+import {
+    useMemo
+} from 'react';
 import useModelsMetadata from './useFetchModelsMetadata';
 
 const debug = Debug("useGPUModels")
 
 function useGPUModels() {
     const modelsMetadata = useModelsMetadata();
-    
+
     debug("models loaded models metadata", modelsMetadata);
 
     const result = useMemo(() => {
 
-        const models = filterModels(modelsMetadata);
+        const models = getModelsWithMetadata(modelsMetadata);
 
         return {
-            models, 
-            error: {}, 
-            areModelsLoading: modelsMetadata === null, 
+            models,
+            error: {},
+            areModelsLoading: modelsMetadata === null,
         }
     }, [modelsMetadata]);
 
     return result;
 }
 
-const filterModels = modelsMetadata => {
+const getModelsWithMetadata = modelsMetadata => {
     if (!modelsMetadata)
         return {};
 
-    // transform the new metadata format to the old one (extract each entrie's "openapi" prop)
 
     const filtered_data = zipObj(
-        MODELS, 
+        MODELS.map(({image}) => image),
         MODELS
-        // return only the models we want
-        .map(model => modelsMetadata[model] )
+        .map(({image, listed}) => ({...modelsMetadata[image], listed}))
     );
-
-    debug("filtered_data" , filtered_data);
+    
     return filtered_data;
 }
 
 
 export default useGPUModels;
 
-const MODELS = [
-    "614871946825.dkr.ecr.us-east-1.amazonaws.com/pollinations/latent-diffusion-400m",
-    "614871946825.dkr.ecr.us-east-1.amazonaws.com/pollinations/preset-envisioning",
-    "614871946825.dkr.ecr.us-east-1.amazonaws.com/voodoohop/dalle-playground",
-    "614871946825.dkr.ecr.us-east-1.amazonaws.com/pollinations/latent-diffusion-400m",
-    "614871946825.dkr.ecr.us-east-1.amazonaws.com/pollinations/min-dalle",
-    "614871946825.dkr.ecr.us-east-1.amazonaws.com/pollinations/avatarclip",
-    "614871946825.dkr.ecr.us-east-1.amazonaws.com/pollinations/preset-frontpage",
-    "614871946825.dkr.ecr.us-east-1.amazonaws.com/pollinations/majesty-diffusion-cog",
-    "614871946825.dkr.ecr.us-east-1.amazonaws.com/pollinations/swinir",
-    //"614871946825.dkr.ecr.us-east-1.amazonaws.com/pollinations/disco-diffusion",
-    "614871946825.dkr.ecr.us-east-1.amazonaws.com/pollinations/adampi",
-    "614871946825.dkr.ecr.us-east-1.amazonaws.com/pollinations/dreamfields-torch",
-    "614871946825.dkr.ecr.us-east-1.amazonaws.com/pollinations/stable-diffusion-private",
+const MODELS = [{
+        listed: true,
+        image: "614871946825.dkr.ecr.us-east-1.amazonaws.com/pollinations/stable-diffusion-private"
+    },
+    {
+        listed: true,
+        image: "614871946825.dkr.ecr.us-east-1.amazonaws.com/pollinations/disco-diffusion"
+    },
+    {
+        listed: true,
+        image: "614871946825.dkr.ecr.us-east-1.amazonaws.com/pollinations/latent-diffusion-400m"
+    },
+    {
+        listed: true,
+        image: "614871946825.dkr.ecr.us-east-1.amazonaws.com/pollinations/latent-diffusion-400m"
+    },
+    {
+        listed: true,
+        image: "614871946825.dkr.ecr.us-east-1.amazonaws.com/pollinations/min-dalle"
+    },
+    {
+        listed: false,
+        image: "614871946825.dkr.ecr.us-east-1.amazonaws.com/pollinations/avatarclip"
+    },
+    {
+        listed: true,
+        image: "614871946825.dkr.ecr.us-east-1.amazonaws.com/pollinations/preset-frontpage"
+    },
+    {
+        listed: true,
+        image: "614871946825.dkr.ecr.us-east-1.amazonaws.com/pollinations/majesty-diffusion-cog"
+    },
+    {
+        listed: true,
+        image: "614871946825.dkr.ecr.us-east-1.amazonaws.com/pollinations/swinir"
+    },
+
+    {
+        listed: true,
+        image: "614871946825.dkr.ecr.us-east-1.amazonaws.com/pollinations/adampi"
+    },
+    {
+        listed: false,
+        image: "614871946825.dkr.ecr.us-east-1.amazonaws.com/pollinations/dreamfields-torch"
+    },
+    // "614871946825.dkr.ecr.us-east-1.amazonaws.com/pollinations/preset-envisioning",
+    // "614871946825.dkr.ecr.us-east-1.amazonaws.com/voodoohop/dalle-playground",
 ]

f26651ffab9cfa08663a86615555bf124e97b6bb
voodoohop <t.haferlach@gmail.com>
2022-09-04 15:44:14 +0200

    reenable alert

--- app/src/App.js
+++ app/src/App.js
@@ -28,6 +28,7 @@ import {
 } from "./routes/publicRoutes"
 import ScrollToTop from './utils/ScrollToTop'
 
+import Alert from "@material-ui/lab/Alert"
 import Showcase3d from "./pages/3dShowcase"
 import CreateModel from './pages/Create/'
 
@@ -57,6 +58,7 @@ const Pollinations = () => {
   }, [node.nodeID])
 
   return ( <>
+      <Alert severity="warning"> Sorry! The bees are crazy today.  We are experiencing an unusually high amount of pollinating at the moment and need a little while to scale up our backend... If possible please try again later.</Alert>
       <TopBar node={node} showNode={navigateToNode} navRoutes={MAIN_NAV_ROUTES} />
 
       {/* Children that get IPFS state */}

81d374975aea845a747741609473cfd6f62efdb9
voodoohop <t.haferlach@gmail.com>
2022-09-04 16:50:12 +0200

    Add less severe warning again
    * reenable sd
    
    * List own models first
    
    * delist dreamfields
    
    * remove disco diffusion
    
    * Alert
    
    * wording
    
    * Add cuter warning
    
    * Update App.js
    
    * remove stable diffusion due to high load
    
    * Revert "remove stable diffusion due to high load"
    
    This reverts commit 4a9dc898b34a610f528a5570de0568a9be6423aa.
    
    * enable disco diffusion
    
    * remove high usage alert
    
    * diable dd again
    
    * Disable feed temporarily
    
    * reenable alert
    
    * Add warning

--- app/src/routes/publicRoutes.js
+++ app/src/routes/publicRoutes.js
@@ -1,7 +1,7 @@
 export const ROUTES = {
     about: { label: "About", to: "/about", id: 'about' },
     integrate: { label: "Integrate", to: "/integrate", id: 'integrate' },
-    feed: { label: "Feed", to: "/feed", id: 'feed' },
+    // feed: { label: "Feed", to: "/feed", id: 'feed' },
     help: { label: "Help", to: "/help", id: 'help' },
     impressum: { label: "impressum", to: "/impressum", id: 'impressum' },
     models: { label: "Create", to: '/c', id: 'create' },
@@ -13,7 +13,7 @@ export const MAIN_NAV_ROUTES = [
     ROUTES.models,
     ROUTES.about, 
     ROUTES.integrate,
-    ROUTES.feed,
+    // ROUTES.feed,
     ROUTES.help
 ]
 export const MARKDOWN_ROUTES = [

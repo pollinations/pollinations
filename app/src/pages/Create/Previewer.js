c39f096574c125b44d94ca0d2737bb0588ffad23
ale-rls <85608868+ale-rls@users.noreply.github.com>
2022-09-06 09:43:22 +0200

    reduce intermediate media size

--- app/src/pages/Create/Previewer.js
+++ app/src/pages/Create/Previewer.js
@@ -64,6 +64,6 @@ width: 100%;
 margin-top: 2em;
 
 display: grid;
-grid-template-columns: repeat(auto-fit, minmax(300px, 1fr));
+grid-template-columns: repeat(auto-fit, minmax(250px, 1fr));
 grid-gap: 0.5em;
 `

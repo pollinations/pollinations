428c9a49320d3cb06ec07d6150facdfa07f2b3dd
ale-rls <85608868+ale-rls@users.noreply.github.com>
2022-09-14 12:45:58 +0200

    hide endless possibilites

--- app/src/pages/Home/index.js
+++ app/src/pages/Home/index.js
@@ -14,7 +14,7 @@ export default function Home() {
     <TryOut />
     <About />
     <MultiplePlatforms />
-    <EndlessPossibilites />
+    {/* <EndlessPossibilites /> */}
     <WhyWeDoIt />
   </HomeStyle>
 }

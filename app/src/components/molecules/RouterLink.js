07b95690f80678ac2f692b70f32c1de27b8efd70
ale-rls <85608868+ale-rls@users.noreply.github.com>
2022-09-15 15:51:38 +0200

    Slogan and NotebookCard (#483)
    * Slogan and NotebookCard
    
    * mobile hero
    
    * category color
    
    * category parse
    
    * abelha e dash
    
    * category color
    
    * bee saturation
    
    * invert background remove section
    
    * revert
    
    * logs on admin only

--- app/src/components/molecules/RouterLink.js
+++ app/src/components/molecules/RouterLink.js
@@ -2,6 +2,6 @@ import { Link as MaterialLink } from '@material-ui/core';
 import React from 'react';
 import { Link } from 'react-router-dom';
 
-export default function RouterLink({ to, children }) {
-  return <MaterialLink component={Link} to={to}>{children}</MaterialLink>;
+export default function RouterLink({ to, children, ...rest }) {
+  return <MaterialLink component={Link} to={to} {...rest}>{children}</MaterialLink>;
 }

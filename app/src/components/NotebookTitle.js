e9c732f3d2f842a4dfed97b854fa9d231cfad6d1
ale-rls <85608868+ale-rls@users.noreply.github.com>
2022-09-24 19:21:57 +0200

    navbar bug and banner (#504)
    * navbar bug and banner
    
    * paddingtop

--- app/src/components/NotebookTitle.js
+++ app/src/components/NotebookTitle.js
@@ -1,11 +1,11 @@
 import { Typography } from "@material-ui/core";
 
 
-const NotebookTitle = ({name, children}) => (name && (
+const NotebookTitle = ({name, children, style}) => (name && (
     <Typography 
         variant="h5" 
         component="h5" 
-        gutterBottom>
+        gutterBottom style={style}>
         { 
             name 
         }

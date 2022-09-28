acf3a0f8081da365dd22883488cfca434f33de3e
ale-rls <85608868+ale-rls@users.noreply.github.com>
2022-09-28 14:41:55 +0200

    new navbar

--- app/src/components/TopBar.js
+++ app/src/components/TopBar.js
@@ -10,7 +10,7 @@ import HamburgerIcon from '@material-ui/icons/Menu'
 import { FlexBetween } from "../styles/classes"
 import Logo from './Logo'
 import NavigationItems from "./organisms/NavigationItems"
-
+import { SocialLinks } from './Social'
 
 const TopBar = ({ navRoutes }) => {
 
@@ -22,13 +22,15 @@ const TopBar = ({ navRoutes }) => {
     <TopContainer css={StyleUrl(location.pathname)}>
     {/* <Alert severity="info">So much pollinating going on that diffusion may be unstable for a little while. Join our <a href='https://discord.gg/XXd99CrkCr'>discord</a> for a chat, help or updates.</Alert> 
      */}
-      <NavBarStyle>
-      <NavLink to='/' style={{ padding: 0 }}>
+    <NavBarStyle>
+      <NavLink to='/' style={{ padding: 0, gridArea: 'logo', display: 'flex', alignItems: 'center', marginLeft: '1em' }}>
         <Logo size='150px' small='150px' margin='0' />  
       </NavLink>
 
       <NavigationItems navRoutes={navRoutes}/>
       
+
+      <SocialLinks small/>
 
       <MenuButton>
         <IconButton onClick={()=>drawerState[1](true)} >
@@ -48,21 +50,39 @@ const TopBar = ({ navRoutes }) => {
 const TopContainer = styled.div`
   ${props => props.css};
   width: 100%;
-  `
-  const NavBarStyle = styled.div`
-  ${FlexBetween}
+`
+
+const NavBarStyle = styled.div`
+
+  display: grid;
+  grid-template-columns: 1fr 3fr 1fr;
+  grid-template-rows: auto;
+  grid-template-areas: "logo nav social";
+  align-content: center;
+  gap: 1em;
+
   font-size: 1rem;
   .MuiTypography-colorPrimary{
-  color: #fdfdfd !important;  
+    color: #fdfdfd !important;  
   }
-  padding: ${GlobalSidePadding}
+  padding: ${GlobalSidePadding};
+  padding: 1% 3%;
+  @media (max-width: ${MOBILE_BREAKPOINT}){
+    grid-template-areas: "logo  nav mobilebutton social";
+  }
 `
 const MenuButton = styled.div`
+grid-area: mobilebutton;
+justify-self: flex-end;
 @media only screen and (min-width: ${MOBILE_BREAKPOINT}){
   display: none;
 }
 `
-
+const LogoStyle = styled.div`
+@media only screen and (max-width: ${MOBILE_BREAKPOINT}){
+  display: none;
+}
+`
 const StyleUrl = (url) => {
   if (url?.slice(0,2) === '/c') return `position: relative;`;
   if (url?.slice(0,2) === '/p') return `position: relative;`;

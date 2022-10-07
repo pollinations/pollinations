f9fd4f891ce0b27228b2b34ddb229b224242c359
ale-rls <85608868+ale-rls@users.noreply.github.com>
2022-10-07 11:31:27 +0200

    ColabCreate (#522)
    fixes issue #379

--- app/src/components/molecules/BigPreview.js
+++ app/src/components/molecules/BigPreview.js
@@ -1,11 +1,9 @@
 import { MediaViewer } from "../MediaViewer"
+import Typography from '@material-ui/core/Typography'
 
 const BigPreview = ({ filename, url, type }) => {
-
-  
-    if (!url) return <div style={{minHeight:'80vh'}}>Processing...</div>
-
     return <MediaViewer filename={filename} content={url} type={type} style={{ width: 'calc(100vh - 90px)', maxWidth: '100%'}} />
 }
 
 export default BigPreview
+

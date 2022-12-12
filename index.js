c7b3637501314090640ba1897d5b8cde7ae78b89
voodoohop <t.haferlach@gmail.com>
2022-12-12 19:49:40 +0100

    Retry on failure

--- index.js
+++ index.js
@@ -32,15 +32,22 @@ const requestListener = async function (req, res) {
 
   const prompt = urldecode(promptRaw).replaceAll("_", " ");
 
+  let url = null;
   
-  const output = await runModel( {
-    prompts: prompt, 
-    num_frames_per_prompt: 1,  
-    diffusion_steps: 15,
-    // seed: seed || 0
-  }, "614871946825.dkr.ecr.us-east-1.amazonaws.com/pollinations/stable-diffusion-private",false, {priority: 5})
+  while (!url) {
+  
+    const output = await runModel( {
+      prompts: prompt,
+      num_frames_per_prompt: 1,
+      diffusion_steps: 15,
+      seed: Math.round(Math.random() * 1000000000)
+      // seed: seed || 0
+    }, "614871946825.dkr.ecr.us-east-1.amazonaws.com/pollinations/stable-diffusion-private",false, {priority: 5})
 
-  const url = output?.output["00003.png"];
+    url = output?.output["00003.png"];
+
+  }
+  
   console.log("Showing image: ", url);
   // await showImage(url);
 

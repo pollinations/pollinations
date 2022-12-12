e29932d02267f415135ac9bb9bb13399abd23e7a
voodoohop <t.haferlach@gmail.com>
2022-12-12 19:25:14 +0100

    higher priority

--- index.js
+++ index.js
@@ -32,12 +32,13 @@ const requestListener = async function (req, res) {
 
   const prompt = urldecode(promptRaw).replaceAll("_", " ");
 
+  
   const output = await runModel( {
     prompts: prompt, 
     num_frames_per_prompt: 1,  
     diffusion_steps: 15,
     // seed: seed || 0
-  }, "614871946825.dkr.ecr.us-east-1.amazonaws.com/pollinations/stable-diffusion-private")
+  }, "614871946825.dkr.ecr.us-east-1.amazonaws.com/pollinations/stable-diffusion-private",false, {priority: 5})
 
   const url = output?.output["00003.png"];
   console.log("Showing image: ", url);

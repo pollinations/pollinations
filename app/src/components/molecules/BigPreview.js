5089ae95e82808d117de30368b90c7406c4702f0
ale-rls <85608868+ale-rls@users.noreply.github.com>
2022-12-15 18:43:39 +0100

    cleanup

--- app/src/components/molecules/BigPreview.js
+++
@@ -1,8 +0,0 @@
-import { MediaViewer } from "../MediaViewer"
-
-const BigPreview = ({ filename, url, type }) => {
-    return <MediaViewer filename={filename} content={url} type={type} style={{ width: 'calc(100vh - 90px)', maxWidth: '100%'}} />
-}
-
-export default BigPreview
-

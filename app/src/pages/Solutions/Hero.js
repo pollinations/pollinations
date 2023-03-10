dcdf213e18a664fff5e11b498ceb7417398bc443
ale-rls <85608868+ale-rls@users.noreply.github.com>
2023-03-10 12:50:45 +0100

    twitch tesdt

--- app/src/pages/Solutions/Hero.js
+++ app/src/pages/Solutions/Hero.js
@@ -3,6 +3,7 @@ import heroBG from '../../assets/imgs/yellow-grad.png';
 import scrolldown from '../../assets/imgs/scrolldown_hero_landing.png';
 import { MOBILE_BREAKPOINT, Colors, Fonts } from '../../styles/global';
 import { BackgroundImage, Container as ContainerBase } from './components';
+import Player from './Player';
 
 
 const Hero = props => <Style>
@@ -12,7 +13,10 @@ const Hero = props => <Style>
     </Headline>
       
   </Container>
-
+  <PlayerWrapper>
+  {/* <iframe width="100%" height="100%" src="https://www.youtube.com/embed/HHaxu013Jfo?controls=0&modestbranding=1&autoplay=1&loop=1" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" allowfullscreen loop autoplay muted></iframe> */}
+  <Player src='https://www.twitch.tv/watchmeforever' />
+  </PlayerWrapper>
   <BackgroundImage 
     src='bgHero.png' 
     zIndex='-2' 
@@ -21,6 +25,14 @@ const Hero = props => <Style>
 </Style>;
   
 export default Hero
+const PlayerWrapper = styled.div`
+position: absolute;
+left: 0;
+right: 0;
+top: 0;
+bottom: 0;
+z-index: -1;
+`
 const Style = styled.div`
 width: 100%;
 height: 100%;
---
+++ app/src/pages/Solutions/Player.js
@@ -0,0 +1,15 @@
+import React from 'react'
+import ReactPlayer from 'react-player'
+
+// Only loads the YouTube player
+export default function Player({ src }){
+if (!src) return <h1>no source</h1>
+return <ReactPlayer 
+config={{
+    youtube: {
+      playerVars: { showinfo: 1, autoplay: 1, muted: 1, modestbranding: 1 }
+    },
+  }}
+  playing={true}
+url={src} width='100%' height='100%' autoplay muted playsinline />
+}

1a67985ea676194b6150df6b8fcc25341dba326d
ale-rls <85608868+ale-rls@users.noreply.github.com>
2023-05-16 12:33:29 +0200

    Features/website2023 (#566)
    * bring solutions page in
    
    * solution page imgs
    
    * solutions discord comment out
    
    * bgs
    
    * v1
    
    * dreamachine section
    
    * dreamachine
    
    * swiper dreamachine
    
    * music video section
    
    * twitch tesdt
    
    * streamable
    
    * loop
    
    * Slight text changes
    
    * last changes
    
    * last changes
    
    * mega cleanup
    
    * video URL change
    
    * video fix
    
    * Fix info
    
    ---------
    
    Co-authored-by: voodoohop <t.haferlach@gmail.com>

--- app/src/hooks/useIsAdmin.js
+++
@@ -1,5 +0,0 @@
-import useLocalStorage from './useLocalStorage';
-
-export function useIsAdmin() {
-    return useLocalStorage('isAdmin', false);
-}
--- app/src/routes/publicRoutes.js
+++ app/src/routes/publicRoutes.js
@@ -1,25 +1,25 @@
 export const ROUTES = {
-    about: { label: "About", to: "/about", id: 'about' },
+    // about: { label: "About", to: "/about", id: 'about' },
     // integrate: { label: "Integrate", to: "/integrate", id: 'integrate' },
     // feed: { label: "Feed", to: "/feed", id: 'feed' },
-    help: { label: "Help", to: "/help", id: 'help' },
+    // help: { label: "Help", to: "/help", id: 'help' },
     impressum: { label: "impressum", to: "/impressum", id: 'impressum' },
-    explore: { label: "Explore", to: '/c', id: 'explore' },
+    // explore: { label: "Explore", to: '/c', id: 'explore' },
     // integrate: { label: "integrate", to: "/integrate" },
     // myPollens: { label: "my pollens", to: "/localpollens" },
     // expo: { children: "made with pollinations", to: "/expo" },
   }
 export const MAIN_NAV_ROUTES = [
-    ROUTES.explore,
+    // ROUTES.explore,
     // ROUTES.integrate,
-    ROUTES.about, 
+    // ROUTES.about, 
     // ROUTES.feed,
     // ROUTES.help
 ]
 export const MARKDOWN_ROUTES = [
     // ROUTES.about,
     // ROUTES.integrate,
-    ROUTES.help,
+    // ROUTES.help,
     ROUTES.impressum
 ]
 

40ff2680d0f982170bdd7c6f975bcb1ef58dad44
thomas & niels <thomas.haferlach@pixelynx.io>
2023-06-05 02:18:28 +0000

    changes

---
+++ cacheGeneratedImages.js
@@ -0,0 +1,44 @@
+import fs from 'fs';
+import crypto from 'crypto';
+
+export const cacheGeneratedImages = (imageGeneratorFn, saveFolder = "/tmp/stableDiffusion_cache") => {
+  // create folder if it doesn't exist
+
+  if (!fs.existsSync(saveFolder)) {
+    fs.mkdirSync(saveFolder);
+  }
+
+  return async  (prompt, ...args) => {
+    // const sanitizedPrompt = prompt.replace(/[^a-zA-Z0-9]/g, "_");
+    // allow foreign language characters
+    const sanitizedPrompt = prompt.replaceAll("/", "_").replaceAll(" ", "_")
+      .replaceAll("?", "_").replaceAll("!", "_").replaceAll(":", "_")
+      .replaceAll(";", "_").replaceAll("(", "_").replaceAll(")", "_")
+      .replaceAll("’", "_").replaceAll("“", "_").replaceAll("”", "_")
+      .replaceAll("‘", "_").replaceAll("…", "_").replaceAll("—", "_")
+      .slice(0, 50);
+    
+    // calculate 4 byte hash of prompt
+
+    const hash = crypto.createHash('md5').update(prompt).digest("hex").slice(0, 4);
+
+    // create a filename from the prompt
+    const path = saveFolder + "/" + sanitizedPrompt+"_" + hash + ".png";
+    // if file exists return it
+    if (fs.existsSync(path)) {
+      console.log("file exists, returning it", path);
+      // read file
+      return fs.readFileSync(path);
+    }
+
+    // generate image
+    const buffer = await imageGeneratorFn(prompt, ...args);
+
+    // write buffer to file
+    console.log("writing file", path);
+    fs.writeFileSync(path, buffer);
+
+    return buffer;
+  };
+
+};
--- connect_reverse_ssh.sh
+++
@@ -1,3 +0,0 @@
-#!/bin/bash
-
-ssh -f -N -L 7860:localhost:7860 -i $HOME/credentials/dev-key.pem ubuntu@ec2-44-193-30-178.compute-1.amazonaws.com

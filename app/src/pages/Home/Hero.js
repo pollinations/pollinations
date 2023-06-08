3dcd156f8af7dc3c3eb212c20f355b1ce68a64fc
voodoohop <t.haferlach@gmail.com>
2023-06-08 03:54:13 +0100

    Show gen image URL feed again

--- app/src/pages/Home/Hero.js
+++ app/src/pages/Home/Hero.js
@@ -2,6 +2,9 @@ import styled from '@emotion/styled';
 import { MOBILE_BREAKPOINT, Colors, Fonts } from '../../styles/global';
 import { BackgroundImage, Container as ContainerBase } from './components';
 import Player from './Player';
+import { useEventSource, useEventSourceListener } from "@react-nano/use-event-source";
+import { useState } from 'react';
+import { isMature } from '../../data/mature';
 
 
 const Hero = props => <Style>
@@ -13,15 +16,72 @@ const Hero = props => <Style>
   </Container> */}
   <HeroContainer>
     <Player src='https://streamable.com/w9p5rz' />
+    <GenerativeImageFeed />
   </HeroContainer>
-  <BackgroundImage 
+  {/* <BackgroundImage 
     src='bgHero.png' 
     zIndex='-2' 
-    alt="hero_bg" />
+    alt="hero_bg" /> */}
 
 </Style>;
   
-export default Hero
+export default Hero;
+
+
+function GenerativeImageFeed() {
+  const [image, setImage] = useState(null);
+  const [nextPrompt, setNextPrompt] = useState("");
+  const [prompt, setPrompt] = useState("");
+  const [serverLoad, setServerLoad] = useState(0);
+  
+  const [eventSource, eventSourceStatus] = useEventSource("https://image.pollinations.ai/feed", true);
+  useEventSourceListener(eventSource, ['message'], evt => {
+    const data = JSON.parse(evt.data);
+    if (data["imageURL"]) {
+      if (isMature(data["prompt"])) {
+        console.log("skipping mature prompt: ", data["prompt"]);
+        return;
+      }
+      setImage(data);
+      setNextPrompt(data["prompt"])
+    }
+
+    setServerLoad(data["concurrentRequests"]);
+  }, [setImage, setServerLoad]);
+
+  return (
+      <div>
+        <br /><br /><br /><br /><br />
+        <GenerativeImageURLContainer>
+        <h2>Generative Image URL feed</h2>
+          {eventSourceStatus === "open" ? null : <div>connecting...</div>}
+          {image && <div>
+                      <img src={image["imageURL"]} alt="generative_image" onLoad={() => {
+                        setPrompt(nextPrompt);
+                        console.log("loaded image. setting prompt to: ", nextPrompt)
+                      }} /> 
+                      <br/>
+                      Prompt: <b>{prompt}</b>
+                    </div>
+          }
+          </GenerativeImageURLContainer>
+      </div>
+  );
+}
+
+// try to vertically and horizontally center content
+const GenerativeImageURLContainer = styled.div`
+  background-color: rgba(0,0,0,0.7);
+  color: white;
+
+  position: absolute;
+  top: 50%;
+  left: 50%;
+  transform: translate(-50%, -50%);
+  padding: 2em;
+
+`;
+
 const HeroContainer = styled.div`
   position: relative;
   height: 100vh;

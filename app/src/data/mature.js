4c82aebc2864db184e8f9b3c2903d8e7b31c9c28
voodoohop <t.haferlach@gmail.com>
2023-06-09 13:06:28 +0100

    Features/gen image url feed (#573)
    * Show gen image URL feed again
    
    * Show server load
    
    * More robust mature word filtering
    
    * Squashed commit of the following:
    
    commit 3bfaa7a721382469658659eabc775035a0efce5f
    Author: voodoohop <t.haferlach@gmail.com>
    Date:   Thu Jun 8 04:13:47 2023 +0100
    
        Features/gen image url feed (#567)
    
        * Show gen image URL feed again
    
        * Show server load
    
    * Improve mature words fitler
    
    * Styling changes
    
    * Update matureWords.js
    
    * Update matureWords.js
    
    * Update matureWords.js
    
    * Update matureWords.js
    
    * Update matureWords.js
    
    * More responsive styling
    
    * Update matureWords.js
    
    * Full word boundaries for mature word filtering
    
    * Improvements
    
    * implement eventsource myself
    
    * fixes

--- app/src/data/mature.js
+++ app/src/data/mature.js
@@ -21,6 +21,7 @@ import { words } from "./matureWords.js";
 
 const wordsSet = new Set(words);
 export const isMature = raw_input => { 
-    const inputWords = raw_input.toLowerCase().split(/\b/);
+    const inputWords = raw_input.toLowerCase().replaceAll("_"," ").split(/\b/);
     return inputWords.find(inputWord => wordsSet.has(inputWord));
 };
+

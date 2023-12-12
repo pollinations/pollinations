5bf3ffac7447de19b67d592a58d3add098ad4288
flowa <152752280+flowa-ai@users.noreply.github.com>
2023-12-11 23:31:39 -0500

    Create __init__.py

---
+++ pollinations/types/__init__.py
@@ -0,0 +1,14 @@
+"""
+pollinations.types
+
+Classes:
+    ImageModel (types.ImageModel): Text-to-image generative AI model.
+    TextModel (types.TextModel): Text-to-text generative AI model.
+    ImageObject (types.ImageObject): Image object.
+    TextObject (types.TextObject): Text object.
+"""
+
+from .ImageModel import ImageModel
+from .TextModel import TextModel
+from .ImageObject import ImageObject
+from .TextObject import TextObject

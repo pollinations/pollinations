149cc6db67dae35617ca728b5d2579da424f77b9
Thomas Haferlach <thomas.haferlach@pixelynx.io>
2024-04-02 14:59:57 +0200

    - Move video to top of event.md and adjust size - Add media query to adjust video position based on screen size in MarkDownContent.js - Remove max-width from SmallContainer in global.js

--- app/src/components/MarkDownContent.js
+++ app/src/components/MarkDownContent.js
@@ -3,6 +3,8 @@ import Markdown from "markdown-to-jsx"
 import { range, zipObj } from "ramda"
 import useFetchText from "../hooks/useFetchText"
 import useMarkdown from "../hooks/useMarkdown"
+import { useTheme } from '@material-ui/core/styles';
+import useMediaQuery from '@material-ui/core/useMediaQuery';
 
 // replacements allow replacing dynamic content in the markdown
 // the syntax is {[key]} which will be matched with the props passed to this object
@@ -11,6 +13,8 @@ const MarkDownContent = ({ url, ...replacements }) => {
   const raw = useFetchText(url)
   const { body } = useMarkdown(raw)
   const headersToInclude = range(1, 7)
+  const theme = useTheme();
+  const isDesktop = useMediaQuery(theme.breakpoints.up('md'));
 
   // header tags
   const tags = headersToInclude.map((i) => `h${i}`)
@@ -20,7 +24,19 @@ const MarkDownContent = ({ url, ...replacements }) => {
     <Typography variant={tag} children={children} />
   ))
 
-  const overrides = zipObj(tags, overrideElements)
+  let overrides = zipObj(tags, overrideElements);
+
+  overrides = {
+    ...overrides,
+    video: ({ src, controls, width, height, children, ...props }) => {
+      const style = isDesktop ? (props.style ? { float: 'right', ...props.style } : { float: 'right' }) : {};
+      return (
+        <video src={src} controls={controls} width={width} height={height} style={style} {...props}>
+          {children}
+        </video>
+      );
+    }
+  };
 
   const contentWithReplacements = applyReplacements(replacements, body)
 

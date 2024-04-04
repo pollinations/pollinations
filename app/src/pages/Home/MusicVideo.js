ee856d511c7b466f155013eae5b062012ff795d7
Thomas Haferlach <thomas.haferlach@pixelynx.io>
2024-04-04 03:44:58 +0200

    Rever MusicVideo.js

--- app/src/pages/Home/MusicVideo.js
+++ app/src/pages/Home/MusicVideo.js
@@ -1,89 +1,39 @@
 import styled from '@emotion/styled'
 import { useNavigate } from 'react-router-dom'
-import { Typography, ButtonGroup, Grid, Link, Box, Paper, Table, TableBody, TableCell, TableRow, TextField, CircularProgress, TableContainer, Checkbox, Tooltip, IconButton, Collapse, Button, Tabs, Tab } from '@material-ui/core';
 import { MOBILE_BREAKPOINT, Colors, Fonts } from '../../styles/global';
-import { GenerativeImageURLContainer, ImageURLHeading, ImageContainer, ImageStyle } from './styles';
 import { BackgroundImage, Container as ContainerBase, Flex } from './components';
 import SwiperComponent from './Swiper';
+// import Swiper styles
+
 import Slider from "react-slick";
 import '../../assets/slick.min.css'
 
+const FeaturedApplicationsContent = [
+    {
+        title: 'AI Video',
+        description: <>
+            Dive into the world of AI-driven music videos crafted specifically for artists, installations, and events. We specialize in creating bespoke videos that bring your artistic visions to life.
+            <br /> <br />Ideal for musicians, event organizers, and visual artists seeking to enhance their projects with unique, compelling visuals.
+            <br /> <br />Let us help you transform your psychedelic ideas into stunning visual narratives.
+        </>,
+    },
+]
+
 const MusicVideo = props => {
-    const videoStyles = {
-        width: '100%',
-        height: 'auto',
-        aspectRatio: '16 / 9',
-    };
 
-    return <Flex display="flex" AlignItems='center'>
-        <ImageURLHeading>AI VIDEO</ImageURLHeading>
-        <PageLayout>
-            <h2>Dreamt it?</h2>
-            <h2 style={{color: Colors.lime}}>We visualize it.</h2>
-        </PageLayout>
-        <GridTwoColumns container style={{ flexWrap: 'wrap'}}>
-            <div style={videoStyles}>
-                <iframe style={{ width: '100%', height: '100%' }} src="https://www.youtube-nocookie.com/embed/HXCd1jmlL-g?si=FTz5JLj7FA8-dpZ9&amp;controls=0" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" allowfullscreen></iframe>
-            </div>
-            <div style={videoStyles}>
-                <iframe style={{ width: '100%', height: '100%' }} src="https://www.youtube-nocookie.com/embed/k_W8UtOO6vQ?si=dYDFG5nHTrXpGfId&amp;controls=0" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" allowfullscreen></iframe>
-            </div>
-        </GridTwoColumns>
-    </Flex>
+    return <Style>
+
+        <Container>
+
+
+            {
+                FeaturedApplicationsContent.map((item, idx) => <FeaturedApp {...item} right={idx % 2} />)
+            }
+
+        </Container>
+
+    </Style>
 }
-
-const FeaturedAppStyle = styled.div`
-p {
-    font-family: 'Uncut-Sans-Variable';
-    font-style: normal;
-    font-size: 24px;
-    color: ${Colors.offwhite};
-    @media (max-width: ${MOBILE_BREAKPOINT}) {
-        max-width: 600px;
-        font-size: 18px;
-        line-height: 18px;
-    }
-}
-`
-// STYLES
-const PageLayout = styled.div`
-
-h2 {
-  font-family: 'Uncut-Sans-Variable';
-  font-style: normal;
-  font-weight: 400;
-  font-size: 36px;
-  display: flex;
-  justify-content: center;
-  color: ${Colors.offwhite};
-  margin: 0;
-  @media (max-width: ${MOBILE_BREAKPOINT}) {
-    display: flex;
-    justify-content: center;
-    width: 100%;
-    font-size: 36px;
-  }
-}`;
-
-const GridTwoColumns = styled.div`
-align-self: center;
-display: grid;
-grid-template-columns: 1fr 1fr;
-gap: 1em;
-margin: 5em 0;
-margin-bottom:6em;
-
-@media (max-width: ${MOBILE_BREAKPOINT}) {
-  grid-template-columns: repeat(1, 1fr);
-  width: 95%;
-}
-
-@media (min-width: ${MOBILE_BREAKPOINT}) {
-  grid-template-columns: repeat(2, 1fr);
-  width: 150%;
-  gap: 2em;
-}
-`;
 
 export default MusicVideo
 
@@ -98,8 +48,11 @@ const Style = styled.div`
   height: 100%;
   position: relative;
   z-index:0;
+
   display: flex;
   justify-content: center;
+  padding: 1em 0;
+  padding-bottom: 5em;
   `
 
 const Headline = styled.p`
@@ -109,6 +62,9 @@ const Headline = styled.p`
   font-size: 96px;
   line-height: 105px;
   text-transform: capitalize;
+
+  margin: 0;
+  margin-top: 1em;
   color: ${Colors.offwhite};
 
   span {
@@ -120,16 +76,21 @@ const Headline = styled.p`
     max-width: 600px;
     font-size: 58px;
     line-height: 55px;
+    margin: 0;
+    margin-top: 1em;
 }`
-
 const SubHeadline = styled.p`
     font-style: normal;
     font-weight: 400;
     font-size: 32px;
     line-height: 30px;
     text-align: center;
+
+    /* lime */
     color: ${Colors.offblack};
+
     margin: 0;
+    margin-bottom: 2em;
     @media (max-width: ${MOBILE_BREAKPOINT}) {
         max-width: 600px;
         font-size: 18px;
@@ -148,26 +109,95 @@ const FeaturedApp = props => {
 
     const { title, subtitle, description, img, right, imgs, video } = props;
 
+
+
     const Media = props => {
         if (props.img) return <FeatureAppImg src={img} />;
         if (props.imgs) return <SlickSlider imgs={imgs} pad={title !== 'Busy Bee'} />;
         if (props.video) return <FeatureAppVideo src={video} playsInline autoPlay muted />;
         return <></>;
     }
+
+    return <Flex AlignItems='center' gap='5em'>
+        <Headline>
+            {title}
+        </Headline>
+        <SubHeadline>
+            {subtitle}
+        </SubHeadline>
+        <GridTwoColumns>
+
+            <FeaturedAppStyle>
+                <p>
+                    {description}
+                </p>
+            </FeaturedAppStyle>
+            {/* <Media {...props}/> */}
+            {/* <SwiperComponent Slides={Slides}/> */}
+            <iframe width="75%" height="540" src="https://www.youtube-nocookie.com/embed/HXCd1jmlL-g?si=FTz5JLj7FA8-dpZ9&amp;controls=0" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" allowfullscreen></iframe>
+            <iframe width="75%" height="540" src="https://www.youtube-nocookie.com/embed/k_W8UtOO6vQ?si=dYDFG5nHTrXpGfId&amp;controls=0" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" allowfullscreen></iframe>
+        </GridTwoColumns>
+    </Flex>
 }
 
+const FeaturedAppStyle = styled.div`
+display: flex;
+flex-direction: column;
+align-items: flex-start;
+padding: 0 40px;
 
+p {
+    align-self: flex-start;
+    font-family: 'Uncut-Sans-Variable';
+    font-style: normal;
+    font-weight: 400;
+    font-size: 24px;
+    line-height: 34px;
+    margin: 0;
+
+    color: ${Colors.offwhite};
+    @media (max-width: ${MOBILE_BREAKPOINT}) {
+        max-width: 600px;
+        font-size: 18px;
+        line-height: 18px;
+        margin: 0;
+        text-align: center;
+    }
+}
+`
 const FeatureAppImg = styled.img`
+// max-width: 500px;
 width: 100%;
 height: auto;
+// padding: 20px;
 border-radius: 0px;
 `
 const FeatureAppVideo = styled.video`
+// max-width: 500px;
 width: 100%;
 height: auto;
 border-radius: 0px;
 `
-    ;
+
+const GridTwoColumns = styled.div`
+width: 100%;
+// max-width: 1110px;
+align-self: center;
+
+display: grid;
+grid-template-columns: 1fr;
+gap: 1em;
+margin: 2em 0;
+
+iframe {
+    margin: 3em auto;
+    padding: 2em 0;
+}
+
+@media (max-width: ${MOBILE_BREAKPOINT}) {
+  grid-template-columns: 1fr;
+}
+`;
 
 
 

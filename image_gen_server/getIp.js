eb072fe6e10df8e6059c494beaa810218afd0d9b
lauraibnz <lauraim1995@gmail.com>
2024-04-10 04:03:03 +0000

    cleanup queueing functionality

---
+++ image_gen_server/getIp.js
@@ -0,0 +1,3 @@
+export function getIp(req) {
+  return req.headers["x-bb-ip"] || req.headers["x-nf-client-connection-ip"] || req.headers["x-real-ip"] || req.headers['x-forwarded-for'] || req.socket.remoteAddress;
+}

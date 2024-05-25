bffaf3b0cc68f60e5b415187635c17f2b728c444
lauraibnz <lauraim1995@gmail.com>
2024-05-25 15:41:54 +0000

    Refactor image generation code and adjust server settings
    - Refactor image generation code in `predict.py` to use different pipelines based on the number of steps
    - Remove unused imports and commented out code
    - Adjust server settings in `generalImageQueue.js` to reduce batch size and concurrency
    - Update queue handling in `index_latent_consistency.js` to bypass queue for bot requests
    - Update table logs in `tableLogs.txt`

--- image_gen/predict.py
+++ image_gen/predict.py
@@ -4,46 +4,36 @@ import torch
 from transformers import T5Tokenizer, T5ForConditionalGeneration
 from flask import Flask, request, jsonify
 from diffusers import (
-    AutoPipelineForText2Image, 
-    StableDiffusionPipeline,
-    EulerAncestralDiscreteScheduler,
-    PixArtAlphaPipeline,
-    ConsistencyDecoderVAE,
-    LCMScheduler,
-    DPMSolverSDEScheduler,
-    StableDiffusionXLPipeline,
-    StableDiffusionImg2ImgPipeline,
-    StableDiffusionXLImg2ImgPipeline,
     DiffusionPipeline,
-    UNet2DConditionModel,
     AutoencoderTiny,
-    UniPCMultistepScheduler
+    DDIMScheduler,
+    EulerAncestralDiscreteScheduler,
+    EulerDiscreteScheduler,
+    StableDiffusionXLPipeline,
+    TCDScheduler,
+    DPMSolverSDEScheduler
 )
+from safetensors.torch import load_file
+
+from huggingface_hub import hf_hub_download
 import sys
 from typing import Literal, Dict, Optional
+from hidiffusion import apply_hidiffusion, remove_hidiffusion
+
+# from sfast.compilers.diffusion_pipeline_compiler import (compile,
+#                                                          CompilationConfig)
 sys.path.append(os.path.join(os.path.dirname(__file__), "StreamDiffusion"))
 
-T_INDEX_LIST=[0, 10, 30, 45]
-n_steps=50
-# from streamdiffusion import StreamDiffusion
-# from streamdiffusion.image_utils import postprocess_image
 
-from utils.wrapper_xl import StreamDiffusionWrapper
 
-# from sfast.compilers.stable_diffusion_pipeline_compiler import compile, CompilationConfig
 from collections import defaultdict
 from performance_metrics import add_timestamp, get_average_generation_duration, calculate_throughput, get_timestamps
 import uuid
 import threading
 from transformers import T5EncoderModel
-import torch
 import re
 from safety_checker.censor import check_safety
 
-
-
-import os
-import torch
 import torch.nn as nn
 from os.path import expanduser  # pylint: disable=import-outside-toplevel
 from urllib.request import urlretrieve  # pylint: disable=import-outside-toplevel
@@ -59,59 +49,121 @@ app = Flask(__name__)
 # create lock
 lock = threading.Lock()
 
+def get_boltning_pipe() -> DiffusionPipeline:
+    pipe = DiffusionPipeline.from_pretrained(
+        "./boltning_diffusers", 
+        torch_dtype=torch.float16, 
+        variant="fp16"
+    ).to("cuda")
+    pipe.vae = AutoencoderTiny.from_pretrained("madebyollin/taesdxl").to(
+        device=pipe.device, dtype=pipe.dtype
+    )
+    pipe.scheduler = EulerAncestralDiscreteScheduler.from_config(pipe.scheduler.config, timestep_spacing="trailing")
+    return pipe
 
+def get_lightning_pipe(steps:int = 4) -> DiffusionPipeline:
+    base_model_id = "./zavychromaxl7"
+    # base_model_id = "stabilityai/stable-diffusion-xl-base-1.0"
+
+    # repo_name = "ByteDance/Hyper-SD"
+    # ckpt_name = f"Hyper-SDXL-{steps}steps-lora.safetensors"
+    
+    ckpt_name = f"sdxl_lightning_{steps}step_lora.safetensors" # Use the correct ckpt for your step setting!
+    repo_name = "ByteDance/SDXL-Lightning"
+    pipe = DiffusionPipeline.from_pretrained(base_model_id, torch_dtype=torch.float16, variant="fp16").to("cuda")
+    pipe.load_lora_weights(hf_hub_download(repo_name, ckpt_name))
+    pipe.fuse_lora()
+    # pipe.scheduler = EulerDiscreteScheduler.from_config(pipe.scheduler.config, timestep_spacing="trailing")
+    # try DPMSolverSDEScheduler
+    pipe.scheduler = DPMSolverSDEScheduler.from_config(pipe.scheduler.config)
+    # pipe.scheduler = DDIMScheduler.from_config(pipe.scheduler.config, timestep_spacing="trailing")
+
+    # # Load resadapter for baseline
+    # resadapter_model_name = "resadapter_v2_sdxl"
+    # pipe.load_lora_weights(
+    #     hf_hub_download(repo_id="jiaxiangc/res-adapter", subfolder=resadapter_model_name, filename="pytorch_lora_weights.safetensors"), 
+    #     adapter_name="res_adapter",
+    #     ) # load lora weights
+    # pipe.set_adapters(["res_adapter"], adapter_weights=[1.0])
+    # pipe.unet.load_state_dict(
+    #     load_file(hf_hub_download(repo_id="jiaxiangc/res-adapter", subfolder=resadapter_model_name, filename="diffusion_pytorch_model.safetensors")),
+    #     strict=False,
+    #     ) # load norm weights
+
+    return pipe
+
+def get_hyper_pipe(steps:int = 4) -> DiffusionPipeline:
+    # base_model_id = "./zavychromaxl7"
+    base_model_id = "stabilityai/stable-diffusion-xl-base-1.0"
+
+    repo_name = "ByteDance/Hyper-SD"
+    ckpt_name = f"Hyper-SDXL-{steps}steps-lora.safetensors"
+    
+    # ckpt_name = f"sdxl_lightning_{steps}step_lora.safetensors" # Use the correct ckpt for your step setting!
+    # repo_name = "ByteDance/SDXL-Lightning"
+    pipe = DiffusionPipeline.from_pretrained(base_model_id, torch_dtype=torch.float16, variant="fp16").to("cuda")
+    pipe.load_lora_weights(hf_hub_download(repo_name, ckpt_name))
+    pipe.fuse_lora()
+    # pipe.scheduler = EulerDiscreteScheduler.from_config(pipe.scheduler.config, timestep_spacing="trailing")
+    # try DPMSolverSDEScheduler
+    # pipe.scheduler = DPMSolverSDEScheduler.from_config(pipe.scheduler.config)
+    pipe.scheduler = DDIMScheduler.from_config(pipe.scheduler.config, timestep_spacing="trailing")
+
+    # # Load resadapter for baseline
+    # resadapter_model_name = "resadapter_v2_sdxl"
+    # pipe.load_lora_weights(
+    #     hf_hub_download(repo_id="jiaxiangc/res-adapter", subfolder=resadapter_model_name, filename="pytorch_lora_weights.safetensors"), 
+    #     adapter_name="res_adapter",
+    #     ) # load lora weights
+    # pipe.set_adapters(["res_adapter"], adapter_weights=[1.0])
+    # pipe.unet.load_state_dict(
+    #     load_file(hf_hub_download(repo_id="jiaxiangc/res-adapter", subfolder=resadapter_model_name, filename="diffusion_pytorch_model.safetensors")),
+    #     strict=False,
+    #     ) # load norm weights
+
+    return pipe
+
+
+def get_tcd_pipe(steps: int = 4) -> DiffusionPipeline:
+    device = "cuda"
+    base_model_id = "./zavychromaxl7"
+    tcd_lora_id = "h1t/TCD-SDXL-LoRA"
+    
+    pipe = StableDiffusionXLPipeline.from_pretrained(base_model_id, torch_dtype=torch.float16, variant="fp16").to(device)
+    pipe.scheduler = TCDScheduler.from_config(pipe.scheduler.config)
+    
+    pipe.load_lora_weights(tcd_lora_id)
+    pipe.fuse_lora()
+    
+    return pipe
+
+# override_steps = 2
+
+# Initialize the default pipe
+pipe_hyper = get_hyper_pipe(2)
+
+pipe_lightning = get_lightning_pipe(4)
+# apply_hidiffusion(pipe)
+# pipe = get_boltning_pipe()
+
+def apply_deepcache(pipe):
+    from DeepCache import DeepCacheSDHelper
+    helper = DeepCacheSDHelper(pipe=pipe)
+    helper.set_params(
+        cache_interval=3,
+        cache_branch_id=0,
+    )
+    helper.enable()
+
+# apply_deepcache(pipe)
 
 
 class Predictor:
     def __init__(self):
-
-        self.streamdiffusion = self._load_streamdiffusion_model()#model_id_or_path="./models/haveall.safetensors")
         print("CUDA version:", torch.version.cuda)
         print("PyTorch version:", torch.__version__)
 
-
-    def _load_streamdiffusion_model(
-        self,
-        model_id_or_path: str = "/home/ubuntu/automatic/models/Stable-diffusion/boltning_fp16.safetensors", #RunDiffusion/Juggernaut-XL-Lightning
-        lora_dict: Optional[Dict[str, float]] = None,
-        width: int = 768,
-        height: int = 768,
-        acceleration: Literal["none", "xformers", "tensorrt"] = "xformers",
-        use_denoising_batch: bool = False,
-        seed: int = 2,
-    ):
-        # lora_dict = {
-        #     "/home/ubuntu/pollinations/image_gen/models/hd20horizon20v10.safetensors": 1.0
-        #
-        # textual_inversions_dict = {
-        #     "badprompt": "/home/ubuntu/pollinations/image_gen/models/bad_prompt_version2-neg.pt"
-        # }
-        # textual_inversions_dict = None
-
-        stream = StreamDiffusionWrapper(
-            model_id_or_path=model_id_or_path,
-            # lora_dict=lora_dict,
-            t_index_list=T_INDEX_LIST,
-            frame_buffer_size=1,
-            width=width,
-            height=height,
-            warmup=10,
-            acceleration=acceleration,
-            mode="txt2img",
-            use_denoising_batch=use_denoising_batch,
-            cfg_type="none",
-            seed=seed,
-            use_tiny_vae=True,
-            use_lcm_lora=True,
-            # textual_inversions_dict=textual_inversions_dict,
-        )
-
-
-        return stream
-    
-    
     def predict_batch(self, batch_data):
-
         results = []
         print("batch_data:", batch_data)
         # Process each batch
@@ -120,10 +172,10 @@ class Predictor:
         model = data["model"]
         width = data["width"]
         height = data["height"]
-        if width<32:
+        if width < 32:
             width = 32
-        if height<32:
-            height=32
+        if height < 32:
+            height = 32
         steps = data["steps"]
         prompts = data["prompts"]
         refine = data["refine"]
@@ -135,56 +187,42 @@ class Predictor:
 
         print(f"Running batch with model: {model}, width: {width}, height: {height}, number of prompts: {len(prompts)}, steps: {steps}")
 
-
-        # make all prompts maximum 250 characters
-        # prompts = [prompt[:250] for prompt in prompts]
         max_batch_size = 1
 
         model = "turbo"
 
-
         print("params:", model, width, height, steps, prompts, refine, negative_prompt)
         predict_duration = 0
-        for i in range(0, len(prompts),max_batch_size):
-            chunked_prompts = prompts[i:i+max_batch_size]
+        for i in range(0, len(prompts), max_batch_size):
+            chunked_prompts = prompts[i:i + max_batch_size]
             original_prompt = chunked_prompts[0]
-            chunked_prompts[0] = original_prompt #+ ". " + prompt_pimping(original_prompt)
+            chunked_prompts[0] = original_prompt
             print("running on prompts", chunked_prompts, "original", original_prompt)
             with lock:
-            # if True:
                 predict_start_time = time.time()
                 try:
-
-                    # print("running streamdiffusion with steps:", steps)
-                    self.streamdiffusion.prepare(
-                            prompt=chunked_prompts[0],
-                            num_inference_steps=n_steps,
-                            negative_prompt=negative_prompt,
-                            guidance_scale=3.5,
-                            width=width,
-                            height=height,
-                            # negative_prompt=None
-                        )
-                    for _ in range(self.streamdiffusion.batch_size - 1):
-                        self.streamdiffusion()
-
-                    batch_results = [self.streamdiffusion()]
+                    batch_results = []
+                    for prompt in chunked_prompts:
+                        # steps = override_steps
+                        if steps < 4:
+                            steps = 2
+                        else:
+                            steps = 4
+                        pipe = pipe_hyper if steps == 2 else pipe_lightning
+                        image = pipe(prompt, num_inference_steps=steps, guidance_scale=0.0, width=width, height=height, eta=0.3).images[0]
+                        batch_results.append(image)
 
                 except Exception as e:
                     print("Exception occurred:", e)
-                    # print stack
                     import traceback
                     traceback.print_exc()
-                    # quit the process
                     os._exit(1)
 
                 concepts, has_nsfw_concepts = check_safety(batch_results, 0.0)
                 predict_end_time = time.time()
 
-                # Calculate the time spent in predict_batch
                 predict_duration += predict_end_time - predict_start_time
 
-            # Save results and add to output
             for i, (result_image, prompt) in enumerate(zip(batch_results, chunked_prompts)):
                 output_path = self._save_result(result_image)
                 results.append({
@@ -202,10 +240,9 @@ class Predictor:
         return results, predict_duration
 
     def _validate_params(self, data):
-        default_params = {"width": 1024, "height": 1024, "steps": 4, "seed": None, "model": "turbo", "refine": False,"negative_prompt":"ugly, chaotic"}
+        default_params = {"width": 1024, "height": 1024, "steps": 4, "seed": None, "model": "turbo", "refine": False, "negative_prompt": "ugly, chaotic"}
         params = default_params.copy()
 
-        # Try to convert and update each parameter individually
         for param in ['width', 'height', 'steps', 'seed']:
             try:
                 if param in data:
@@ -213,15 +250,13 @@ class Predictor:
             except:
                 print(f"Warning: Failed to convert '{param}'. Using default value.")
 
-        # Ensure width and height are divisible by 8
         params["width"] -= params["width"] % 8
         params["height"] -= params["height"] % 8
 
-        # Get 'model' parameter
         params["model"] = data.get("model", default_params["model"])
         print(f"Validated parameters: width: {params['width']}, height: {params['height']}, steps: {params['steps']}, seed: {params['seed']}, model: {params['model']}")
         return params
-    
+
     def _log_performance_metrics(self, start_time, end_time):
         add_timestamp(start_time, end_time)
         average_duration = get_average_generation_duration()
@@ -232,9 +267,7 @@ class Predictor:
         print(f"Images Generated in Last 60 Seconds: {len(get_timestamps())}")
 
     def _save_result(self, result):
-        """Saves the result image and returns its path."""
         print("Saving result image...")
-        # timestamp = time.strftime("%Y%m%d-%H%M%S")
 
         unique_id = uuid.uuid4()
         output_dir = "/tmp/imagecache"
@@ -246,7 +279,6 @@ class Predictor:
 
 predictor = Predictor()
 
-
 import time
 total_start_time = time.time()
 accumulated_predict_duration = 0
@@ -256,23 +288,17 @@ def predict_endpoint():
     global accumulated_predict_duration
 
     data = request.json
-    # Call _validate_params and update data with the returned params
     validated_params = predictor._validate_params(data)
     data.update(validated_params)
-    
-    # Start timing for predict_batch
 
     response, predict_duration = predictor.predict_batch(data)
 
     accumulated_predict_duration += predict_duration
 
-    # Calculate the total time the app has been running
     total_time = time.time() - total_start_time
 
-    # Calculate and print the percentage of time spent in predict_batch
     predict_percentage = (accumulated_predict_duration / total_time) * 100
     print(f"Predict time percentage: {predict_percentage}%")
-
 
     print("Returning response for one request.")
     return jsonify(response)
@@ -280,7 +306,6 @@ def predict_endpoint():
 import clip
 device = "cuda" if torch.cuda.is_available() else "cpu"
 clip_model, _ = clip.load("ViT-L/14", device=device)
-# aesthetic_model = get_aesthetic_model()
 
 @app.route('/embeddings', methods=['POST'])
 def embeddings_endpoint():
@@ -292,35 +317,27 @@ def embeddings_endpoint():
     start_time = time.time()
 
     aesthetics_scores = []
-    # for prompt in prompts:
     token = clip.tokenize(prompts, truncate=True).to(device)
     with torch.no_grad():
         embeddings = clip_model.encode_text(token)
         aesthetics_scores = aesthetic_model(embeddings)
-        # squash last dimeinsion of aesthetics_scores
         aesthetics_scores = aesthetics_scores.squeeze(-1)
         print("aesthetics_score:", aesthetics_scores)
     end_time = time.time()
-    print(f"Time to calculate embeddings: {(end_time - start_time)*1000} milliseconds")
+    print(f"Time to calculate embeddings: {(end_time - start_time) * 1000} milliseconds")
     print("Returning embeddings for one request.", len(embeddings))
-
 
     return jsonify({
         "embeddings": embeddings.cpu().numpy().tolist(),
         "aesthetics_scores": aesthetics_scores.cpu().numpy().tolist()
     })
 
-
 def prompt_pimping(input_text):
     output = pimper_model.generate(tokenizer(input_text, return_tensors="pt").input_ids.to("cuda"), max_length=30)
     result_text = tokenizer.decode(output[0])
-    # if len(result_text) < len(input_text):
-    #     print("ERRROR for some reason pimped prompt is shorter than original. returning original")
-    #     return input_text
     return result_text
+
 import os
 if __name__ == "__main__":
     print("Starting Flask app...")
-    # from env PORT or 5555
     app.run(debug=False, host="0.0.0.0", port=os.environ.get("PORT", 5555))
-

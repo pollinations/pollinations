f1b6002c36284c9bd661ea7fc2c2d51ee6ccc071
Thomas Haferlach <thomas.haferlach@pixelynx.io>
2024-06-21 05:25:19 +0200

    Add new link to GenerativeImageFeed
    - Shorten model link text to "Boltning"
    - Add new link for "Prompt Pimper" with Groq reference
    - Improve user navigation and resource access

--- app/src/pages/Home/GenerativeImageFeed.js
+++ app/src/pages/Home/GenerativeImageFeed.js
@@ -65,9 +65,11 @@ export function GenerativeImageFeed() {
                   />
                 </Link>
                 <Typography variant="caption" color="textSecondary" style={{ marginTop: '10px', textAlign: 'center' }}>
-                  Model: <Link href="https://civitai.com/models/413466/boltning-realistic-lightning-hyper" target="_blank" rel="noopener noreferrer" style={{ color: Colors.lime }}>Boltning - Realistic, Lightning, HYPER</Link>
+                  Model: <Link href="https://civitai.com/models/413466/boltning-realistic-lightning-hyper" target="_blank" rel="noopener noreferrer" style={{ color: Colors.lime }}>Boltning</Link>
                   &nbsp;&nbsp;
                   LoRA: <Link href="https://huggingface.co/tianweiy/DMD2" target="_blank" rel="noopener noreferrer" style={{ color: Colors.lime }}>DMD2</Link>
+                  &nbsp;&nbsp;
+                  Prompt Pimper: <Link href="https://github.com/pollinations/pollinations/blob/master/image_gen_server/groqPimp.js" target="_blank" rel="noopener noreferrer" style={{ color: Colors.lime }}>Groq</Link>
                 </Typography>
               </>
               ) : (

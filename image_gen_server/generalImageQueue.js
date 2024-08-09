3bc03c10fb99080406fd563d76e14409570e6124
voodoohop <thomas.haferlach@pixelynx.io>
2024-08-09 03:04:55 +0000

    update

--- image_gen_server/generalImageQueue.js
+++ image_gen_server/generalImageQueue.js
@@ -1,7 +1,7 @@
 import PQueue from 'p-queue';
 
-export const BATCH_SIZE = 4; // Number of requests per batch
+export const BATCH_SIZE = 1; // Number of requests per batch
 
-export const concurrency = 1; // Number of concurrent requests per bucket key
+export const concurrency = 6; // Number of concurrent requests per bucket key
 
 export const generalImageQueue = new PQueue({ concurrency });

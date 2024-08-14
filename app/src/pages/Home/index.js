868eb14259f42ed5302b220906e5f20339490cd3
Thomas Haferlach <thomas.haferlach@pixelynx.io>
2024-08-14 20:06:20 +0200

    Revert "Re-add karma"
    This reverts commit 9e541aa248a75772ea20a12d1d01ec5e45da5577.

--- app/src/pages/Home/index.js
+++ app/src/pages/Home/index.js
@@ -36,7 +36,7 @@ export default function Solutions() {
       {/* <TopBand src={TopBandPresetsDesign} alt="Top Band" />
       <Dreamachine /> */}
       <TopBand src={TopBandPresetsDesign} alt="Top Band" />
-      <KarmaYT />
+      {/* <KarmaYT /> */}
       <ImageURLHeading>Events</ImageURLHeading>
       {/* <PageTemplate label="event" /> */}
       {/* <TwitchSection /> */}

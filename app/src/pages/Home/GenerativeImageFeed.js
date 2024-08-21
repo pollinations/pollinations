16fdba5dd339d4216a974ceca1b3c2d096860bcf
Thomas Haferlach <thomas.haferlach@pixelynx.io>
2024-08-21 15:45:56 +0200

    Add mobile responsiveness to button group
    - Import useMediaQuery from @material-ui/core
    - Add isMobile constant to check screen width
    - Conditionally render ButtonGroup based on isMobile value

--- app/src/pages/Home/GenerativeImageFeed.js
+++ app/src/pages/Home/GenerativeImageFeed.js
@@ -1,5 +1,5 @@
 import { useState, useEffect, useCallback } from 'react';
-import { Typography, ButtonGroup, Grid, Link, Box, Paper, Table, TableBody, TableCell, TableRow, TextField, CircularProgress, Slider, TableContainer, Checkbox, Tooltip, IconButton, Collapse, Button, Tabs, Tab, TextareaAutosize, Select, MenuItem, FormControl, InputLabel, Accordion, AccordionSummary, AccordionDetails } from '@material-ui/core';
+import { Typography, ButtonGroup, Grid, Link, Box, Paper, Table, TableBody, TableCell, TableRow, TextField, CircularProgress, Slider, TableContainer, Checkbox, Tooltip, IconButton, Collapse, Button, Tabs, Tab, TextareaAutosize, Select, MenuItem, FormControl, InputLabel, Accordion, AccordionSummary, AccordionDetails, useMediaQuery } from '@material-ui/core';
 import InfoIcon from '@material-ui/icons/Info';
 import ExpandMoreIcon from '@material-ui/icons/ExpandMore';
 import { debounce } from 'lodash';
@@ -22,6 +22,7 @@ export function GenerativeImageFeed() {
   const { image: slideshowImage, onNewImage, stop } = useImageSlideshow();
   const { updateImage, image, isLoading } = useImageEditor({ stop, image: slideshowImage });
   const { imagesGenerated } = useFeedLoader(onNewImage, setLastImage);
+  const isMobile = useMediaQuery(`(max-width:${MOBILE_BREAKPOINT})`);
 
   useEffect(() => {
     setImageParams(image);
@@ -89,25 +90,27 @@ export function GenerativeImageFeed() {
             </ImageContainer>
           </Grid>
           <Grid item xs={12}>
-            <Box display="flex" justifyContent="center" >
-              <ButtonGroup aria-label="edit-integrate-button-group" style={{ border: 'none' }}>
-                <Button
-                  onClick={() => setTabValue(0)}
-                  variant={tabValue === 0 ? "contained" : "text"}
-                  color={tabValue === 0 ? Colors.offblack : Colors.lime}
-                  style={{ color: tabValue === 0 ? Colors.offblack : Colors.lime, backgroundColor: tabValue === 0 ? Colors.lime : "transparent", boxShadow: 'none', width: "120px", height: "40px", fontSize: "0.875rem" }}
-                >
-                  Edit
-                </Button>
-                <Button
-                  onClick={() => setTabValue(1)}
-                  variant={tabValue === 1 ? "contained" : "text"}
-                  style={{ color: tabValue === 1 ? Colors.offblack : Colors.lime, backgroundColor: tabValue === 1 ? Colors.lime : "transparent", boxShadow: 'none', width: "120px", height: "40px", fontSize: "0.875rem" }}
-                >
-                  Integrate
-                </Button>
-              </ButtonGroup>
-            </Box>
+            {!isMobile && (
+              <Box display="flex" justifyContent="center" >
+                <ButtonGroup aria-label="edit-integrate-button-group" style={{ border: 'none' }}>
+                  <Button
+                    onClick={() => setTabValue(0)}
+                    variant={tabValue === 0 ? "contained" : "text"}
+                    color={tabValue === 0 ? Colors.offblack : Colors.lime}
+                    style={{ color: tabValue === 0 ? Colors.offblack : Colors.lime, backgroundColor: tabValue === 0 ? Colors.lime : "transparent", boxShadow: 'none', width: "120px", height: "40px", fontSize: "0.875rem" }}
+                  >
+                    Edit
+                  </Button>
+                  <Button
+                    onClick={() => setTabValue(1)}
+                    variant={tabValue === 1 ? "contained" : "text"}
+                    style={{ color: tabValue === 1 ? Colors.offblack : Colors.lime, backgroundColor: tabValue === 1 ? Colors.lime : "transparent", boxShadow: 'none', width: "120px", height: "40px", fontSize: "0.875rem" }}
+                  >
+                    Integrate
+                  </Button>
+                </ButtonGroup>
+              </Box>
+            )}
             <Box>
               {tabValue === 0 && <ImageData {...{ image: imageParams, handleParamChange, handleFocus, isLoading, handleSubmit }} />}
               {tabValue === 1 && (

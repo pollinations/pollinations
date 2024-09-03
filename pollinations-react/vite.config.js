40fce768a1d0b7079e7bd4e3699e7c87d689b5af
Thomas Haferlach <thomas.haferlach@pixelynx.io>
2024-09-03 17:57:17 +0200

    Initial checkin

---
+++ pollinations-react/vite.config.js
@@ -0,0 +1,22 @@
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
+
+export default defineConfig({
+    plugins: [react()],
+    build: {
+        lib: {
+            entry: 'src/index.js',
+            name: 'PollinationsReact',
+            fileName: (format) => `pollinations-react.${format}.js`
+        },
+        rollupOptions: {
+            external: ['react', 'react-dom'],
+            output: {
+                globals: {
+                    react: 'React',
+                    'react-dom': 'ReactDOM'
+                }
+            }
+        }
+    }
+});

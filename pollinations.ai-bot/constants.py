af67ce0c0c3d32ff879eb89a191ea93fe9f75d52
Thomas Haferlach <thomas.haferlach@pixelynx.io>
2024-09-04 15:42:24 +0200

    Disable MONGODB_URI - Commented out MONGODB_URI to prevent usage - Ensures no connection to MongoDB during runtime

--- pollinations.ai-bot/constants.py
+++ pollinations.ai-bot/constants.py
@@ -6,7 +6,7 @@ import json
 load_dotenv(override=True)
 
 TOKEN = os.environ["TOKEN"]
-MONGODB_URI = os.environ["MONGODB_URI"]
+# MONGODB_URI = os.environ["MONGODB_URI"]
 
 r = requests.get("https://image.pollinations.ai/models")
 MODELS = json.loads(r.text)

3ff56fd4a5f6a03ac3013efc75ba9073db05dbac
voodoohop <t.haferlach@gmail.com>
2024-09-14 00:50:24 +0200

    add usePollinationsChat (#650)
    Co-authored-by: Thomas Haferlach <thomas.haferlach@pixelynx.io>

--- app/src/components/AsciiArtGenerator.js
+++ app/src/components/AsciiArtGenerator.js
@@ -1,4 +1,4 @@
-import { PollinationsText } from '@pollinations/react';
+import { usePollinationsText } from '@pollinations/react';
 import React, { useEffect, useRef, useState } from 'react';
 import { useInterval } from 'usehooks-ts';
 import styled from '@emotion/styled';
@@ -11,16 +11,23 @@ const AsciiContainer = styled.pre`
 `;
 
 const AsciiArtGenerator = (props) => {
-    const [seed, setSeed] = useState(0);
+    const [{ seed, lastAsciiArt }, setState] = useState({ seed: 1, lastAsciiArt: null });
+    const prompt = "Unicode/ Ascii Art depicting digital pollination. 30 width x 10 height characters. Use fun unicode stuff but keep a lot of space empty. Return only the characters, no other text or quotes.";
+    const promptWithLast = lastAsciiArt ? `${prompt}\n\nModify the previous art like a cellular automata. Use a variety of emojis and unicode characters that fit with the theme.\n\n\`\`\`${lastAsciiArt}\`\`\`` : prompt;
+    const asciiArt = usePollinationsText(promptWithLast, seed);
+    // useInterval(() => {
+    //     setSeed(seed => (seed + 1) % 30);
+    // }, 500);
 
-    useInterval(() => {
-        setSeed(seed => (seed + 1) % 30);
-    }, 500);
+
+    useEffect(() => {
+        setTimeout(() => {
+            setState(({ seed: (seed) % 10 + 1, lastAsciiArt: asciiArt }));
+        }, 1000);
+    }, [seed]);
 
     return <AsciiContainer style={props?.style}>
-        <PollinationsText seed={seed}>
-            Unicode/ Ascii Art depicting digital pollination. 30 width x 10 height characters. Use fun unicode stuff but keep a lot of space empty. Return only the characters, no other text or quotes.
-        </PollinationsText>
+        {asciiArt}
     </AsciiContainer>;
 }
 

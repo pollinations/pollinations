a9802c63c3e2db127a261c4087ea5a828d4af935
pollinations.ai <151645294+pollinations-ai@users.noreply.github.com>
2024-09-16 22:11:37 -0400

    Pollinations.py Update to 0.5.2 (Latest/Stable) (#643)
    * Update __init__.py
    
    * Delete pollinations.py/_core directory
    
    * Create __init__.py
    
    * Create imageprotocol.py
    
    * Delete pollinations.py/egg directory
    
    * Update __init__.py
    
    * Create __init__.py
    
    * Create ImageModel.py
    
    * Create ImageObject.py
    
    * Update __init__.py
    
    ---------
    
    Co-authored-by: Thomas Haferlach <thomas.haferlach@pixelynx.io>

---
+++ pollinations.py/abc/imageprotocol.py
@@ -0,0 +1,23 @@
+"""
+pollinations.abc.imageprotocol
+
+Classes:
+    ImageProtocol: Image protocol for the ImageObject class.
+"""
+
+from typing import Protocol
+
+
+class ImageProtocol(Protocol):
+    """
+    ImageProtocol: Image protocol for the ImageObject class.
+    """
+
+    prompt: str
+    model: str
+    width: int
+    height: int
+    seed: int
+    url: str
+    date: str
+    content: bin
--- pollinations.py/ai/__init__.py
+++ pollinations.py/ai/__init__.py
@@ -1,80 +1,94 @@
-import time
-from .._core._handler import *
-from .._core._handler import _request_image
-from ..ext import *
+"""
+pollinations.ai
 
-models = ['turbo', 'dreamshaper', 'deliberate', 'pixart', 'playground', 'dpo', 'dalle3xl', 'formulaxl']
+Classes:
+    ImageModel (ai.ImageModel): Text-to-image generative AI model.
+    (deprecated) > TextModel (ai.TextModel): Text-to-text generative AI model.
+    ImageObject (ai.ImageObject): Image object.
+    (deprecated) > TextObject (ai.TextObject): Text object.
 
-class GlobalCooldown:
-    def __init__(self, cooldown: int = 10):
-        self.cooldown: int = cooldown
-        self.last_request: int = time.time() - 11
+Functions:
+    sample(str): Returns a sample prompt for the Image model.
+    sample_style(str): Returns a style of prompt for the Image model.
+    sample_batch(list, size=10): Returns a batch of sample prompts for the Image model.
+    help(str): Returns general help information for pollinations.ai
 
-cooldown = GlobalCooldown()
+Variables:
+    samples (list): List of sample prompts for the Image model.
+    styles (dict): Dictionary of prompt styles for the Image model.
+    realistic (str): styles[realistic]
+    cartoon (str): styles[cartoon]
+    anime (str): styles[anime]
+    logo (str): styles[logo]
 
-class Model:
-    '''
-    pollinations.ai.Model
-    '''
-    def __init__(self, *args, **kwargs) -> None:
-        self.model = Models.turbo
-        self.width = 1024
-        self.height = 1024
-        self.seed = 'random'
-        self.prompt = ''
-        self.last = None
+    BANNED_WORDS (list): List of banned words for the Image model filter.
 
-    def generate(self, prompt: str, *, model: str = Models.turbo, width: int = 1024, height: int = 1024, seed: int = 'random') -> ImageType:
-        '''
-        Generate an image from the specified prompt.
-        '''
-        if model.lower() not in models:
-            raise ValueError(f'\n\nInvalid model: {model}')
+"""
 
-        if cooldown.last_request + cooldown.cooldown > time.time():
-            time_until_allowed = cooldown.cooldown - (time.time() - cooldown.last_request)
-            raise ValueError(f'\n\nCooldown: {time_until_allowed} seconds until allowed.')
-            
-        image = _request_image(
-            prompt=prompt,
-            model=model,
-            width=width,
-            height=height,
-            seed=seed,
-        )
-        self.model = model
-        self.width = width
-        self.height = height
-        self.seed = seed
-        self.prompt = prompt
-        self.last = image
-        cooldown.last_request = time.time()
-        return image
+import random
+from .. import abc
+from .. import ext
+from ..types import (
+    ImageModel,
+    # TextModel,
+    ImageObject,
+    # TextObject,
+)
 
-    def save(self, path: str) -> Image:
-        '''
-        Save the image to the specified path.
-        '''
-        if self.last:
-            self.last.save(path)
-        else:
-            raise ValueError('No image generated yet.')
+Image: ImageModel = ImageModel
+# Text: TextModel = TextModel
 
-        return self
+samples: list = ext.samples
+styles: dict = ext.styles
 
-    def __repr__(self, *args, **kwargs) -> str:
-        return f"Model(model={self.model!r}, width={self.width}, height={self.height}, seed={self.seed}, prompt={self.prompt!r}, last={str(self.last)[:10]}...)"
+realistic: str = ext.realistic
+cartoon: str = ext.cartoon
+anime: str = ext.anime
+logo: str = ext.logo
 
-    def __str__(self, *args, **kwargs) -> str:
-        return self.__repr__()
+sample_style: object = ext.sample_style
+sample: object = ext.sample
+sample_batch: object = ext.sample_batch
 
-Image = Model
-default = Models.default
-turbo = Models.turbo
-dreamshaper = Models.dreamshaper
-deliberate = Models.deliberate
-pixart = Models.pixart
-playground = Models.playground
-dpo = Models.dpo
-dalle3xl = Models.dalle3xl
-formulaxl = Models.formulaxl
+
+@abc.resource(deprecated=True)
+def help(*args, **kwargs) -> str:
+    """
+    pollinations.ai.help
+
+    Return:
+        str: Help information for pollinations.ai
+    """
+    help_return: str = (
+        """
+  sample(): returns 1 random sample prompt
+
+  sample_style(): returns a style of art (realistic, cartoon, anime, logo))
+
+  sample_batch(size: int): returns size batch of random sample prompts
+
+  Image(save_file: str (OPTIONAL)): inialize the ai.Image
+
+  Image.set_filter(filter: list): set the filter for list of backlisted words
+
+  Image.generate(prompt: str): generate an image from a prompt
+
+  Image.generate_batch(prompts: list): generate an image from a batch of prompts
+
+  Image.save(save_file: str (OPTIONAL)): save the image to a file
+
+  Image.load(load_file: str (OPTIONAL)): load the image from a file
+
+  Image.image(): return the image object
+
+  (deprecated) > Text(save_file: str (OPTIONAL)): inialize the ai.Text
+
+  (deprecated) > Text.chat(prompt: str): chat with the ai and return the response
+
+  """
+        ""
+    )
+    return help_return
+
+
+BANNED_WORDS: list = abc.BANNED_WORDS

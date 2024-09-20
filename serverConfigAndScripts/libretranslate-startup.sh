fd09d0fb30e11a8794b6e62c67e3f16382e09ed2
voodoohop <t.haferlach@gmail.com>
2024-09-20 14:12:43 +0200

    709 automatically initialize libretranslate and turbo models (#710)
    * fix libretranslate installer
    
    * Update port for uvicorn server
    
    - Change port from 5002 to 5003 for uvicorn server in text_to_image_sdxl.py
    - Ensures no conflict with other services running on port 5002
    
    * Create turbo-startup.sh
    
    * Add pollinations-turbo service
    
    - Copy pollinations-turbo.service to systemd directory
    - Set correct permissions for pollinations-turbo.service
    - Enable pollinations-turbo.service
    - Update log messages to include pollinations-turbo service
    
    * Create pollinations-turbo.service
    
    ---------
    
    Co-authored-by: Thomas Haferlach <thomas.haferlach@pixelynx.io>

--- serverConfigAndScripts/libretranslate-startup.sh
+++ serverConfigAndScripts/libretranslate-startup.sh
@@ -6,18 +6,24 @@ log() {
 
 log "Starting LibreTranslate startup script"
 
+# update apt
+sudo apt update || { log "ERROR: Failed to update apt"; exit 1; }
+
+# install python3.10-venv
+sudo apt install -y python3.10-venv || { log "ERROR: Failed to install python3.10-venv"; exit 1; }
+
 # Navigate to the home directory
 cd $HOME || { log "ERROR: Failed to change directory to $HOME"; exit 1; }
 
 # Check if the virtual environment exists, if not create it
-if [ ! -d "libretranslateenv" ]; then
+if [ ! -d "libretranslateenv2" ]; then
     log "Creating virtual environment for LibreTranslate"
-    python3 -m venv libretranslateenv || { log "ERROR: Failed to create virtual environment"; exit 1; }
+    python3 -m venv libretranslateenv2 || { log "ERROR: Failed to create virtual environment"; exit 1; }
 fi
 
 # Activate the virtual environment
 log "Activating virtual environment"
-source libretranslateenv/bin/activate || { log "ERROR: Failed to activate virtual environment"; exit 1; }
+source libretranslateenv2/bin/activate || { log "ERROR: Failed to activate virtual environment"; exit 1; }
 
 # Install or upgrade LibreTranslate
 log "Installing/Upgrading LibreTranslate"
---
+++ serverConfigAndScripts/ssh_and_initialize.sh
@@ -0,0 +1,16 @@
+#! /bin/bash
+
+if [ -z "$1" ]; then
+  echo "Usage: $0 <ip_address_or_host_name>"
+  exit 1
+fi
+
+HOST=$1
+
+# Automatically accept adding the key if ssh asks
+ssh -o StrictHostKeyChecking=no -i $HOME/.ssh/thomashkey ubuntu@$HOST << EOF
+  cd /home/ubuntu/pollinations/serverConfigAndScripts
+  git pull
+  bash install-services.sh
+  sudo reboot
+EOF

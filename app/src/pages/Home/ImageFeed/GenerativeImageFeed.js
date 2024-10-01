e597f6c1a1e0e804cbc845389f6f286740c27479
Thomas Haferlach <thomas.haferlach@pixelynx.io>
2024-10-01 23:57:11 +0200

    Remove commented custom prompt from ImageURLHeading
    - Clean up code by removing unnecessary comment
    - Improve readability and maintainability of the file

--- app/src/pages/Home/ImageFeed/GenerativeImageFeed.js
+++ app/src/pages/Home/ImageFeed/GenerativeImageFeed.js
@@ -161,7 +161,6 @@ export function GenerativeImageFeed() {
     <GenerativeImageURLContainer className={classes.container}>
       <Grid item className={classes.gridItem} style={{ marginTop: "2em" }}>
         <ImageURLHeading
-          // customPrompt={`an image with the text "Image Feed" displayed in an elegant, decorative serif font. The font has high contrast between thick and thin strokes, that give the text a sophisticated and stylized appearance. The text is in white, set against a solid black background, creating a striking and bold visual contrast. Incorporate elements related to pollinations, digital circuitry, such as flowers, chips, insects, wafers, and other organic forms into the design of the font. Each letter features unique, creative touches that make the typography stand out. Incorporate colorful elements related to pollinators and pollens, insects and plants into the design of the font. Make it very colorful with vibrant hues and gradients.`}
           width={isMobile ? 400 : 700}
           height={isMobile ? 150 : 200}
         >

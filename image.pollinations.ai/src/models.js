6a70a0e9b915e21d873b34343f6a29e5cd1a953f
voodoohop <t.haferlach@gmail.com>
2024-10-04 13:53:24 +0200

    800 add flux pro model from meoow (#801)
    * Add support for Meoow-2 API integration
    - Introduce Meoow-2 API endpoint and key
    - Implement callMeoow2 function for image generation
    - Update createAndReturnImageCached to handle Meoow-2 models
    - Modify getLogoPath to accommodate Meoow-2 models
    - Add flux-pro model to MODELS with Meoow-2 type
    
    * Update logo path logic
    
    - Change condition to use 'or' instead of 'and'
    - Ensure logo is not added for specific conditions
    
    * Decode URL-encoded prompts
    - Add urldecode import to handle URL-encoded prompts
    - Implement try-catch to safely decode prompts
    - Log error and use original prompt if decoding fails
    
    * Update ImageEditor.js
    
    * Update @pollinations/react to v2.0.6
    - Bump version in package.json and package-lock.json
    - Remove console.log from ImageHeading.js
    - Remove console.log from usePollinationsImage.js
    
    ---------
    
    Co-authored-by: Thomas Haferlach <thomash@pollinations.ai>
    Co-authored-by: Thomas Haferlach <thomas.haferlach@pixelynx.io>

--- image.pollinations.ai/src/models.js
+++ image.pollinations.ai/src/models.js
@@ -4,5 +4,6 @@ export const MODELS = {
     "flux-anime": { type: "meoow", enhance: false, maxSideLength: 1384 },
     "flux-3d": { type: "meoow", enhance: false, maxSideLength: 1384 },
     "any-dark": { type: "meoow", enhance: false, maxSideLength: 1384 },
-    "turbo": { type: "pollinations", enhance: false, maxSideLength: 1024 } // Assuming 'turbo' is of type 'sd'
+    "flux-pro": { type: "meoow-2", enhance: false, maxSideLength: 1512 },
+    "turbo": { type: "pollinations", enhance: true, maxSideLength: 1024 } // Assuming 'turbo' is of type 'sd'
 };
--- pollinations-react/src/hooks/usePollinationsImage.js
+++ pollinations-react/src/hooks/usePollinationsImage.js
@@ -16,7 +16,6 @@ import React, { useMemo } from 'react';
 const usePollinationsImage = (prompt, options = {}) => {
     const { width = 1024, height = 1024, model = 'flux', seed = 42, nologo = true, enhance = false } = options;
 
-    console.log("prompt", prompt);
     const imageUrl = useMemo(() => {
         const params = new URLSearchParams({ width, height, model, seed, nologo, enhance });
         return `https://pollinations.ai/p/${encodeURIComponent(prompt)}?${params.toString()}`;

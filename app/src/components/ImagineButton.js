1123aebfaa6ff19351c25b425ff6097c970a198b
voodoohop <t.haferlach@gmail.com>
2024-11-14 14:05:56 +0900

    Add cancel functionality to image generation
    - Implement cancel button in ImagineButton component
    - Add cancelLoading function in useImageEditor hook
    - Remove isLoading prop from disabled attributes
    - Adjust CircularProgress position in ImagineButton
    - Update handleButtonClick to handle cancel action

--- app/src/components/ImagineButton.js
+++ app/src/components/ImagineButton.js
@@ -5,44 +5,44 @@ import { Colors } from "../styles/global";
 
 export function ImagineButton({ handleButtonClick, isLoading, isInputChanged }) {
   return (
-    <CustomTooltip title="Generate an alternative image from the current prompt/settings.">
-      <Button
-        variant="contained"
-        color="primary"
-        onClick={handleButtonClick}
-        disabled={isLoading}
-        style={{
-          backgroundColor: isInputChanged ? Colors.lime : Colors.lime,
-          color: isInputChanged ? null : Colors.offblack,
-          fontSize: '1.3rem',
-          fontFamily: 'Uncut-Sans-Variable',
-          fontStyle: 'normal',
-          fontWeight: 600,
-          height: "60px",
-          width: "160px",
-          position: "relative",
-          marginTop: "0em",
-          display: "flex",
-          alignItems: "center",
-          justifyContent: "center",
-          letterSpacing: "0.1em", // Increased letter spacing
-        }}
-      >
-        {isLoading ? <span></span> : "Pollinate"}
-        {isLoading && (
-          <CircularProgress
-            size={24}
-            style={{
-              color: "black",
-              position: "absolute",
-              top: "50%",
-              left: "50%",
-              marginTop: -12,
-              marginLeft: -12,
-            }}
-          />
-        )}
-      </Button>
-    </CustomTooltip>
+    <div style={{ position: 'relative', display: 'inline-block' }}>
+      <CustomTooltip title="Generate an alternative image from the current prompt/settings.">
+        <Button
+          variant="contained"
+          color="primary"
+          onClick={handleButtonClick}
+          style={{
+            backgroundColor: isInputChanged ? Colors.lime : Colors.lime,
+            color: isInputChanged ? null : Colors.offblack,
+            fontSize: '1.3rem',
+            fontFamily: 'Uncut-Sans-Variable',
+            fontStyle: 'normal',
+            fontWeight: 600,
+            height: "60px",
+            width: "160px",
+            position: "relative",
+            marginTop: "0em",
+            display: "flex",
+            alignItems: "center",
+            justifyContent: "center",
+            letterSpacing: "0.1em",
+          }}
+        >
+          {isLoading ? "Cancel" : "Pollinate"}
+        </Button>
+      </CustomTooltip>
+      {isLoading && (
+        <CircularProgress
+          size={24}
+          style={{
+            position: 'absolute',
+            right: -36,
+            top: '50%',
+            marginTop: -12,
+            color: Colors.lime
+          }}
+        />
+      )}
+    </div>
   )
 }

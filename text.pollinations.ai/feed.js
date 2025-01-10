bf33fdd046916a5aaf6f918dde7a176ae05d23f4
voodoohop <t.haferlach@gmail.com>
2025-01-10 01:32:14 +0100

    Create separate fles for feed and cache

---
+++ text.pollinations.ai/feed.js
@@ -0,0 +1,44 @@
+import debug from 'debug';
+
+const log = debug('pollinations:feed');
+
+// Map to store connected clients
+const connectedClients = new Map();
+
+
+// Function to handle new responses
+function handleNewResponse(response, parameters, ip) {
+    res.write(`data: ${JSON.stringify({ 
+        response, 
+        parameters, 
+        ip 
+    })}\n\n`);
+}
+
+function sendToFeedListeners(response, requestParams, ip) {
+    for (const [_, send] of connectedClients) {
+        log('broadcasting response', response, "to", connectedClients.size);
+        send(response, requestParams, ip);
+    }
+}
+
+function setupFeedEndpoint(app) {
+    app.get('/feed', (req, res) => {
+        res.writeHead(200, {
+            'Content-Type': 'text/event-stream',
+            'Cache-Control': 'no-cache',
+            'Connection': 'keep-alive'
+        });
+
+        // Add the client to a list of connected clients
+        const clientId = Date.now();
+        connectedClients.set(clientId, handleNewResponse.bind(null, res));
+
+        // Remove the client when they disconnect
+        req.on('close', () => {
+            connectedClients.delete(clientId);
+        });
+    });
+}
+
+export { setupFeedEndpoint, sendToFeedListeners };

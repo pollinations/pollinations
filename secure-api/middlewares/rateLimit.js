3eaed99138f412839745a62455457fad39c1a9c4
voodoohop <t.haferlach@gmail.com>
2025-01-12 22:15:42 +0100

    Revert "secure-api init commit"
    This reverts commit 2c93676f45679e25428b4942fc6e6f3bc3b2323d.

--- secure-api/middlewares/rateLimit.js
+++
@@ -1,9 +0,0 @@
-const rateLimit = require('express-rate-limit');
-
-const limiter = rateLimit({
-  windowMs: 15 * 60 * 1000, // 15 minutes
-  max: 100, // limit each IP to 100 requests per window
-  message: { error: 'Too many requests, please try again later.' },
-});
-
-exports.rateLimitMiddleware = limiter;

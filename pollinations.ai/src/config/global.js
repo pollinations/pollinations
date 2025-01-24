3c21a79aba269034f75074b04c8ef54bfa0cded3
voodoohop <t.haferlach@gmail.com>
2025-01-24 20:52:14 +0100

    colors from image

--- pollinations.ai/src/config/global.js
+++ pollinations.ai/src/config/global.js
@@ -12,10 +12,10 @@ import backgroundImage8 from "../assets/background/08.webp"
 export const SHOW_PROMPT_TOOLTIP = false; 
 
 export const Colors = {
-  lime: '#85b7bc', //#37b9b0 #00afbf
-  offwhite: '#FAFAFA',
-  offblack: '#1a1a1a', 
-  offblack2: '#3b3b3b',
+  lime: '#ecf874', //#37b9b0 #00afbf
+  offwhite: '#c7d4d6',
+  offblack: '#110518', 
+  offblack2: '#181a2c',
   gray1: '#B3B3B3',
   gray2: '#8A8A8A',
 };

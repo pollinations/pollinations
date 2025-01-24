c5b83d736e7b56a2a92ef8268c4bc2f130539b4b
elliot.fouchy <elliotfouchy@gmail.com>
2025-01-24 20:32:38 +0100

    removed grey bg on image display

--- pollinations.ai/src/components/FeedImage/ImageDisplay.js
+++ pollinations.ai/src/components/FeedImage/ImageDisplay.js
@@ -39,7 +39,7 @@ export const ImageDisplay = memo(function ImageDisplay({ image }) {
         alignItems: "center",
         position: "relative",
         boxShadow: "none",
-        backgroundColor: `${Colors.offblack2}99`,
+        backgroundColor: Colors.offblack,
       }}
     >
       {image ? (

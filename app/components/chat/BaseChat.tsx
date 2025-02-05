536d1935fe5bf8dfdbbbea7b2e6b0ea43664ec67
voodoohop <t.haferlach@gmail.com>
2025-02-05 05:41:17 +0100

    Update BaseChat.tsx

--- app/components/chat/BaseChat.tsx
+++ app/components/chat/BaseChat.tsx
@@ -523,7 +523,7 @@ export const BaseChat = React.forwardRef<HTMLDivElement, BaseChatProps>(
                         minHeight: TEXTAREA_MIN_HEIGHT,
                         maxHeight: TEXTAREA_MAX_HEIGHT,
                       }}
-                      placeholder="How can Bolt help you today?"
+                      placeholder="How can Polli help you today?"
                       translate="no"
                     />
                     <ClientOnly>

8f419c0fa866c685912a99c166cf6934124ffeec
ElliotEtag <36901823+ElliotEtag@users.noreply.github.com>
2025-03-15 10:31:51 +0100

    added featured tab to project section (#1202)
    * added featured tab to project section
    
    * Make mature check more strict with substring matching and reorder project sections
    
    * mature words changes and remove unused model viewer
    
    ---------
    
    Co-authored-by: MentatBot <160964065+MentatBot@users.noreply.github.com>
    Co-authored-by: voodoohop <t.haferlach@gmail.com>

--- pollinations.ai/src/Home/index.js
+++ pollinations.ai/src/Home/index.js
@@ -20,9 +20,9 @@ export default function Home() {
         <Hero />
         <News />
         <FeedImage />
+        <Projects />
         <Integration />
         <Discord />
-        <Projects />
         {/* <Team /> */}
         <Supporter />
       </Style>

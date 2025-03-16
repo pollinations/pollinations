3870157d970abba032794b084753fbdaf0aebab7
voodoohop <t.haferlach@gmail.com>
2025-03-16 21:28:38 +0100

    remove pollinations.diy

--- pollinations.diy/app/lib/webcontainer/auth.client.ts
+++
@@ -1,6 +0,0 @@
-/**
- * This client-only module that contains everything related to auth and is used
- * to avoid importing `@webcontainer/api` in the server bundle.
- */
-
-export { auth, type AuthAPI } from '@webcontainer/api';
--- pollinations.diy/app/types/terminal.ts
+++
@@ -1,9 +0,0 @@
-export interface ITerminal {
-  readonly cols?: number;
-  readonly rows?: number;
-
-  reset: () => void;
-  write: (data: string) => void;
-  onData: (cb: (data: string) => void) => void;
-  input: (data: string) => void;
-}

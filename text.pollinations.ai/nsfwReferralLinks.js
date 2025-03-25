12d2e3e27635f8088ffc8be248896bec9474d3ad
elliot.fouchy <elliot.fouchy@pixelynx.io>
2025-03-25 19:28:41 +0100

    abstracted detection keyword lists to separated file

--- text.pollinations.ai/nsfwReferralLinks.js
+++ text.pollinations.ai/nsfwReferralLinks.js
@@ -2,55 +2,13 @@ import { generateTextPortkey } from './generateTextPortkey.js';
 import debug from 'debug';
 import { sendToAnalytics } from './sendToAnalytics.js';
 import { getRequestData } from './requestUtils.js';
+import { nsfwKeywords } from './ads/nsfwKeywords.js';
 
 const log = debug('pollinations:referral:nsfw');
 const errorLog = debug('pollinations:referral:nsfw:error');
 
 // Probability of adding NSFW referral links (60%)
 const NSFW_REFERRAL_LINK_PROBABILITY = 1;
-
-// Simplified NSFW keywords list for first-pass detection
-const nsfwKeywords = [
-  // English
-  'girlfriend', 'dating', 'relationship', 'romance', 'intimate',
-  'love', 'companion', 'roleplay', 'nsfw', 'adult', 'erotic',
-  'sexy', 'flirt', 'kiss', 'date',
-
-  // Russian
-  'подруга', 'знакомства', 'отношения', 'романтика', 'интим',
-  'любовь', 'спутник', 'ролевая игра', 'для взрослых', 'эротика',
-  'свидание', 'поцелуй', 'флирт',
-
-  // German
-  'freundin', 'dating', 'beziehung', 'romantik', 'intim',
-  'liebe', 'begleiter', 'rollenspiel', 'erotisch', 'erwachsene',
-  'kuss', 'flirt', 'date',
-
-  // French
-  'petite amie', 'rencontre', 'relation', 'romance', 'intime',
-  'amour', 'compagnon', 'jeu de rôle', 'érotique', 'adulte',
-  'baiser', 'flirt', 'rendez-vous',
-
-  // Hindi
-  'गर्लफ्रेंड', 'डेटिंग', 'रिश्ता', 'रोमांस', 'प्रेम',
-  'प्यार', 'साथी', 'रोल प्ले', 'वयस्क', 'कामुक',
-  'चुंबन', 'फ्लर्ट',
-
-  // Portuguese
-  'namorada', 'namoro', 'relacionamento', 'romance', 'íntimo',
-  'amor', 'companheiro', 'roleplay', 'erótico', 'adulto',
-  'beijo', 'paquera', 'encontro',
-
-  // Indonesian
-  'pacar', 'kencan', 'hubungan', 'romansa', 'intim',
-  'cinta', 'teman', 'roleplay', 'dewasa', 'erotis',
-  'ciuman', 'goda', 'date',
-
-  // Italian
-  'fidanzata', 'incontri', 'relazione', 'romantico', 'intimo',
-  'amore', 'compagno', 'gioco di ruolo', 'erotico', 'adulto',
-  'bacio', 'flirt', 'appuntamento'
-];
 
 // Available NSFW referral links
 const REFERRAL_LINKS = {

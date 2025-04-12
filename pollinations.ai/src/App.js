f00264fbe7359d8d1d30f3901ded9fb5836e64b3
voodoohop <thomash@pollinations.ai>
2025-04-12 17:13:12 +0200

    [pollinations.ai] Replace React Helmet with static Open Graph metadata in index.html (#1637)

--- pollinations.ai/src/App.js
+++ pollinations.ai/src/App.js
@@ -1,6 +1,5 @@
 import { Navigate, Route, Routes, useSearchParams } from "react-router-dom"
 import { useEffect } from "react"
-import { SEOMetadata, SEOImage } from "./components/Helmet"
 import Header from "./Home/Header"
 import Footer from "./Home/Footer"
 import Home from "./Home"
@@ -62,8 +61,6 @@ const AppRoutes = [
 
 const App = () => (
   <ThemeProvider theme={theme}>
-    <SEOMetadata />
-    <SEOImage />
     <Header />
     <Routes>
       {AppRoutes.map(({ key, ...route }) => (

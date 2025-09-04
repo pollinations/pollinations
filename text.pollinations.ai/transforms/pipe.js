042242624490f666a6162b77a256f38aef7e8af9
voodoohop <t.haferlach@gmail.com>
2025-09-04 11:18:31 +0200

    Add simple functional pipe transform system
    - Create transforms/pipe.js with 3-line functional pipe implementation
    - Add helper functions: addTools() and overrideModel()
    - Remove over-engineered createModelTransform.js approach
    - Provide clean, composable transform system for future use
    
    Usage:
    transform: pipe(
      overrideModel('base-model'),
      addTools([{ type: 'function', function: { name: 'google_search' } }])
    )

---
+++ text.pollinations.ai/transforms/pipe.js
@@ -0,0 +1,32 @@
+/**
+ * Simple functional pipe for composing transforms
+ * @param {...Function} transforms - Transform functions to compose
+ * @returns {Function} Composed transform function
+ */
+export function pipe(...transforms) {
+    return (messages, options) => transforms.reduce((acc, transform) => transform(acc.messages, acc.options), { messages, options });
+}
+
+/**
+ * Simple transform to add tools
+ * @param {Array} tools - Tools to add
+ * @returns {Function} Transform function
+ */
+export function addTools(tools) {
+    return (messages, options) => ({
+        messages,
+        options: { ...options, tools: [...(options.tools || []), ...tools] }
+    });
+}
+
+/**
+ * Simple transform to override model name
+ * @param {string|Function} modelName - New model name or function that returns model name
+ * @returns {Function} Transform function
+ */
+export function overrideModel(modelName) {
+    return (messages, options) => ({
+        messages,
+        options: { ...options, model: typeof modelName === 'function' ? modelName() : modelName }
+    });
+}

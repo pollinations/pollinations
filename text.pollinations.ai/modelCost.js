0c6a141f9e7ab70b2c7f6586d292a10fa95e3622
elliot.fouchy <elliot.fouchy@pixelynx.io>
2025-09-22 22:27:42 +0200

    chore(text): clean up models list and align pricing for gemini-2.5-flash-vertex

--- text.pollinations.ai/modelCost.js
+++ text.pollinations.ai/modelCost.js
@@ -53,13 +53,6 @@ const MODEL_COST = {
 	  prompt_audio: 0.0,
 	  completion_text: 0.0
 	},
-	"gemini-2.5-flash": {
-	  provider: "vertex-ai",
-	  region: "us-central1",
-	  prompt_text: 0.075,
-	  prompt_cache: 0.01875,
-	  completion_text: 0.30
-	},
 	"o4-mini-2025-04-16": {
 		provider: "api.navy",
 		region: "us-central",
@@ -67,7 +60,6 @@ const MODEL_COST = {
 		prompt_cache: 0.0,
 		completion_text: 0.0
 	  },
-
 	// ===== Google Vertex AI ===== Pricing: https://cloud.google.com/vertex-ai/generative-ai/pricing
 	"deepseek-ai/deepseek-v3.1-maas": {
 		provider: "vertex-ai",
@@ -75,7 +67,13 @@ const MODEL_COST = {
 		prompt_text: 0.60,
 		completion_text: 1.70
 	},
-  
+	"gemini-2.5-flash-vertex": {
+		provider: "vertex-ai",
+		region: "us-central1",
+		prompt_text: 0.075,
+		prompt_cache: 0.01875,
+		completion_text: 0.30
+	  },
 	// ===== Scaleway ===== Pricing: https://www.scaleway.com/en/pricing/model-as-a-service/
 	"qwen2.5-coder-32b-instruct": {
 		provider: "scaleway-ai",

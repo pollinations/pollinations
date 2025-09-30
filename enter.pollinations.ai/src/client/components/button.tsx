865c64a81b7e94a5fcd05eae79fac2831160d67a
Josh <3340175+eulervoid@users.noreply.github.com>
2025-09-30 16:35:21 +0200

    Fix fonts, improve UI (#4207)
    * Improve API key dialog: remove allowed domains, add form reset on close
    
    * Add used fonts, improve ui
    
    * fix(enter): Make user dropdown follow trigger width
    
    * feat(enter): Add confirmation dialog before deleting api keys

--- enter.pollinations.ai/src/client/components/button.tsx
+++ enter.pollinations.ai/src/client/components/button.tsx
@@ -2,14 +2,32 @@ import type { Link, LinkProps } from "@tanstack/react-router";
 import type { PropsWithChildren } from "react";
 import { cn } from "../../util.ts";
 
-const variants = {
-    default: "bg-green-950 text-green-100",
-    outline: "border-2 border-green-950 text-green-950",
-    pink: "bg-fuchsia-200 text-fuchsia-900",
-    blue: "bg-blue-200 text-blue-900",
-    green: "bg-green-200 text-green-900",
-    purple: "bg-indigo-200 text-indigo-900",
-    red: "bg-red-200 text-red-900",
+const colors = {
+    green: {
+        light: "bg-green-200 text-green-900",
+        strong: "bg-green-950 text-green-100",
+        outline: "border-2 border-green-950 text-green-950",
+    },
+    pink: {
+        light: "bg-fuchsia-200 text-fuchsia-900",
+        strong: "bg-fuchsia-900 text-fuchsia-50",
+        outline: "border-2 border-fuchsia-900 text-fuchsia-900",
+    },
+    blue: {
+        light: "bg-blue-200 text-blue-900",
+        strong: "bg-blue-900 text-blue-50",
+        outline: "border-2 border-blue-900 text-blue-900",
+    },
+    purple: {
+        light: "bg-indigo-200 text-indigo-900",
+        strong: "bg-indigo-900 text-indigo-50",
+        outline: "border-2 border-indigo-900 text-indigo-900",
+    },
+    red: {
+        light: "bg-red-200 text-red-900",
+        strong: "bg-red-900 text-red-50",
+        outline: "border-2 border-red-900 text-red-900",
+    },
 } as const;
 
 const sizes = {
@@ -18,24 +36,31 @@ const sizes = {
     large: "px-6 py-3",
 } as const;
 
+const outlineSizes = {
+    small: "px-[6px] pt-[0px] pb-[2px]",
+    medium: "px-[14px] pt-[4px] pb-[6px]",
+    large: "px-[22px] py-[10px]",
+} as const;
+
 const shapes = {
     pill: "rounded-full",
     rounded: "rounded",
     rect: "rounded-none",
 };
 
-const buttonClasses = ({ variant, size, shape, className }: BaseButtonProps) =>
+const buttonClasses = ({ color, weight, size, shape, className }: BaseButtonProps) =>
     cn(
-        "rounded-full self-center placeholder-green-950 font-medium",
+        "rounded-full self-center placeholder-green-950 font-medium box-border",
         "hover:filter hover:brightness-105 cursor-pointer",
-        variants[variant || "default"],
-        sizes[size || "medium"],
+        colors[color || "green"][weight || "strong"],
+        weight === "outline" ? outlineSizes[size || "medium"] : sizes[size || "medium"],
         shapes[shape || "pill"],
         className,
     );
 
 type BaseButtonProps = {
-    variant?: keyof typeof variants;
+    color?: keyof typeof colors;
+    weight?: "light" | "strong" | "outline";
     size?: keyof typeof sizes;
     shape?: keyof typeof shapes;
     className?: string;
@@ -62,7 +87,8 @@ type ButtonProps<T extends React.ElementType> = T extends ButtonElement
 export function Button<T extends React.ElementType>({
     as,
     children,
-    variant,
+    color,
+    weight,
     size,
     shape,
     className,
@@ -72,7 +98,7 @@ export function Button<T extends React.ElementType>({
 
     return (
         <Component
-            className={buttonClasses({ variant, size, shape, className })}
+            className={buttonClasses({ color, weight, size, shape, className })}
             {...buttonProps}
         >
             {children}

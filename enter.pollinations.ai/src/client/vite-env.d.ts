a8eba8bb2f6e46db1d8d351dee707111fc46f064
Joshua Peschke <joshua.peschke@gmail.com>
2025-11-04 23:31:43 +0100

    Fix type errors related to worker-configuration.d.ts

--- enter.pollinations.ai/src/client/vite-env.d.ts
+++ enter.pollinations.ai/src/client/vite-env.d.ts
@@ -2,10 +2,10 @@
 
 declare module "*.md" {
     import type { ComponentType } from "react";
-    
+
     const attributes: Record<string, unknown>;
     const html: string;
     const ReactComponent: ComponentType;
-    
+
     export { attributes, html, ReactComponent };
 }

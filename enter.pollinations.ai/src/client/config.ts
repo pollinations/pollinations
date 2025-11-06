aa16788887b75fb4144fbd31b681ff04a5270c29
Joshua Peschke <joshua.peschke@gmail.com>
2025-11-06 21:55:23 +0100

    Update frontend config with new meter ids

--- enter.pollinations.ai/src/client/config.ts
+++ enter.pollinations.ai/src/client/config.ts
@@ -2,21 +2,21 @@ const environmentConfig = {
     development: {
         baseUrl: "http://localhost:3000",
         authPath: "/api/auth",
-        pollenTierMeterId: "00a732fc-75c7-4ec8-975b-af0dfca7ab3c",
-        pollenPackMeterId: "ec075bd7-8073-4b30-bbc2-3a9afe6c9de7",
+        pollenTierMeterId: "1a51d03b-aea0-4e0a-a61c-b3a8914918b4",
+        pollenPackMeterId: "1fcb2fed-e21a-4e1b-9774-cf25db39d11b",
     },
     staging: {
         baseUrl: "https://enter.pollinations.ai",
         authPath: "/api/auth",
-        pollenTierMeterId: "00a732fc-75c7-4ec8-975b-af0dfca7ab3c",
-        pollenPackMeterId: "ec075bd7-8073-4b30-bbc2-3a9afe6c9de7",
+        pollenTierMeterId: "1a51d03b-aea0-4e0a-a61c-b3a8914918b4",
+        pollenPackMeterId: "1fcb2fed-e21a-4e1b-9774-cf25db39d11b",
     },
     production: {
         baseUrl: "https://enter.pollinations.ai",
         authPath: "/api/auth",
         // TODO: Update this to real value once created
-        pollenTierMeterId: "00a732fc-75c7-4ec8-975b-af0dfca7ab3c",
-        pollenPackMeterId: "ec075bd7-8073-4b30-bbc2-3a9afe6c9de7",
+        pollenTierMeterId: "1a51d03b-aea0-4e0a-a61c-b3a8914918b4",
+        pollenPackMeterId: "1fcb2fed-e21a-4e1b-9774-cf25db39d11b",
     },
 } as const;
 

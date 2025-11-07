69b1333723a4d1697c84c3cf73283df295279cd7
Joshua Peschke <joshua.peschke@gmail.com>
2025-11-07 22:19:08 +0100

    Provide a callback function to track consumed pollen in the frontend-key rate-limiter

--- enter.pollinations.ai/drizzle/0000_amused_living_tribunal.sql
+++ enter.pollinations.ai/drizzle/0000_amused_living_tribunal.sql
@@ -66,9 +66,9 @@ CREATE TABLE `user` (
 	`banned` integer DEFAULT false,
 	`ban_reason` text,
 	`ban_expires` integer
-    `github_id` integer;
-    `github_username` text;
-    `tier` text DEFAULT 'seed' NOT NULL;
+    `github_id` integer,
+    `github_username` text,
+    `tier` text DEFAULT 'seed' NOT NULL
 );
 --> statement-breakpoint
 CREATE UNIQUE INDEX `user_email_unique` ON `user` (`email`);--> statement-breakpoint

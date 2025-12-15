8c3c28ddee2713abc9cee5fcd43313e3cc27de38
Ayushman Bhattacharya <74301576+Circuit-Overtime@users.noreply.github.com>
2025-12-16 01:29:52 +0530

    Removed the /docs dir => removes the temp web ui used for showing the hooks usage

--- pollinations-react/docs/src/config.ts
+++
@@ -1,1 +0,0 @@
-export const API_KEY = import.meta.env.VITE_POLLINATIONS_API_KEY || "";
--- pollinations-react/docs/src/vite-env.d.ts
+++
@@ -1,9 +0,0 @@
-/// <reference types="vite/client" />
-
-interface ImportMetaEnv {
-    readonly VITE_POLLINATIONS_API_KEY: string;
-}
-
-interface ImportMeta {
-    readonly env: ImportMetaEnv;
-}

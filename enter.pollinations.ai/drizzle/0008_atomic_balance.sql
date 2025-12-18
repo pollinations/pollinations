baa18fa258d7bc09cde3cc5db9c8ac0c19251ae1
aaron6906 <fricker2025@gmail.com>
2025-12-17 20:45:13 -0500

    feat: add index on user balance for query performance

--- enter.pollinations.ai/drizzle/0008_atomic_balance.sql
+++ enter.pollinations.ai/drizzle/0008_atomic_balance.sql
@@ -1,3 +1,4 @@
 -- Add balance column to user table (INTEGER for micro-pollen precision)
 -- Default: 10 pollen = 10,000,000 micro-pollen
 ALTER TABLE `user` ADD `balance` integer DEFAULT 10000000 NOT NULL;
+CREATE INDEX `idx_user_balance` ON `user` (`balance`);

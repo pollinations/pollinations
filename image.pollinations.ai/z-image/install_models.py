b1d9bde26cc22d4f52f34bf602a8267823792698
Circuit-Overtime <ayushbhatt633@gmail.com>
2025-12-25 12:15:38 +0000

    removed the not necessary files

--- image.pollinations.ai/z-image/install_models.py
+++ image.pollinations.ai/z-image/install_models.py
@@ -1,10 +1,8 @@
 import os 
 import requests
 UPSCALER_MODEL_PATH = "model_cache"
-def download_model(model_name="RealESRGAN_x4plus.pth"):
+def download_model(model_name : str):
     model_url_map = {
-        "RealESRGAN_x2plus.pth": "https://github.com/Circuit-Overtime/upscale.pollinations/releases/download/1.0.0/RealESRGAN_x2plus.pth",
-        "RealESRGAN_x4plus.pth": "https://github.com/Circuit-Overtime/upscale.pollinations/releases/download/1.0.0/RealESRGAN_x4plus.pth",
         "GFPGANv1.4.pth": "https://github.com/TencentARC/GFPGAN/releases/download/v1.3.0/GFPGANv1.4.pth"
     }
     
@@ -26,6 +24,4 @@ def download_model(model_name="RealESRGAN_x4plus.pth"):
         
         print(f"Model downloaded: {UPSCALER_MODEL_PATH}/{model_name}")
 if __name__ == "__main__":
-    download_model("RealESRGAN_x2plus.pth")
-    download_model("RealESRGAN_x4plus.pth")
     download_model("GFPGANv1.4.pth")

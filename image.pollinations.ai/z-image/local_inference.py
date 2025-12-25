71877c6fc9cc51df6c41383f97733c67f0f2619a
Circuit-Overtime <ayushbhatt633@gmail.com>
2025-12-25 15:45:01 +0000

    added mechanism to detect subject wise lancoz upscaling

--- image.pollinations.ai/z-image/local_inference.py
+++ image.pollinations.ai/z-image/local_inference.py
@@ -5,9 +5,9 @@ import time
 url = "http://localhost:10002/generate"
 start_time = time.time()
 payload = {
-    "prompts": ["an indian boy"],
-    "width":2048,   
-    "height": 2048,   
+    "prompts": ["A korean girl and indian girl with a dog in a meadow with flowers, 16:9 aspect ratio"],
+    "width":1024,   
+    "height": 576,   
     "steps": 9
 }
 

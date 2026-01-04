9fb1bafd437e6bd9b1f6330a46730462ca4b9e06
Circuit-Overtime <ayushbhatt633@gmail.com>
2026-01-04 06:41:17 +0000

    t2i Inference is casuing OOM for high step counts and resolution on GPU

--- image.pollinations.ai/longcat/t2i_infer.py
+++ image.pollinations.ai/longcat/t2i_infer.py
@@ -1,23 +1,32 @@
 import torch
 from diffusers import LongCatImagePipeline
+import time 
 
-if __name__ == '__main__':
-    device = torch.device('cuda')
+def main():
+    device = "cuda"
 
-    pipe = LongCatImagePipeline.from_pretrained("meituan-longcat/LongCat-Image", torch_dtype= torch.bfloat16 )
-    pipe.to(device, torch.bfloat16)  
+    pipe = LongCatImagePipeline.from_pretrained(
+        "meituan-longcat/LongCat-Image",
+        cache_dir="model_cache",
+    )
 
-    prompt = 'a cute beautiful girl'
-    
-    image = pipe(
+    pipe.to(device, torch.bfloat16)
+    pipe.enable_attention_slicing("max")
+    pipe.enable_model_cpu_offload()
+    prompt = "a cute beautiful girl"
+
+    out = pipe(
         prompt,
         height=768,
-        width=1344,
+        width=768,
         guidance_scale=4.0,
-        num_inference_steps=25,
+        num_inference_steps=6,
         num_images_per_prompt=1,
-        generator=torch.Generator("cpu").manual_seed(43),
         enable_cfg_renorm=True,
-        enable_prompt_rewrite=True
+        # enable_prompt_rewrite=True
     ).images[0]
-    image.save('./t2i_example.png')
+
+    out.save("t2i_example.png")
+
+if __name__ == "__main__":
+    main()

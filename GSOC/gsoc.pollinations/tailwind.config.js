a04ae5f72fd4d239fb2f777053ef37fe3cbc35d0
Ayushman Bhattacharya <74301576+Circuit-Overtime@users.noreply.github.com>
2026-01-08 02:07:14 +0530

    allocated tailwind config to the @type

--- GSOC/gsoc.pollinations/tailwind.config.js
+++ GSOC/gsoc.pollinations/tailwind.config.js
@@ -1,7 +1,8 @@
+/** @type {import('tailwindcss').Config} */
 export default {
   content: [
     "./index.html",
-    "./src/**/*.{js,jsx}"
+    "./src/**/*.{js,jsx,ts,tsx}",
   ],
   theme: {
     extend: {},

a4e1db0b8bcc18f209fcf9207b1126905f604131
Ayushman Bhattacharya <74301576+Circuit-Overtime@users.noreply.github.com>
2026-01-10 10:56:31 +0530

    added video generation hook

--- sdk/pollinations-react/src/index.js
+++ sdk/pollinations-react/src/index.js
@@ -1,5 +1,5 @@
-// Export hooks
-export { default as usePollinationsImage } from "./hooks/usePollinationsImage.js";
 export { default as usePollinationsText } from "./hooks/usePollinationsText.js";
 export { default as usePollinationsChat } from "./hooks/usePollinationsChat.js";
 export { default as usePollinationsModels } from "./hooks/usePollinationsModels.js";
+export { default as usePollinationsImage } from "./hooks/usePollinationsImage.js";
+export { default as usePollinationsVideo } from "./hooks/usePollinationsVideo.js";

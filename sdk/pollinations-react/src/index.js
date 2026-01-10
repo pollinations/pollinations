3d7093b4cbf65f4d3a67815b78aab54413fae7b6
Ayushman Bhattacharya <74301576+Circuit-Overtime@users.noreply.github.com>
2026-01-10 11:22:02 +0530

    refactor: remove pollinations-react SDK and its hooks
    - Deleted the entire pollinations-react package including all hooks: usePollinationsChat, usePollinationsImage, usePollinationsModels, usePollinationsText, and usePollinationsVideo.
    - Removed associated types and index files for the React hooks.
    - Updated package.json and package-lock.json in pollinations-sdk to include peer dependencies for React and React-DOM.
    - Added support for React hooks in the pollinations-sdk package, allowing users to import hooks from '@pollinations/sdk/react'.

--- sdk/pollinations-react/src/index.js
+++
@@ -1,5 +0,0 @@
-export { default as usePollinationsText } from "./hooks/usePollinationsText.js";
-export { default as usePollinationsChat } from "./hooks/usePollinationsChat.js";
-export { default as usePollinationsModels } from "./hooks/usePollinationsModels.js";
-export { default as usePollinationsImage } from "./hooks/usePollinationsImage.js";
-export { default as usePollinationsVideo } from "./hooks/usePollinationsVideo.js";

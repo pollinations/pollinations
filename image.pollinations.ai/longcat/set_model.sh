31846b944094ecc2215fcfbe04f92b8a2d0593bc
Circuit-Overtime <ayushbhatt633@gmail.com>
2026-01-22 20:34:48 +0000

    added 2 volume names for the i2i and t2i

--- image.pollinations.ai/longcat/set_model.sh
+++ image.pollinations.ai/longcat/set_model.sh
@@ -14,9 +14,14 @@ hf download meituan-longcat/LongCat-Image --local-dir models/
 # if you have a hugging face hub token, you can also do:
 # hf download meituan-longcat/LongCat-Image --local-dir models/ --token=YOUR_HF_TOKEN
 
-
+#create a modal volume to store the model
+modal volume create <name-of-your-volume> 
 # Upload the model to modal dir 
 modal volume put <name-of-your-volume> models/ longcat-model/
+
+# Do it twice for the 2 models and use 2 different volume names
+# Link to i2i model:- hf download meituan-longcat/LongCat-Image-Edit --local-dir models/diffusion_models/LongCat-Image-Edit
+# Link to t2i model:- hf download meituan-longcat/LongCat-Image --local-dir models/diffusion_models/LongCat-Image
 # Remove the local model
 rm -rf models/
 

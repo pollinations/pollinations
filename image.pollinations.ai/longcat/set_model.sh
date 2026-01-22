cbafed0914c5621ca937e51136164bf32df255ab
Circuit-Overtime <ayushbhatt633@gmail.com>
2026-01-22 16:37:39 +0000

    steps and instruction for the bash file

---
+++ image.pollinations.ai/longcat/set_model.sh
@@ -0,0 +1,21 @@
+# make an venv folder first and activate it
+# example commands:
+# python3 -m venv venv
+# source venv/bin/activate
+# then run this script
+#install modal python and huggingface hub
+pip install modal huggingface_hub
+#auth modal 
+modal new token
+#download the model locally 
+hf download meituan-longcat/LongCat-Image --local-dir models/
+
+
+# if you have a hugging face hub token, you can also do:
+# hf download meituan-longcat/LongCat-Image --local-dir models/ --token=YOUR_HF_TOKEN
+
+
+# Upload the model to modal dir 
+modal volume put <name-of-your-volume> models/ longcat-model/
+# Remove the local model
+rm -rf models/

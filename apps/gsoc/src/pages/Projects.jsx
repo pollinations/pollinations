b0eba2884d8ad247bc7e98a6dce2cbfa2095d3ad
Circuit-Overtime <ayushbhatt633@gmail.com>
2026-02-08 12:46:41 +0530

    fixed biome issues

--- apps/gsoc/src/pages/Projects.jsx
+++ apps/gsoc/src/pages/Projects.jsx
@@ -171,7 +171,7 @@ const ProjectsPage = () => {
                         }}
                     >
                         <strong>Project sizes:</strong> Medium ≈ 175h • Large ≈
-                        350h 
+                        350h
                     </Typography>
                     <Typography
                         variant="body2"
@@ -181,7 +181,10 @@ const ProjectsPage = () => {
                             fontSize: "0.85rem",
                         }}
                     >
-                        <strong>We are looking forward for taking up 3 projects this season based on applications.</strong>
+                        <strong>
+                            We are looking forward for taking up 3 projects this
+                            season based on applications.
+                        </strong>
                     </Typography>
                 </Box>
 

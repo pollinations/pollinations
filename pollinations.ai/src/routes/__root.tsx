c66021652eb6f2001d1c85f4f2c60d68c589ebba
elliot.fouchy <elliotfouchy@gmail.com>
2026-06-03 00:24:53 +0200

    render Header + Footer in root layout

--- pollinations.ai/src/routes/__root.tsx
+++ pollinations.ai/src/routes/__root.tsx
@@ -1,4 +1,6 @@
 import { createRootRoute, HeadContent, Outlet } from "@tanstack/react-router";
+import { Footer } from "../components/shell/Footer.tsx";
+import { Header } from "../components/shell/Header.tsx";
 
 export const Route = createRootRoute({
     head: () => ({
@@ -9,11 +11,17 @@ export const Route = createRootRoute({
 
 function RootLayout() {
     return (
-        <div className="flex h-full flex-col">
+        <div data-theme="green" className="flex h-full flex-col">
             <HeadContent />
             {/* Shell body is overflow-hidden; this <main> is the scroll owner. */}
             <main id="app-scroll" className="flex-1 overflow-y-auto">
-                <Outlet />
+                <Header />
+                <div className="flex min-h-full flex-col">
+                    <div className="flex-1">
+                        <Outlet />
+                    </div>
+                    <Footer />
+                </div>
             </main>
         </div>
     );

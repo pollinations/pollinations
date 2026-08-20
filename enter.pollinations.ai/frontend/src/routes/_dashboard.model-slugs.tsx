893c65480337d38c577beb182db44864ccd169cb
elliot.fouchy <elliotfouchy@gmail.com>
2026-08-20 13:08:31 +0200

    fix: color model slug titles amber

--- enter.pollinations.ai/frontend/src/routes/_dashboard.model-slugs.tsx
+++ enter.pollinations.ai/frontend/src/routes/_dashboard.model-slugs.tsx
@@ -10,7 +10,7 @@ function ModelSlugsReference() {
     return (
         <div className="mx-auto max-w-6xl">
             <Surface variant="panel" className="p-4 pt-12 sm:p-6">
-                <Prose className="[&_td_code]:!bg-transparent [&_td_code]:!px-0 [&_td_code]:!py-0 [&_td:nth-child(2)_code]:!text-intent-danger-text [&_td:nth-child(3)_code]:!text-intent-success-text [&_th:nth-child(2)]:text-intent-danger-text [&_th:nth-child(3)]:text-intent-success-text">
+                <Prose className="[&_td:first-child]:text-intent-warning-text [&_td_code]:!bg-transparent [&_td_code]:!px-0 [&_td_code]:!py-0 [&_td:nth-child(2)_code]:!text-intent-danger-text [&_td:nth-child(3)_code]:!text-intent-success-text [&_th:first-child]:text-intent-warning-text [&_th:nth-child(2)]:text-intent-danger-text [&_th:nth-child(3)]:text-intent-success-text">
                     {modelSlugsMarkdown}
                 </Prose>
             </Surface>

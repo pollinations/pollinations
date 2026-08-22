9927b7c234488fafb7812bc7dffb385abdfdcd48
gggff123 <anumitkumardutta@gmail.com>
2026-08-22 09:28:47 +0530

    Add iotserver24 to the list.
    Closes #13666

--- shared/auth/github-id-list.ts
+++ shared/auth/github-id-list.ts
@@ -40,6 +40,7 @@ export const COMMUNITY_MODEL_ALLOWED_GITHUB_IDS = [
     30191185, // rekty
     145819645, // rodrigookk
     85689068, // pegalink
+    147928812, // iotserver24
 ] as const;
 
 const COMMUNITY_MODEL_ALLOWED_GITHUB_ID_SET = new Set<number>(

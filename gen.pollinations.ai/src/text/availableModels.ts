cf123e3b353400787c54d5beac3c2dd08390337a
ElliotEtag <36901823+ElliotEtag@users.noreply.github.com>
2026-08-31 14:02:40 +0200

    feat: add full Inkling through Fireworks (#14156)

--- gen.pollinations.ai/src/text/availableModels.ts
+++ gen.pollinations.ai/src/text/availableModels.ts
@@ -13,6 +13,7 @@ import {
 import { createMessageTransform } from "./transforms/createMessageTransform.js";
 import { createReasoningEffortTransform } from "./transforms/createReasoningEffortTransform.ts";
 import { createSystemPromptTransform } from "./transforms/createSystemPromptTransform.js";
+import { inputAudioToFireworks } from "./transforms/inputAudioToFireworks.js";
 import { pipe } from "./transforms/pipe.js";
 import { removeToolsForJsonResponse } from "./transforms/removeToolsForJsonResponse.ts";
 import { sanitizeToolSchemas } from "./transforms/sanitizeToolSchemas.js";
@@ -366,6 +367,15 @@ const models: ModelDefinition[] = [
     {
         name: "inkling",
         config: portkeyConfig["thinkingmachines/inkling-small"],
+    },
+    {
+        name: "thinkingmachines/inkling",
+        config: portkeyConfig["accounts/fireworks/models/inkling"],
+        transform: pipe(
+            stripCacheControl,
+            inputAudioToFireworks,
+            mandatoryReasoning,
+        ),
     },
     {
         name: "nemotron",

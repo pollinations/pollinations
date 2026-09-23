6299020be33053a8bd679a9bfaba4edc9f544e49
voodoohop <thomash@pollinations.ai>
2026-09-24 01:30:46 +0200

    chore: update community publisher access (#15378)

--- shared/auth/github-id-list.ts
+++ shared/auth/github-id-list.ts
@@ -41,7 +41,6 @@ export const COMMUNITY_MODEL_ALLOWED_GITHUB_IDS = [
     85689068, // pegalink
     147928812, // iotserver24
     24752658, // zero2launch
-    170070104, // NamanSoni78
     63298614, // xiaotian1171
     84572851, // immature-yt
     177411725, // Davizigjojo

3acada7998112dcdcf752333beeab6422ae661fc
gggff123 <anumitkumardutta@gmail.com>
2026-09-23 06:57:52 +0530

    Add Jonakss to the list
    Closes #15324

--- shared/auth/github-id-list.ts
+++ shared/auth/github-id-list.ts
@@ -53,6 +53,7 @@ export const COMMUNITY_MODEL_ALLOWED_GITHUB_IDS = [
     156744205, // tsgabrielle
     243879765, // fadyabohamza-netizen
     77214872, // aadirajuthup
+    11493034, // Jonakss
 ] as const;
 
 const COMMUNITY_MODEL_ALLOWED_GITHUB_ID_SET = new Set<number>(
